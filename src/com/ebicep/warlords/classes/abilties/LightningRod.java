ff185557d9df55a6e170c8f2948adb967366e3c3
Plikie <plikie341@gmail.com>
2021-04-30 18:05:39 +0200

    updated LightningRod

--- src/com/ebicep/warlords/classes/abilties/LightningRod.java
+++ src/com/ebicep/warlords/classes/abilties/LightningRod.java
@@ -3,14 +3,25 @@ package com.ebicep.warlords.classes.abilties;
 import com.ebicep.warlords.Warlords;
 import com.ebicep.warlords.WarlordsPlayer;
 import com.ebicep.warlords.classes.AbstractAbility;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.util.Vector;
+
+import java.util.List;
 
 public class LightningRod extends AbstractAbility {
 
     public LightningRod() {
-        super("Lightning Rod", 0, 0, 32, 0, 0, 0, "lightning rod description");
+        super("Lightning Rod", 0, 0, 2, 0, 0, 0,
+                "§7Call down an energizing bolt of lightning\n" +
+                        "§7upon yourself, restoring §a30% §7health and\n" +
+                        "§e160 §7energy and knock all nearby enemies back.");
     }
+    // set cooldown back to original, 2 is for testing
 
     @Override
     public void onActivate(PlayerInteractEvent e) {
@@ -19,5 +30,25 @@ public class LightningRod extends AbstractAbility {
         warlordsPlayer.subtractEnergy(-160);
         warlordsPlayer.addHealth(warlordsPlayer, name, (int) (warlordsPlayer.getMaxHealth() * .3), (int) (warlordsPlayer.getMaxHealth() * .3), critChance, critMultiplier);
 
+        Location playerLocation = player.getLocation();
+
+        List<Entity> near = player.getNearbyEntities(5.0D, 5.0D, 5.0D);
+        for (Entity entity : near) {
+            final Location otherLocation = entity.getLocation();
+            if (otherLocation.distanceSquared(playerLocation) < 30) {
+                // TODO: entity check to exclude armorstands and other entities if needed
+                if (entity != warlordsPlayer) {
+
+                    final Location loc = entity.getLocation();
+                    final Vector v = player.getLocation().toVector().subtract(loc.toVector()).normalize().multiply(-1.5).setY(0.4);
+
+                    entity.setVelocity(v);
+                }
+            }
+        }
+
+        // TODO: add effects around player with armorstands, also fix sound
+        player.playSound(player.getLocation(), "shaman.lightningrod.activation", 2, 1);
+        player.getWorld().strikeLightningEffect(playerLocation);
     }
 }
