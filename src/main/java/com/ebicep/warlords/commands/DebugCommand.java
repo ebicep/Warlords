7811695f277c9e706a6af651dd096822a1efba4e
WM <34175525+ebicep@users.noreply.github.com>
2021-08-06 18:21:01 -0400

    Added player options for every player

--- src/main/java/com/ebicep/warlords/commands/DebugCommand.java
+++ src/main/java/com/ebicep/warlords/commands/DebugCommand.java
@@ -28,6 +28,15 @@ public class DebugCommand implements CommandExecutor {
             //sender.sendMessage("§cYou need to pass an argument, valid arguments: [timer, energy, cooldown, cooldownmode, takedamage]");
             return true;
         }
+        WarlordsPlayer player = BaseCommand.requireWarlordsPlayer(sender);
+        if (args[0].equals("energy") || args[0].equals("cooldown") || args[0].equals("damage") || args[0].equals("takedamage")) {
+            if (args.length == 3 && args[2] != null) {
+                player = Warlords.getPlayer(Bukkit.getPlayer(args[2]).getUniqueId());
+            }
+            if (player == null) { // We only have a warlords player if the game is running
+                return true;
+            }
+        }
         switch (args[0]) {
             case "timer":
                 if (!(game.getState() instanceof TimerDebugAble)) {
@@ -53,23 +62,18 @@ public class DebugCommand implements CommandExecutor {
                         return true;
                 }
             case "energy": {
-                WarlordsPlayer player = BaseCommand.requireWarlordsPlayer(sender);
-                if (player == null) { // We only have a warlords player if the game is running
-                    return true;
-                }
                 if (args.length < 2) {
                     sender.sendMessage("§cEnergy requires 2 or more arguments, valid arguments: [disable, enable]");
                     return true;
                 }
-
                 switch (args[1]) {
                     case "disable":
                         player.setInfiniteEnergy(true);
-                        sender.sendMessage(ChatColor.RED + "DEV: §aEnergy consumption has been disabled!");
+                        sender.sendMessage(ChatColor.RED + "DEV: " + player.getColoredName() + "'s §aEnergy consumption has been disabled!");
                         return true;
                     case "enable":
                         player.setInfiniteEnergy(false);
-                        sender.sendMessage(ChatColor.RED + "DEV: §aEnergy consumption has been enabled!");
+                        sender.sendMessage(ChatColor.RED + "DEV: " + player.getColoredName() + "'s §aEnergy consumption has been enabled!");
                         return true;
                     default:
                         sender.sendMessage("§cInvalid option!");
@@ -78,23 +82,18 @@ public class DebugCommand implements CommandExecutor {
             }
 
             case "cooldown": {
-                WarlordsPlayer player = BaseCommand.requireWarlordsPlayer(sender);
-                if (player == null) { // We only have a warlords player if the game is running
-                    return true;
-                }
                 if (args.length < 2) {
                     sender.sendMessage("§cCooldown requires 2 or more arguments, valid arguments: [disable, enable]");
                     return true;
                 }
-
                 switch (args[1]) {
                     case "disable":
                         player.setDisableCooldowns(true);
-                        sender.sendMessage(ChatColor.RED + "DEV: §aCooldown timers have been disabled!");
+                        sender.sendMessage(ChatColor.RED + "DEV: " + player.getColoredName() + "'s §aCooldown timers have been disabled!");
                         return true;
                     case "enable":
                         player.setDisableCooldowns(false);
-                        sender.sendMessage(ChatColor.RED + "DEV: §aCooldown timers have been enabled!");
+                        sender.sendMessage(ChatColor.RED + "DEV: " + player.getColoredName() + "'s §aCooldown timers have been enabled!");
                         return true;
                     default:
                         sender.sendMessage("§cInvalid option!");
@@ -103,23 +102,18 @@ public class DebugCommand implements CommandExecutor {
             }
 
             case "damage": {
-                WarlordsPlayer player = BaseCommand.requireWarlordsPlayer(sender);
-                if (player == null) { // We only have a warlords player if the game is running
-                    return true;
-                }
                 if (args.length < 2) {
                     sender.sendMessage("§cDamage requires 2 or more arguments, valid arguments: [disable, enable]");
                     return true;
                 }
-
                 switch (args[1]) {
                     case "disable":
                         player.setTakeDamage(false);
-                        sender.sendMessage(ChatColor.RED + "§cDEV: §aTaking damage has been disabled!");
+                        sender.sendMessage(ChatColor.RED + "§cDEV: " + player.getColoredName() + "'s §aTaking damage has been disabled!");
                         return true;
                     case "enable":
                         player.setTakeDamage(true);
-                        sender.sendMessage(ChatColor.RED + "§cDEV: §aTaking damage has been enabled!");
+                        sender.sendMessage(ChatColor.RED + "§cDEV: " + player.getColoredName() + "'s §aTaking damage has been enabled!");
                         return true;
                     default:
                         sender.sendMessage("§cInvalid option!");
@@ -128,10 +122,6 @@ public class DebugCommand implements CommandExecutor {
             }
 
             case "takedamage": {
-                WarlordsPlayer player = BaseCommand.requireWarlordsPlayer(sender);
-                if (player == null) { // We only have a warlords player if the game is running
-                    return true;
-                }
                 if (args.length < 2) {
                     sender.sendMessage("§cTake Damage requires more arguments, valid arguments: [1000, 2000, 3000, 4000]");
                     return true;
@@ -140,23 +130,23 @@ public class DebugCommand implements CommandExecutor {
                 switch (args[1]) {
                     case "1000":
                         player.addHealth(player, "debug", -1000, -1000, -1, 100);
-                        sender.sendMessage(ChatColor.RED + "§cDEV: §aYou took 1000 damage!");
+                        sender.sendMessage(ChatColor.RED + "§cDEV: " + player.getColoredName() + " §atook 1000 damage!");
                         return true;
                     case "2000":
                         player.addHealth(player, "debug", -2000, -2000, -1, 100);
-                        sender.sendMessage(ChatColor.RED + "§cDEV: §aYou took 2000 damage!");
+                        sender.sendMessage(ChatColor.RED + "§cDEV: " + player.getColoredName() + " §atook 2000 damage!");
                         return true;
                     case "3000":
                         player.addHealth(player, "debug", -3000, -3000, -1, 100);
-                        sender.sendMessage(ChatColor.RED + "§cDEV: §aYou took 3000 damage!");
+                        sender.sendMessage(ChatColor.RED + "§cDEV: " + player.getColoredName() + " §atook 3000 damage!");
                         return true;
                     case "4000":
                         player.addHealth(player, "debug", -4000, -4000, -1, 100);
-                        sender.sendMessage(ChatColor.RED + "§cDEV: §aYou took 4000 damage!");
+                        sender.sendMessage(ChatColor.RED + "§cDEV: " + player.getColoredName() + " §atook 4000 damage!");
                         return true;
                     case "5000":
                         player.addHealth(player, "debug", -5000, -5000, -1, 100);
-                        sender.sendMessage(ChatColor.RED + "§cDEV: §aYou took 5000 damage!");
+                        sender.sendMessage(ChatColor.RED + "§cDEV: " + player.getColoredName() + " §atook 5000 damage!");
                         return true;
                     default:
                         sender.sendMessage("§cInvalid option! [Options: 1000, 2000, 3000, 4000]");
@@ -165,7 +155,6 @@ public class DebugCommand implements CommandExecutor {
             }
 
             case "cooldownmode": {
-
                 if (args.length < 2) {
                     sender.sendMessage("§cCooldown Mode requires 2 or more arguments, valid arguments: [disable, enable]");
                     return true;
