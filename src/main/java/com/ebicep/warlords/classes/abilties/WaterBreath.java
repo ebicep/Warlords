5ff1cc401c05756435f2b02c23491997fb25acd5
Plikie <plikie341@gmail.com>
2021-09-29 20:06:31 +0200

    patch 1.04

--- src/main/java/com/ebicep/warlords/classes/abilties/WaterBreath.java
+++ src/main/java/com/ebicep/warlords/classes/abilties/WaterBreath.java
@@ -17,7 +17,7 @@ import javax.annotation.Nonnull;
 public class WaterBreath extends AbstractAbility {
 
     public WaterBreath() {
-        super("Water Breath", 556.5f, 753.9f, 10.96f, 60, 25, 175);
+        super("Water Breath", 528, 723, 6.3f, 60, 25, 175);
     }
 
     @Override
@@ -54,7 +54,7 @@ public class WaterBreath extends AbstractAbility {
                             target.addHealth(wp, name, minDamageHeal, maxDamageHeal, critChance, critMultiplier, false);
                         } else {
                             final Location loc = target.getLocation();
-                            final Vector v = player.getLocation().toVector().subtract(loc.toVector()).normalize().multiply(-1).setY(0.3);
+                            final Vector v = player.getLocation().toVector().subtract(loc.toVector()).normalize().multiply(-1.1).setY(0.25);
 
                             target.setVelocity(v);
                         }
