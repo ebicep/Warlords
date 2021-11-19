4f5c7917dd489edb4afd521ca69465475b6f418e
WM <34175525+ebicep@users.noreply.github.com>
2021-11-18 23:01:13 -0500

    Commented out unused leaderboards

--- src/main/java/com/ebicep/warlords/database/LeaderboardManager.java
+++ src/main/java/com/ebicep/warlords/database/LeaderboardManager.java
@@ -98,11 +98,11 @@ public class LeaderboardManager {
         addCalculatedStats(playersInformation, cachedSortedPlayersLifeTime);
         //leaderboard position
         leaderboards.put("plays", new Leaderboard("Plays", new Location(world, -2564.5, 56, 712.5)));
-        leaderboards.put("dhp", new Leaderboard("DHP", new Location(world, -2555, 60.5, 717)));
+//        leaderboards.put("dhp", new Leaderboard("DHP", new Location(world, -2555, 60.5, 717)));
         leaderboards.put("dhp_per_game", new Leaderboard("DHP per Game", new Location(world, -2546.5, 56, 712.5)));
-        leaderboards.put("kills_per_game", new Leaderboard("Kills per Game", new Location(world, -2567, 75.5, 717)));
-        leaderboards.put("deaths_per_game", new Leaderboard("Deaths per Game", new Location(world, -2563, 75.5, 717)));
-        leaderboards.put("ka_per_game", new Leaderboard("Kills/Assists per Game", new Location(world, -2559, 75.5, 717)));
+//        leaderboards.put("kills_per_game", new Leaderboard("Kills per Game", new Location(world, -2567, 75.5, 717)));
+//        leaderboards.put("deaths_per_game", new Leaderboard("Deaths per Game", new Location(world, -2563, 75.5, 717)));
+//        leaderboards.put("ka_per_game", new Leaderboard("Kills/Assists per Game", new Location(world, -2559, 75.5, 717)));
     }
 
     private static void addCalculatedStats(MongoCollection<Document> collection, HashMap<String, List<Document>> cache) {
