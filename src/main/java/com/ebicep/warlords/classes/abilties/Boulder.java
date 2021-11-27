fa8c568fffeddf38661b62eac2bef67aaaba12fe
Plikie <plikie341@gmail.com>
2021-11-27 19:44:16 +0100

    bould speed, forgot to revert

--- src/main/java/com/ebicep/warlords/classes/abilties/Boulder.java
+++ src/main/java/com/ebicep/warlords/classes/abilties/Boulder.java
@@ -16,8 +16,8 @@ import org.bukkit.util.Vector;
 
 public class Boulder extends AbstractAbility {
 
-    private static final double SPEED = 0.280;
-    private static final double GRAVITY = -0.0055;
+    private static final double SPEED = 0.290;
+    private static final double GRAVITY = -0.0058;
 
     public Boulder() {
         super("Boulder", 451, 673, 7.05f, 80, 15, 175);
