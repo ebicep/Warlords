6b9819267940480c5e34ed5c29bc9f24f9b89580
Plikie <plikie341@gmail.com>
2021-12-17 23:44:37 +0100

    orb

--- src/main/java/com/ebicep/warlords/Warlords.java
+++ src/main/java/com/ebicep/warlords/Warlords.java
@@ -679,7 +679,7 @@ public class Warlords extends JavaPlugin {
                                 itr.remove();
 
                                 float minHeal = 225;
-                                float maxHeal = 300;
+                                float maxHeal = 225;
                                 if (Warlords.getPlayerSettings(orb.getOwner().getUuid()).getClassesSkillBoosts() == ClassesSkillBoosts.ORBS_OF_LIFE) {
                                     minHeal *= 1.2;
                                     maxHeal *= 1.2;
