190a60cfeaebc836bbaf5a0a525821b619d78ba1
WM <34175525+ebicep@users.noreply.github.com>
2022-01-17 06:46:05 -0500

    Disabled dropping flag if you have time warp active Added comp/pub indicator for game leaderboards Added removing slow + blind + absorp when you join the lobby Fixed player levels at the end of game

--- src/main/java/com/ebicep/warlords/database/repositories/games/pojos/DatabaseGame.java
+++ src/main/java/com/ebicep/warlords/database/repositories/games/pojos/DatabaseGame.java
@@ -118,7 +118,7 @@ public class DatabaseGame {
         //readding game holograms
         Hologram lastGameStats = HolographicDisplaysAPI.get(Warlords.getInstance()).createHologram(DatabaseGame.lastGameStatsLocation);
         holograms.add(lastGameStats);
-        lastGameStats.getLines().appendText(ChatColor.AQUA + ChatColor.BOLD.toString() + "Last Game Stats");
+        lastGameStats.getLines().appendText(ChatColor.AQUA + ChatColor.BOLD.toString() + "Last " + (isPrivate ? "Comp" : "Pub") + " Game Stats");
 
         Hologram topDamage = HolographicDisplaysAPI.get(Warlords.getInstance()).createHologram(DatabaseGame.topDamageLocation);
         holograms.add(topDamage);
