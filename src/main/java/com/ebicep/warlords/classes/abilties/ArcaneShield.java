3092e72ded740801ce4f7d3dd7bd8951cc3446ef
WM <34175525+ebicep@users.noreply.github.com>
2022-02-09 21:08:35 -0500

    Fixed arcane shield error trying to set absorption hearts to offline players

--- src/main/java/com/ebicep/warlords/classes/abilties/ArcaneShield.java
+++ src/main/java/com/ebicep/warlords/classes/abilties/ArcaneShield.java
@@ -43,7 +43,9 @@ public class ArcaneShield extends AbstractAbility {
         wp.getCooldownManager().addRegularCooldown(name, "ARCA", ArcaneShield.class, tempArcaneShield, wp, CooldownTypes.ABILITY,
                 cooldownManager -> {
                     if (new CooldownFilter<>(cooldownManager, RegularCooldown.class).filterCooldownClass(ArcaneShield.class).getStream().count() == 1) {
-                        ((EntityLiving) ((CraftPlayer) wp.getEntity()).getHandle()).setAbsorptionHearts(0);
+                        if (wp.getEntity() instanceof Player) {
+                            ((EntityLiving) ((CraftPlayer) wp.getEntity()).getHandle()).setAbsorptionHearts(0);
+                        }
                     }
                 }, duration * 20);
         ((EntityLiving) ((CraftPlayer) p).getHandle()).setAbsorptionHearts(20);
