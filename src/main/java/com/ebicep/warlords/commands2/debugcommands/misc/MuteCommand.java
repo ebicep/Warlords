f18b50685897e3ab277b135f61ff3c51c46485a3
ebicep <wmah314@gmail.com>
2022-07-28 20:41:28 -0400

    Added reworked MyLocationCommand Added more descriptions

--- src/main/java/com/ebicep/warlords/commands2/debugcommands/misc/MuteCommand.java
+++ src/main/java/com/ebicep/warlords/commands2/debugcommands/misc/MuteCommand.java
@@ -1,6 +1,7 @@
 package com.ebicep.warlords.commands2.debugcommands.misc;
 
 import co.aikar.commands.BaseCommand;
+import co.aikar.commands.CommandHelp;
 import co.aikar.commands.CommandIssuer;
 import co.aikar.commands.annotation.*;
 import com.ebicep.warlords.commands2.miscellaneouscommands.ChatChannelCommand;
@@ -21,6 +22,7 @@ public class MuteCommand extends BaseCommand {
 
     @Default
     @CommandCompletion("@players")
+    @Description("Mutes a player")
     public void mute(CommandIssuer issuer, @Values("@players") Player player) {
         UUID uuid = player.getUniqueId();
         String name = player.getName();
@@ -34,6 +36,7 @@ public class MuteCommand extends BaseCommand {
 
     @CommandAlias("unmute")
     @CommandCompletion("@players")
+    @Description("Unmutes a player")
     public void unmute(CommandIssuer issuer, @Values("@players") Player player) {
         UUID uuid = player.getUniqueId();
         String name = player.getName();
@@ -46,6 +49,7 @@ public class MuteCommand extends BaseCommand {
     }
 
     @CommandAlias("mutelist")
+    @Description("Shows the list of muted players")
     public void muteList(CommandIssuer issuer) {
         String mutedList = mutedPlayers.entrySet().stream()
                 .filter(Map.Entry::getValue)
@@ -57,4 +61,10 @@ public class MuteCommand extends BaseCommand {
             ChatChannelCommand.sendDebugMessage(issuer, ChatColor.GREEN + "Muted Players: " + ChatColor.AQUA + mutedList);
         }
     }
+
+    @HelpCommand
+    public void help(CommandIssuer issuer, CommandHelp help) {
+        help.showHelp();
+    }
+
 }
--- src/main/java/com/ebicep/warlords/commands2/debugcommands/misc/MyLocationCommand.java
+++ src/main/java/com/ebicep/warlords/commands2/debugcommands/misc/MyLocationCommand.java
@@ -4,6 +4,7 @@ import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
+import co.aikar.commands.annotation.Description;
 import com.ebicep.warlords.util.java.NumberFormat;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
@@ -20,6 +21,7 @@ public class MyLocationCommand extends BaseCommand {
     }
 
     @Default
+    @Description("Prints your current location, clickable to copy to clipboard")
     public void myLocation(Player player) {
         Location location = player.getLocation();
         String locationString = NumberFormat.formatOptionalTenths(roundToHalf(location.getX())) + ", " + NumberFormat.formatOptionalTenths(roundToHalf(location.getY())) + ", " + NumberFormat.formatOptionalTenths(roundToHalf(location.getZ()));
