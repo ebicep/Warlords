2e4863b3b015db614ceaf54694965793054bbe3e
ebicep <wmah314@gmail.com>
2022-07-30 05:01:27 -0400

    Added clickable poll ID Added setClickEvent in TextComponentBuilder

--- src/main/java/com/ebicep/warlords/util/bukkit/TextComponentBuilder.java
+++ src/main/java/com/ebicep/warlords/util/bukkit/TextComponentBuilder.java
@@ -1,6 +1,7 @@
 package com.ebicep.warlords.util.bukkit;
 
 import com.ebicep.warlords.util.chat.ChatUtils;
+import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
@@ -27,6 +28,11 @@ public class TextComponentBuilder {
         String c = ChatUtils.convertItemStackToJsonRegular(itemStack);
         c = c.substring(0, c.length() - 1) + ",}";
         textComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ITEM, new ComponentBuilder(c).create()));
+        return this;
+    }
+
+    public TextComponentBuilder setClickEvent(ClickEvent.Action action, String value) {
+        textComponent.setClickEvent(new ClickEvent(action, value));
         return this;
     }
 
