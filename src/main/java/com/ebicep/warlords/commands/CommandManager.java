0d314d80e2eaacad45752a513225a95fe4c97969
ebicep <wmah314@gmail.com>
2022-08-15 00:03:42 -0400

    Added basic MyStatsCommand Added total waves cleared to PvEDatabaseStatInformation

--- src/main/java/com/ebicep/warlords/commands/CommandManager.java
+++ src/main/java/com/ebicep/warlords/commands/CommandManager.java
@@ -41,6 +41,7 @@ import com.ebicep.warlords.player.ingame.WarlordsEntity;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
 import com.ebicep.warlords.poll.AbstractPoll;
 import com.ebicep.warlords.poll.PollCommand;
+import com.ebicep.warlords.pve.commands.MyStatsCommand;
 import com.ebicep.warlords.pve.events.mastersworkfair.MasterworksFairCommand;
 import com.ebicep.warlords.pve.weapons.WeaponCommand;
 import com.ebicep.warlords.util.chat.ChatChannels;
@@ -129,6 +130,7 @@ public class CommandManager {
         manager.registerCommand(new GuildCommand());
         manager.registerCommand(new GuildDebugCommand());
 
+        manager.registerCommand(new MyStatsCommand());
         manager.registerCommand(new WeaponCommand());
         manager.registerCommand(new MasterworksFairCommand());
     }
