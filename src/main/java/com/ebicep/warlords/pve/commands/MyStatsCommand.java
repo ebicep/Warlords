9f9a9f713e61805caa36cabcc0f33f91e41fcd3b
wmah3 <nicholaswilliam526@yahoo.com>
2022-08-15 08:59:26 -0400

    Added optional spec to /mystats command

--- src/main/java/com/ebicep/warlords/pve/commands/MyStatsCommand.java
+++ src/main/java/com/ebicep/warlords/pve/commands/MyStatsCommand.java
@@ -4,10 +4,12 @@ import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.Conditions;
 import co.aikar.commands.annotation.Default;
+import co.aikar.commands.annotation.Optional;
 import com.ebicep.warlords.database.DatabaseManager;
 import com.ebicep.warlords.database.repositories.player.pojos.AbstractDatabaseStatInformation;
 import com.ebicep.warlords.database.repositories.player.pojos.general.DatabasePlayer;
 import com.ebicep.warlords.database.repositories.player.pojos.pve.DatabasePlayerPvE;
+import com.ebicep.warlords.database.repositories.player.pojos.pve.PvEDatabaseStatInformation;
 import com.ebicep.warlords.player.general.Specializations;
 import com.ebicep.warlords.pve.events.mastersworkfair.MasterworksFairEntry;
 import com.ebicep.warlords.pve.weapons.WeaponsPvE;
@@ -24,33 +26,46 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class MyStatsCommand extends BaseCommand {
 
     @Default
-    public void myStats(Player player) {
+    public void myStats(Player player, @Optional Specializations spec) {
         DatabasePlayer databasePlayer = DatabaseManager.playerService.findByUUID(player.getUniqueId());
         DatabasePlayerPvE databasePlayerPvE = databasePlayer.getPveStats();
-        AbstractDatabaseStatInformation specStats = databasePlayerPvE.getSpec(databasePlayer.getLastSpec());
-        HashMap<WeaponsPvE, AtomicInteger> rarityWins = new HashMap<>();
-        for (MasterworksFairEntry masterworksFairEntry : databasePlayerPvE.getMasterworksFairEntries()) {
-            if (masterworksFairEntry.getPlacement() == 1) {
-                rarityWins.computeIfAbsent(masterworksFairEntry.getRarity(), v -> new AtomicInteger(0)).incrementAndGet();
+        StringBuilder stats = new StringBuilder(ChatColor.GOLD + "Your Stats");
+        if (spec == null) {
+            HashMap<WeaponsPvE, AtomicInteger> rarityWins = new HashMap<>();
+            for (MasterworksFairEntry masterworksFairEntry : databasePlayerPvE.getMasterworksFairEntries()) {
+                if (masterworksFairEntry.getPlacement() == 1) {
+                    rarityWins.computeIfAbsent(masterworksFairEntry.getRarity(), v -> new AtomicInteger(0)).incrementAndGet();
+                }
             }
+
+            stats.append(ChatColor.GREEN).append("\n Games Played: ").append(databasePlayerPvE.getPlays());
+            stats.append(ChatColor.GREEN).append("\n Wins: ").append(databasePlayerPvE.getWins());
+            stats.append(ChatColor.GREEN).append("\n Waves Cleared: ").append(databasePlayerPvE.getTotalWavesCleared());
+            stats.append(ChatColor.GREEN).append("\n DHP: ").append(databasePlayerPvE.getDHP());
+            stats.append(ChatColor.GREEN).append("\n  Damage: ").append(databasePlayerPvE.getDamage());
+            stats.append(ChatColor.GREEN).append("\n  Healing: ").append(databasePlayerPvE.getHealing());
+            stats.append(ChatColor.GREEN).append("\n  Absorbed: ").append(databasePlayerPvE.getAbsorbed());
+            stats.append(ChatColor.GREEN).append("\n Masterworks Fair Wins: ").append(rarityWins.values().stream()
+                    .mapToInt(AtomicInteger::intValue)
+                    .sum());
+            for (WeaponsPvE value : WeaponsPvE.values()) {
+                if (value.getPlayerEntries != null) {
+                    stats.append(ChatColor.GREEN).append("\n  Masterworks Fair ").append(value.name).append(" Wins: ").append(rarityWins.getOrDefault(value, new AtomicInteger(0)).intValue());
+                }
+            }
+        } else {
+            PvEDatabaseStatInformation specStats = databasePlayerPvE.getSpec(databasePlayer.getLastSpec());
+            stats.append(ChatColor.GREEN).append("\n Games Played: ").append(specStats.getPlays());
+            stats.append(ChatColor.GREEN).append("\n Wins: ").append(specStats.getWins());
+            stats.append(ChatColor.GREEN).append("\n Waves Cleared: ").append(specStats.getTotalWavesCleared());
+            stats.append(ChatColor.GREEN).append("\n DHP: ").append(specStats.getDHP());
+            stats.append(ChatColor.GREEN).append("\n  Damage: ").append(specStats.getDamage());
+            stats.append(ChatColor.GREEN).append("\n  Healing: ").append(specStats.getHealing());
+            stats.append(ChatColor.GREEN).append("\n  Absorbed: ").append(specStats.getAbsorbed());
+            stats.append(ChatColor.GREEN).append("\n Kills: ").append(specStats.getKills());
+            stats.append(ChatColor.GREEN).append("\n Assists: ").append(specStats.getAssists());
         }
 
-        StringBuilder stats = new StringBuilder(ChatColor.GOLD + "Your Stats");
-        stats.append(ChatColor.GREEN).append("\n Games Played: ").append(databasePlayerPvE.getPlays());
-        stats.append(ChatColor.GREEN).append("\n Wins: ").append(databasePlayerPvE.getWins());
-        stats.append(ChatColor.GREEN).append("\n Waves Cleared: ").append(databasePlayerPvE.getTotalWavesCleared());
-        stats.append(ChatColor.GREEN).append("\n DHP: ").append(specStats.getDHP());
-        stats.append(ChatColor.GREEN).append("\n  Damage: ").append(specStats.getDamage());
-        stats.append(ChatColor.GREEN).append("\n  Healing: ").append(specStats.getHealing());
-        stats.append(ChatColor.GREEN).append("\n  Absorbed: ").append(specStats.getAbsorbed());
-        stats.append(ChatColor.GREEN).append("\n Masterworks Fair Wins: ").append(rarityWins.values().stream()
-                .mapToInt(AtomicInteger::intValue)
-                .sum());
-        for (WeaponsPvE value : WeaponsPvE.values()) {
-            if (value.getPlayerEntries != null) {
-                stats.append(ChatColor.GREEN).append("\n  Masterworks Fair ").append(value.name).append(" Wins: ").append(rarityWins.getOrDefault(value, new AtomicInteger(0)).intValue());
-            }
-        }
         player.sendMessage(stats.toString());
     }
 
