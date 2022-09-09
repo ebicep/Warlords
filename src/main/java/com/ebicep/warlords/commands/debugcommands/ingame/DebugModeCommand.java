242cdacf4aa30526a46bed0076498a05aa92592c
ebicep <wmah314@gmail.com>
2022-09-08 20:24:47 -0400

    Removed disabling debug command in pub games as this is bypassed manually

--- src/main/java/com/ebicep/warlords/commands/debugcommands/ingame/DebugModeCommand.java
+++ src/main/java/com/ebicep/warlords/commands/debugcommands/ingame/DebugModeCommand.java
@@ -6,7 +6,6 @@ import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
 import co.aikar.commands.annotation.Description;
 import com.ebicep.warlords.abilties.internal.AbstractAbility;
-import com.ebicep.warlords.game.GameAddon;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
 import com.ebicep.warlords.util.chat.ChatChannels;
 import org.bukkit.ChatColor;
@@ -19,13 +18,6 @@ public class DebugModeCommand extends BaseCommand {
     @Default
     @Description("Disables energy consumption, Disables cooldowns, and Prevents damage from being taken")
     public void debugMode(WarlordsPlayer warlordsPlayer) {
-        if (!warlordsPlayer.getGame().getAddons().contains(GameAddon.PRIVATE_GAME)) {
-            ChatChannels.sendDebugMessage(warlordsPlayer,
-                                          ChatColor.RED + "Debug commands are disabled in public games!",
-                                          true
-            );
-            return;
-        }
         warlordsPlayer.setNoEnergyConsumption(true);
         warlordsPlayer.setDisableCooldowns(true);
         warlordsPlayer.setTakeDamage(false);
