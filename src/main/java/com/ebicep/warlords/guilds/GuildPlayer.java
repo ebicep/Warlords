b25565736c9efccd5e0a39f3871818c7f722eb0d
ebicep <wmah314@gmail.com>
2022-09-15 13:09:02 -0400

    Readded GuildPlayer#setDailyCoinsConverted

--- src/main/java/com/ebicep/warlords/guilds/GuildPlayer.java
+++ src/main/java/com/ebicep/warlords/guilds/GuildPlayer.java
@@ -86,6 +86,10 @@ public class GuildPlayer {
         return dailyCoinsConverted;
     }
 
+    public void setDailyCoinsConverted(long dailyCoinsConverted) {
+        this.dailyCoinsConverted = dailyCoinsConverted;
+    }
+
     public void addDailyCoinsConverted(long dailyCoinsConverted) {
         this.dailyCoinsConverted += dailyCoinsConverted;
     }
