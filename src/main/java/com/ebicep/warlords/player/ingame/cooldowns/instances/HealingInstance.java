289e325f666b9e01e59a0be35e1dd40af4c56469
ebicep <wmah314@gmail.com>
2022-11-12 23:16:46 -0500

    Fixed LegendaryRequiem not boosting healing and activating on self death

--- src/main/java/com/ebicep/warlords/player/ingame/cooldowns/instances/HealingInstance.java
+++ src/main/java/com/ebicep/warlords/player/ingame/cooldowns/instances/HealingInstance.java
@@ -5,9 +5,16 @@ import com.ebicep.warlords.events.player.ingame.WarlordsDamageHealingEvent;
 public interface HealingInstance extends Instance {
 
     /**
-     * Done before any healing is done to players
+     * Done before any healing is done to players - based on self cooldowns
      */
     default float doBeforeHealFromSelf(WarlordsDamageHealingEvent event, float currentHealValue) {
+        return currentHealValue;
+    }
+
+    /**
+     * Done before any healing is done to players - based on attackers cooldowns
+     */
+    default float doBeforeHealFromAttacker(WarlordsDamageHealingEvent event, float currentHealValue) {
         return currentHealValue;
     }
 
