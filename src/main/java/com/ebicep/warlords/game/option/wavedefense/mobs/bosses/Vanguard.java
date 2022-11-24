2fb5c97ec6c085219a4b456fcb667f48d0ae795e
Plikie <plikie341@gmail.com>
2022-11-24 15:22:07 +0100

    Update Vanguard boss

--- src/main/java/com/ebicep/warlords/game/option/wavedefense/mobs/bosses/Vanguard.java
+++ src/main/java/com/ebicep/warlords/game/option/wavedefense/mobs/bosses/Vanguard.java
@@ -2,6 +2,7 @@ package com.ebicep.warlords.game.option.wavedefense.mobs.bosses;
 
 import com.ebicep.warlords.abilties.internal.DamageCheck;
 import com.ebicep.warlords.effects.EffectUtils;
+import com.ebicep.warlords.effects.FallingBlockWaveEffect;
 import com.ebicep.warlords.effects.FireWorkEffectPlayer;
 import com.ebicep.warlords.events.player.ingame.WarlordsDamageHealingEvent;
 import com.ebicep.warlords.game.option.wavedefense.WaveDefenseOption;
@@ -44,11 +45,11 @@ public class Vanguard extends AbstractZombie implements BossMob {
                         Utils.applyColorTo(Material.LEATHER_BOOTS, 200, 200, 200),
                         Weapons.SILVER_PHANTASM_SWORD_3.getItem()
                 ),
-                30000,
+                35000,
                 0.15f,
                 10,
-                3200,
-                3600
+                2000,
+                3000
         );
     }
 
@@ -74,131 +75,39 @@ public class Vanguard extends AbstractZombie implements BossMob {
     public void whileAlive(int ticksElapsed, WaveDefenseOption option) {
         long playerCount = option.getGame().warlordsPlayers().count();
 
-        if (!phaseOneTriggered) {
-
+        if (ticksElapsed % 100 == 0) {
+            new FallingBlockWaveEffect(warlordsNPC.getLocation().add(0, 1, 0), 7, 1.2, Material.LEAVES, (byte) 0).play();
+            for (WarlordsEntity we : PlayerFilter
+                    .entitiesAround(warlordsNPC, 10, 10, 10)
+                    .aliveEnemiesOf(warlordsNPC)
+            ) {
+                we.getSpeed().addSpeedModifier(warlordsNPC, "Bramble Slowness", -99, 30);
+                we.addDamageInstance(
+                        warlordsNPC,
+                        "Bramble",
+                        700,
+                        1000,
+                        -1,
+                        100,
+                        true
+                );
+            }
         }
 
         if (warlordsNPC.getHealth() < (warlordsNPC.getMaxHealth() * .75f) && !phaseThreeTriggered) {
             phaseThreeTriggered = true;
-            damageToDeal.set((int) (5000 * playerCount));
-
-            for (WarlordsEntity we : PlayerFilter.playingGame(getWarlordsNPC().getGame())) {
-                if (we.getEntity() instanceof Player) {
-                    PacketUtils.sendTitle(
-                            (Player) we.getEntity(),
-                            "",
-                            ChatColor.RED + "Keep attacking Vanguard to stop the draining!",
-                            10, 40, 10
-                    );
-                }
-            }
-
-            warlordsNPC.getCooldownManager().addCooldown(new PermanentCooldown<>(
-                    "Damage Check",
-                    null,
-                    DamageCheck.class,
-                    DamageCheck.DAMAGE_CHECK,
-                    warlordsNPC,
-                    CooldownTypes.ABILITY,
-                    cooldownManager -> {
-                    },
-                    true
-            ) {
-                @Override
-                public float modifyDamageAfterInterveneFromSelf(WarlordsDamageHealingEvent event, float currentDamageValue) {
-                    if (phaseThreeTriggered) {
-                        damageToDeal.set((int) (damageToDeal.get() - currentDamageValue));
-                    }
-
-                    return currentDamageValue;
-                }
-            });
-
-            AtomicInteger countdown = new AtomicInteger(10);
-            new GameRunnable(warlordsNPC.getGame()) {
-                int counter = 0;
-                @Override
-                public void run() {
-                    if (warlordsNPC.isDead() || damageToDeal.get() <= 0) {
-                        FireWorkEffectPlayer.playFirework(warlordsNPC.getLocation(), FireworkEffect.builder()
-                                .withColor(Color.WHITE)
-                                .with(FireworkEffect.Type.BALL_LARGE)
-                                .build());
-                        this.cancel();
-                        return;
-                    }
-
-                    if (counter++ % 20 == 0) {
-                        countdown.getAndDecrement();
-                        Utils.playGlobalSound(warlordsNPC.getLocation(), Sound.NOTE_STICKS, 500, 0.8f);
-                        for (WarlordsEntity we : PlayerFilter
-                                .entitiesAround(warlordsNPC, 100, 100, 100)
-                                .aliveEnemiesOf(warlordsNPC)
-                        ) {
-                            EffectUtils.playParticleLinkAnimation(we.getLocation(), warlordsNPC.getLocation(), 255, 255, 255, 2);
-                            we.addDamageInstance(
-                                    warlordsNPC,
-                                    "Vampiric Leash",
-                                    300,
-                                    300,
-                                    -1,
-                                    100,
-                                    true
-                            );
-                        }
-                    }
-
-                    if (countdown.get() <= 0 && damageToDeal.get() > 0) {
-                        FireWorkEffectPlayer.playFirework(warlordsNPC.getLocation(), FireworkEffect.builder()
-                                .withColor(Color.WHITE)
-                                .with(FireworkEffect.Type.BALL_LARGE)
-                                .build());
-                        EffectUtils.strikeLightning(warlordsNPC.getLocation(), false, 10);
-
-                        for (WarlordsEntity we : PlayerFilter
-                                .entitiesAround(warlordsNPC, 100, 100, 100)
-                                .aliveEnemiesOf(warlordsNPC)
-                        ) {
-                            we.addDamageInstance(
-                                    warlordsNPC,
-                                    "Vampiric Leash",
-                                    300,
-                                    300,
-                                    -1,
-                                    100,
-                                    true
-                            );
-                        }
-                        this.cancel();
-                    }
-
-                    for (WarlordsEntity we : PlayerFilter.playingGame(getWarlordsNPC().getGame())) {
-                        if (we.getEntity() instanceof Player) {
-                            PacketUtils.sendTitle(
-                                    (Player) we.getEntity(),
-                                    ChatColor.YELLOW.toString() + countdown.get(),
-                                    ChatColor.RED.toString() + damageToDeal.get(),
-                                    0, 4, 0
-                            );
-                        }
-                    }
-                }
-            }.runTaskTimer(40, 0);
+            timedDamage(playerCount, 5000, 10);
         }
 
-        if (warlordsNPC.getHealth() < (warlordsNPC.getMaxHealth() * .5f) && !phaseFourTriggered) {
+        if (warlordsNPC.getHealth() < (warlordsNPC.getMaxHealth() * .25f) && !phaseFourTriggered) {
             phaseFourTriggered = true;
-
-
+            timedDamage(playerCount, 7500, 10);
         }
     }
 
     @Override
     public void onAttack(WarlordsEntity attacker, WarlordsEntity receiver, WarlordsDamageHealingEvent event) {
-        FireWorkEffectPlayer.playFirework(receiver.getLocation(), FireworkEffect.builder()
-                .withColor(Color.BLACK)
-                .with(FireworkEffect.Type.BALL)
-                .build());
+
     }
 
     @Override
@@ -213,5 +122,112 @@ public class Vanguard extends AbstractZombie implements BossMob {
                 .with(FireworkEffect.Type.BALL_LARGE)
                 .build());
         EffectUtils.strikeLightning(deathLocation, false, 2);
+    }
+
+    private void timedDamage(long playerCount, int damageValue, int timeToDealDamage) {
+        damageToDeal.set((int) (damageValue * playerCount));
+
+        for (WarlordsEntity we : PlayerFilter.playingGame(getWarlordsNPC().getGame())) {
+            if (we.getEntity() instanceof Player) {
+                PacketUtils.sendTitle(
+                        (Player) we.getEntity(),
+                        "",
+                        ChatColor.RED + "Keep attacking Illumina to stop the draining!",
+                        10, 40, 0
+                );
+            }
+            Utils.addKnockback(warlordsNPC.getLocation(), we, -2.5, 0.4);
+        }
+
+        warlordsNPC.getCooldownManager().removeCooldown(DamageCheck.class);
+        warlordsNPC.getCooldownManager().addCooldown(new PermanentCooldown<>(
+                "Damage Check",
+                null,
+                DamageCheck.class,
+                DamageCheck.DAMAGE_CHECK,
+                warlordsNPC,
+                CooldownTypes.ABILITY,
+                cooldownManager -> {
+                },
+                true
+        ) {
+            @Override
+            public float modifyDamageAfterInterveneFromSelf(WarlordsDamageHealingEvent event, float currentDamageValue) {
+                damageToDeal.set((int) (damageToDeal.get() - currentDamageValue));
+                return currentDamageValue;
+            }
+        });
+
+        AtomicInteger countdown = new AtomicInteger(timeToDealDamage);
+        new GameRunnable(warlordsNPC.getGame()) {
+            int counter = 0;
+            @Override
+            public void run() {
+                if (warlordsNPC.isDead() || damageToDeal.get() <= 0) {
+                    FireWorkEffectPlayer.playFirework(warlordsNPC.getLocation(), FireworkEffect.builder()
+                            .withColor(Color.WHITE)
+                            .with(FireworkEffect.Type.BALL_LARGE)
+                            .build());
+                    this.cancel();
+                    return;
+                }
+
+                if (counter++ % 20 == 0) {
+                    countdown.getAndDecrement();
+                    Utils.playGlobalSound(warlordsNPC.getLocation(), Sound.NOTE_STICKS, 500, 0.4f);
+                    for (WarlordsEntity we : PlayerFilter
+                            .entitiesAround(warlordsNPC, 100, 100, 100)
+                            .aliveEnemiesOf(warlordsNPC)
+                    ) {
+                        EffectUtils.playParticleLinkAnimation(we.getLocation(), warlordsNPC.getLocation(), 255, 255, 255, 2);
+                        we.addDamageInstance(
+                                warlordsNPC,
+                                "Vampiric Leash",
+                                300,
+                                300,
+                                -1,
+                                100,
+                                true
+                        );
+                    }
+                }
+
+                if (countdown.get() <= 0 && damageToDeal.get() > 0) {
+                    FireWorkEffectPlayer.playFirework(warlordsNPC.getLocation(), FireworkEffect.builder()
+                            .withColor(Color.WHITE)
+                            .with(FireworkEffect.Type.BALL_LARGE)
+                            .build());
+                    EffectUtils.strikeLightning(warlordsNPC.getLocation(), false, 10);
+
+                    for (WarlordsEntity we : PlayerFilter
+                            .entitiesAround(warlordsNPC, 100, 100, 100)
+                            .aliveEnemiesOf(warlordsNPC)
+                    ) {
+                        Utils.addKnockback(warlordsNPC.getLocation(), we, -2, 0.4);
+                        we.addDamageInstance(
+                                warlordsNPC,
+                                "Death Ray",
+                                we.getMaxHealth() * 0.9f,
+                                we.getMaxHealth() * 0.9f,
+                                -1,
+                                100,
+                                true
+                        );
+                    }
+                    this.cancel();
+                }
+
+                for (WarlordsEntity we : PlayerFilter.playingGame(getWarlordsNPC().getGame())) {
+                    if (we.getEntity() instanceof Player) {
+                        PacketUtils.sendTitle(
+                                (Player) we.getEntity(),
+                                ChatColor.YELLOW.toString() + countdown.get(),
+                                ChatColor.RED.toString() + damageToDeal.get(),
+                                0, 4, 0
+                        );
+                    }
+                }
+            }
+        }.runTaskTimer(40, 0);
     }
 }
