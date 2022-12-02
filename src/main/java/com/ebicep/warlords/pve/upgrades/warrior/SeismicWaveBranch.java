35280054027a987b87593575147f501df9684f04
Plikie <plikie341@gmail.com>
2022-12-02 11:38:56 +0100

    Update bers upgrades

--- src/main/java/com/ebicep/warlords/pve/upgrades/warrior/SeismicWaveBranch.java
+++ src/main/java/com/ebicep/warlords/pve/upgrades/warrior/SeismicWaveBranch.java
@@ -15,38 +15,38 @@ public class SeismicWaveBranch extends AbstractUpgradeBranch<SeismicWave> {
         super(abilityTree, ability);
         treeA.add(new Upgrade(
                 "Impair - Tier I",
-                "+7.5% Damage",
+                "+5% Damage",
                 5000,
                 () -> {
-                    ability.setMinDamageHeal(minDamage * 1.075f);
-                    ability.setMaxDamageHeal(maxDamage * 1.075f);
+                    ability.setMinDamageHeal(minDamage * 1.05f);
+                    ability.setMaxDamageHeal(maxDamage * 1.05f);
                 }
         ));
         treeA.add(new Upgrade(
                 "Impair - Tier II",
-                "+15% Damage",
+                "+10% Damage",
                 10000,
+                () -> {
+                    ability.setMinDamageHeal(minDamage * 1.1f);
+                    ability.setMaxDamageHeal(maxDamage * 1.1f);
+                }
+        ));
+        treeA.add(new Upgrade(
+                "Impair - Tier III",
+                "+15% Damage",
+                15000,
                 () -> {
                     ability.setMinDamageHeal(minDamage * 1.15f);
                     ability.setMaxDamageHeal(maxDamage * 1.15f);
                 }
         ));
         treeA.add(new Upgrade(
-                "Impair - Tier III",
-                "+22.5% Damage",
-                15000,
-                () -> {
-                    ability.setMinDamageHeal(minDamage * 1.225f);
-                    ability.setMaxDamageHeal(maxDamage * 1.225f);
-                }
-        ));
-        treeA.add(new Upgrade(
                 "Impair - Tier IV",
-                "+30% Damage",
+                "+20% Damage",
                 20000,
                 () -> {
-                    ability.setMinDamageHeal(minDamage * 1.3f);
-                    ability.setMaxDamageHeal(maxDamage * 1.3f);
+                    ability.setMinDamageHeal(minDamage * 1.2f);
+                    ability.setMaxDamageHeal(maxDamage * 1.2f);
                 }
         ));
 
