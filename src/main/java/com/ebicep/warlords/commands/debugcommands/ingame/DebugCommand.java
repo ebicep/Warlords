5b188522204c37a77ba4645c8d2b45f86daf1f48
ebicep <wmah314@gmail.com>
2022-12-19 04:02:56 -0500

    Fixed DebugCommand permissions

--- src/main/java/com/ebicep/warlords/commands/debugcommands/ingame/DebugCommand.java
+++ src/main/java/com/ebicep/warlords/commands/debugcommands/ingame/DebugCommand.java
@@ -20,12 +20,10 @@ import java.util.Comparator;
 import static com.ebicep.warlords.util.chat.ChatChannels.sendDebugMessage;
 
 @CommandAlias("wl")
-@CommandPermission("group.administrator")
-//@CommandPermission("warlords.game.debug")
+@CommandPermission("warlords.game.debug")
 public class DebugCommand extends BaseCommand {
 
     @Default
-    @CommandPermission("warlords.game.debug")
     public void openDebugMenu(Player player) {
         DebugMenu.openDebugMenu(player);
     }
