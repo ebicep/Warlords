9cde9df9552adb93e14c2c1b03a2457da68bbd54
Plikie <plikie341@gmail.com>
2023-01-07 18:24:20 +0100

    Fix hardmode health

--- src/main/java/com/ebicep/warlords/game/option/wavedefense/mobs/AbstractMob.java
+++ src/main/java/com/ebicep/warlords/game/option/wavedefense/mobs/AbstractMob.java
@@ -134,7 +134,7 @@ public abstract class AbstractMob<T extends CustomEntity<?>> implements Mob {
             // Flag check whether mob is a boss.
             boolean bossFlagCheck = playerCount > 1 && warlordsNPC.getMobTier() == MobTier.BOSS;
             // Reduce base scale by 100 for each player after 2 or more players in game instance.
-            double modifiedScale = scale - (playerCount > 1 ? 100 * playerCount : 0);
+            double modifiedScale = scale - (playerCount > 1 ? (isEndless ? 100 : 75) * playerCount : 0);
             // Divide scale based on wave count.
             double modifier = option.getWaveCounter() / modifiedScale + 1;
 
