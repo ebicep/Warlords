c20bf212cfc537aecaff575644e3fe63ebc4e54d
ebicep <wmah314@gmail.com>
2023-02-04 22:47:00 -0500

    feat: narmer ancestors now increase 15% health per 5 waves

--- src/main/java/com/ebicep/warlords/pve/mobs/events/pharaohsrevenge/EventDjer.java
+++ src/main/java/com/ebicep/warlords/pve/mobs/events/pharaohsrevenge/EventDjer.java
@@ -58,8 +58,10 @@ public class EventDjer extends AbstractZombie implements BossMob {
 
     @Override
     public void onSpawn(WaveDefenseOption option) {
-        if (option.getWaveCounter() > 5) {
-            warlordsNPC.setMaxBaseHealth(warlordsNPC.getMaxBaseHealth() * 1.15f);
+        int currentWave = option.getWaveCounter();
+        if (currentWave % 5 == 0 && currentWave > 5) {
+            float additionalHealthMultiplier = 1 + .15f * (currentWave / 5f - 1);
+            warlordsNPC.setMaxBaseHealth(warlordsNPC.getMaxBaseHealth() * additionalHealthMultiplier);
             warlordsNPC.heal();
         }
         option.getGame().registerEvents(new Listener() {
