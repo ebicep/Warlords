29c7e7d843bd44953b2a5cd0f6df89e12e4f0139
Plikie <53056381+Plikie@users.noreply.github.com>
2023-02-24 13:10:17 +0100

    Update floor architecture

--- src/main/java/com/ebicep/warlords/game/option/treasurehunt/FloorGenerator.java
+++ src/main/java/com/ebicep/warlords/game/option/treasurehunt/FloorGenerator.java
@@ -1,6 +1,9 @@
 package com.ebicep.warlords.game.option.treasurehunt;
 
+import java.util.HashSet;
+
 public class FloorGenerator {
 
+    private HashSet<FloorRoom> floors;
     private int maxTotalFloorRooms;
 }
--- src/main/java/com/ebicep/warlords/game/option/treasurehunt/FloorRoom.java
+++ src/main/java/com/ebicep/warlords/game/option/treasurehunt/FloorRoom.java
@@ -1,32 +1,31 @@
 package com.ebicep.warlords.game.option.treasurehunt;
 
-import net.minecraft.server.v1_8_R3.World;
+import com.ebicep.warlords.util.bukkit.LocationFactory;
+import org.bukkit.Location;
 
 public abstract class FloorRoom {
 
     private String floorName;
     private int floorLevel;
 
-    private World world;
+    private final Location beginLocation;
+    private final Location endLocation;
 
-    private double x1;
-    private double y1;
-    private double z1;
-
-    private double x2;
-    private double y2;
-    private double z2;
-
-    protected FloorRoom(String floorName, int floorLevel, World world, double x1, double y1, double z1, double x2, double y2, double z2) {
+    protected FloorRoom(String floorName, int floorLevel, LocationFactory loc, double x1, double y1, double z1, double x2, double y2, double z2) {
         this.floorName = floorName;
         this.floorLevel = floorLevel;
-        this.world = world;
-        this.x1 = x1;
-        this.y1 = y1;
-        this.z1 = z1;
-        this.x2 = x2;
-        this.y2 = y2;
-        this.z2 = z2;
+        this.beginLocation = new Location(
+                loc.getWorld(),
+                loc.getX() + Math.min(x1, x2),
+                loc.getY() + Math.min(y1, y2),
+                loc.getZ() + Math.min(z1, z2)
+        );
+        this.endLocation = new Location(
+                loc.getWorld(),
+                loc.getX() + Math.max(x1, x2),
+                loc.getY() + Math.max(y1, y2),
+                loc.getZ() + Math.max(z1, z2)
+        );
     }
 
     public String getFloorName() {
@@ -45,51 +44,11 @@ public abstract class FloorRoom {
         this.floorLevel = floorLevel;
     }
 
-    public double getX1() {
-        return x1;
+    public Location getBeginLocation() {
+        return beginLocation;
     }
 
-    public void setX1(double x1) {
-        this.x1 = x1;
-    }
-
-    public double getY1() {
-        return y1;
-    }
-
-    public void setY1(double y1) {
-        this.y1 = y1;
-    }
-
-    public double getZ1() {
-        return z1;
-    }
-
-    public void setZ1(double z1) {
-        this.z1 = z1;
-    }
-
-    public double getX2() {
-        return x2;
-    }
-
-    public void setX2(double x2) {
-        this.x2 = x2;
-    }
-
-    public double getY2() {
-        return y2;
-    }
-
-    public void setY2(double y2) {
-        this.y2 = y2;
-    }
-
-    public double getZ2() {
-        return z2;
-    }
-
-    public void setZ2(double z2) {
-        this.z2 = z2;
+    public Location getEndLocation() {
+        return endLocation;
     }
 }
