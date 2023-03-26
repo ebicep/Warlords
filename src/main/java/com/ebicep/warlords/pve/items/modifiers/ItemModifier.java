98b5a2a389c45b448c1804071d535d4e462519d1
ebicep <wmah314@gmail.com>
2023-03-26 16:21:52 -0400

    refactor: bless/curse found/rng tier chance

--- src/main/java/com/ebicep/warlords/pve/items/modifiers/ItemModifier.java
+++ src/main/java/com/ebicep/warlords/pve/items/modifiers/ItemModifier.java
@@ -7,31 +7,25 @@ import java.util.HashMap;
 public interface ItemModifier<T extends Enum<T>> {
 
     HashMap<Integer, Double> BLESSING_TIER_CHANCE = new HashMap<>() {{
-        put(1, 29.0);
-        put(2, 10.0);
+        put(1, 68.75);
+        put(2, 25.0);
         put(3, 5.0);
-        put(4, 2.5);
-        put(5, 1.0);
+        put(4, 1.0);
+        put(5, 0.25);
     }};
     HashMap<Integer, Double> CURSE_TIER_CHANCE = new HashMap<>() {{
         put(1, 5.0);
-        put(2, 12.5);
-        put(3, 25.0);
-        put(4, 7.5);
-        put(5, 2.5);
+        put(2, 15.0);
+        put(3, 50.0);
+        put(4, 20.5);
+        put(5, 10.0);
     }};
-    RandomCollection<Integer> GENERATE_BLESSING = new RandomCollection<Integer>()
-            .add(29, 1)
-            .add(10, 2)
-            .add(5, 3)
-            .add(2.5, 4)
-            .add(1, 5);
-    RandomCollection<Integer> GENERATE_CURSE = new RandomCollection<Integer>()
-            .add(5, 1)
-            .add(12.5, 2)
-            .add(25, 3)
-            .add(7.5, 4)
-            .add(2.5, 5);
+    RandomCollection<Integer> GENERATE_BLESSING = new RandomCollection<>() {{
+        BLESSING_TIER_CHANCE.forEach((tier, weight) -> add(weight, tier));
+    }};
+    RandomCollection<Integer> GENERATE_CURSE = new RandomCollection<>() {{
+        CURSE_TIER_CHANCE.forEach((tier, weight) -> add(weight, tier));
+    }};
 
     T[] getValues();
 
