e8aedf9bb34eee2472a759578b9b3be5fa6d934e
ebicep <wmah314@gmail.com>
2023-04-05 20:52:56 -0400

    refactor: changed vendor star stock to 3 with increasing price

--- src/main/java/com/ebicep/customentities/npc/traits/IllusionVendorTrait.java
+++ src/main/java/com/ebicep/customentities/npc/traits/IllusionVendorTrait.java
@@ -27,7 +27,7 @@ import java.util.UUID;
 public class IllusionVendorTrait extends WarlordsTrait {
 
     private static final List<SpendableBuyShop> SHOP = List.of(
-            new SpendableBuyShop(1, MobDrops.ZENITH_STAR, 1, 30),
+            new SpendableBuyShop(1, MobDrops.ZENITH_STAR, 3, 30),
             new SpendableBuyShop(200, Currencies.LEGEND_FRAGMENTS, 1, 20),
             new SpendableBuyShop(1, Currencies.CELESTIAL_BRONZE, 1, 500),
             new SpendableBuyShop(50, Currencies.FAIRY_ESSENCE, 1, 10),
@@ -93,13 +93,18 @@ public class IllusionVendorTrait extends WarlordsTrait {
             Spendable rewardSpendable = reward.getSpendable();
             int rewardPrice = reward.getPrice();
             String mapName = reward.getMapName();
+            Long purchasedAmount = weeklyRewardsPurchased.getOrDefault(mapName, 0L);
+            if (rewardSpendable == MobDrops.ZENITH_STAR) {
+                rewardPrice += purchasedAmount * 10;
+            }
 
             String stock;
             if (reward.getStock() == -1) {
                 stock = "Unlimited";
             } else {
-                stock = "" + (reward.getStock() - weeklyRewardsPurchased.getOrDefault(mapName, 0L));
+                stock = "" + (reward.getStock() - purchasedAmount);
             }
+            int finalRewardPrice = rewardPrice;
             menu.setItem(i + 1, 1,
                     new ItemBuilder(rewardSpendable.getItem())
                             .name(rewardSpendable.getCostColoredName(rewardAmount))
@@ -110,24 +115,24 @@ public class IllusionVendorTrait extends WarlordsTrait {
                             .flags(ItemFlag.HIDE_POTION_EFFECTS)
                             .get(),
                     (m, e) -> {
-                        if (pveStats.getCurrencyValue(Currencies.ILLUSION_SHARD) < rewardPrice) {
-                            player.sendMessage(ChatColor.RED + "You need " + Currencies.ILLUSION_SHARD.getCostColoredName(rewardPrice) + ChatColor.RED + " to purchase this item!");
+                        if (pveStats.getCurrencyValue(Currencies.ILLUSION_SHARD) < finalRewardPrice) {
+                            player.sendMessage(ChatColor.RED + "You need " + Currencies.ILLUSION_SHARD.getCostColoredName(finalRewardPrice) + ChatColor.RED + " to purchase this item!");
                             player.playSound(player.getLocation(), Sound.VILLAGER_NO, 2, 0.5f);
                             return;
                         }
-                        if (reward.getStock() != -1 && weeklyRewardsPurchased.getOrDefault(mapName, 0L) >= reward.getStock()) {
+                        if (reward.getStock() != -1 && purchasedAmount >= reward.getStock()) {
                             player.sendMessage(ChatColor.RED + "This item is out of stock!");
                             player.playSound(player.getLocation(), Sound.VILLAGER_NO, 2, 0.5f);
                             return;
                         }
-                        pveStats.subtractCurrency(Currencies.ILLUSION_SHARD, rewardPrice);
+                        pveStats.subtractCurrency(Currencies.ILLUSION_SHARD, finalRewardPrice);
                         rewardSpendable.addToPlayer(databasePlayer, rewardAmount);
 
                         pveStats.getIllusionVendorRewardsPurchased().merge(mapName, 1L, Long::sum);
                         weeklyRewardsPurchased.merge(mapName, 1L, Long::sum);
 
                         player.sendMessage(ChatColor.GREEN + "Purchased " + rewardSpendable.getCostColoredName(rewardAmount) +
-                                ChatColor.GREEN + " for " + Currencies.ILLUSION_SHARD.getCostColoredName(rewardPrice) +
+                                ChatColor.GREEN + " for " + Currencies.ILLUSION_SHARD.getCostColoredName(finalRewardPrice) +
                                 ChatColor.GREEN + "!");
                         player.playSound(player.getLocation(), Sound.LEVEL_UP, 500, 2f);
                         openIllusionVendor(player, databasePlayer, databasePlayerWeekly);
