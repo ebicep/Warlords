b74fe7dba69554b1d922cb2c24857267bd78b016
Plikie <plikie341@gmail.com>
2023-04-22 22:44:26 +0200

    Fix reward title in onslaught displaying the wrong reward

--- src/main/java/com/ebicep/warlords/game/option/pve/onslaught/OnslaughtOption.java
+++ src/main/java/com/ebicep/warlords/game/option/pve/onslaught/OnslaughtOption.java
@@ -363,13 +363,14 @@ public class OnslaughtOption implements Option, PveOption {
             Long amount = reward.getB();
             playerPouch.computeIfAbsent(uuid, k -> new HashMap<>())
                        .merge(spendable, amount, Long::sum);
+            String rewardString = spendable.getChatColor() + "+" + spendable.getCostColoredName(amount);
             RewardInventory.sendRewardMessage(uuid,
-                    pouchName + ": " + spendable.getChatColor() + "+" + spendable.getCostColoredName(amount)
+                    pouchName + ": " + rewardString
             );
             ChatUtils.sendTitleToGamePlayers(
                     game,
                     pouchName + ":",
-                    spendable.getChatColor() + "+" + spendable.getCostColoredName(amount)
+                    rewardString
             );
         }
     }
