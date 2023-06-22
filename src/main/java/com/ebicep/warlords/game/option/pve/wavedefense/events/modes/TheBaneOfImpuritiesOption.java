538bd26d1e56a66324da4ba24775026650c62b72
ebicep <wmah314@gmail.com>
2023-06-22 15:35:59 -0400

    fix: started illumina event crystxll

---
+++ src/main/java/com/ebicep/warlords/game/option/pve/wavedefense/events/modes/TheBaneOfImpuritiesOption.java
@@ -0,0 +1,9 @@
+package com.ebicep.warlords.game.option.pve.wavedefense.events.modes;
+
+import com.ebicep.warlords.game.option.Option;
+import com.ebicep.warlords.game.option.pve.EventGameEndOption;
+
+public class TheBaneOfImpuritiesOption implements Option, EventGameEndOption {
+
+
+}
