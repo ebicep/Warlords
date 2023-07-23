f2ff02c9d9a038f5656a350a545e67a2f81b85a7
ebicep <wmah314@gmail.com>
2023-07-23 12:55:37 -0400

    feat: weapon drop rate increase message

--- src/main/java/com/ebicep/warlords/game/option/pve/wavedefense/events/modes/TheBorderlineOfIllusionEvent.java
+++ src/main/java/com/ebicep/warlords/game/option/pve/wavedefense/events/modes/TheBorderlineOfIllusionEvent.java
@@ -8,6 +8,8 @@ import com.ebicep.warlords.game.option.pve.EventGameEndOption;
 import com.ebicep.warlords.player.ingame.WarlordsEntity;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
 import com.ebicep.warlords.pve.upgrades.AbilityTree;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -15,6 +17,7 @@ import javax.annotation.Nonnull;
 
 public class TheBorderlineOfIllusionEvent implements Option, EventGameEndOption {
 
+    private static final int BONUS = 10;
     private float weaponDropBonus = 0;
 
     @Override
@@ -24,7 +27,7 @@ public class TheBorderlineOfIllusionEvent implements Option, EventGameEndOption 
             public void onWaveClear(WarlordsGameWaveClearEvent event) {
                 int waveCleared = event.getWaveCleared();
                 if (waveCleared != 0 && waveCleared % 15 == 0) {
-                    weaponDropBonus += .1;
+                    increaseWeaponDropBonus(game);
                 }
             }
 
@@ -41,6 +44,11 @@ public class TheBorderlineOfIllusionEvent implements Option, EventGameEndOption 
             AbilityTree abilityTree = ((WarlordsPlayer) player).getAbilityTree();
             abilityTree.setMaxMasterUpgrades(5);
         }
+    }
+
+    private void increaseWeaponDropBonus(Game game) {
+        weaponDropBonus += BONUS / 100f;
+        game.forEachOnlinePlayer((player, team) -> player.sendMessage(Component.text("The chance of a weapon dropping has increased by " + BONUS + "%!", NamedTextColor.GREEN)));
     }
 
 }
