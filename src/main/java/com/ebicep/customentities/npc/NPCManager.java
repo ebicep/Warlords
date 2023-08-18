13d5a9e8b0a944f9175cb2100aa6a844511f93f4
ebicep <wmah314@gmail.com>
2023-08-18 17:14:12 -0400

    fix: vendor (idk)

--- src/main/java/com/ebicep/customentities/npc/NPCManager.java
+++ src/main/java/com/ebicep/customentities/npc/NPCManager.java
@@ -298,9 +298,6 @@ public class NPCManager {
 
         NPC npc = npcRegistry.createNPC(EntityType.IRON_GOLEM, "illusion-vendor");
         npc.addTrait(IllusionVendorTrait.class);
-        HologramTrait hologramTrait = npc.getOrAddTrait(HologramTrait.class);
-        hologramTrait.setLine(0, ChatColor.YELLOW.toString() + ChatColor.BOLD + "RIGHT-CLICK");
-        hologramTrait.setLine(1, ChatColor.GREEN + "Illusion Vendor");
 
         npc.data().set(NPC.Metadata.NAMEPLATE_VISIBLE, false);
 
