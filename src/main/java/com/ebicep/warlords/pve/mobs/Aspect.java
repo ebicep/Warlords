e96061726948a3466a696d5de7c75b305051b6d3
ebicep <wmah314@gmail.com>
2023-09-02 22:30:56 -0400

    feat: started aspects

---
+++ src/main/java/com/ebicep/warlords/pve/mobs/Aspect.java
@@ -0,0 +1,14 @@
+package com.ebicep.warlords.pve.mobs;
+
+public enum Aspect {
+
+    ARMOURED(),
+    BLIZZARD(),
+    EVASIVE(),
+    INFERNAL(),
+    JUGGERNAUT(),
+    REGENERATIVE(),
+    SWIFT(),
+    VAMPIRIC(),
+
+}
