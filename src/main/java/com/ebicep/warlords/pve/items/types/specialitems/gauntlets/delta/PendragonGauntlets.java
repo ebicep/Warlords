a21ef33b647a661c70e6f41adf0c4d64e376147d
ebicep <wmah314@gmail.com>
2023-09-28 03:22:54 -0400

    refactor: PendragonGauntlets bonus

--- src/main/java/com/ebicep/warlords/pve/items/types/specialitems/gauntlets/delta/PendragonGauntlets.java
+++ src/main/java/com/ebicep/warlords/pve/items/types/specialitems/gauntlets/delta/PendragonGauntlets.java
@@ -1,14 +1,17 @@
 package com.ebicep.warlords.pve.items.types.specialitems.gauntlets.delta;
 
 import com.ebicep.warlords.abilities.internal.AbstractAbility;
-import com.ebicep.warlords.abilities.internal.AbstractGroundSlam;
+import com.ebicep.warlords.events.player.ingame.WarlordsDamageHealingEvent;
 import com.ebicep.warlords.game.option.pve.PveOption;
 import com.ebicep.warlords.player.general.Classes;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
+import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.PermanentCooldown;
 import com.ebicep.warlords.pve.items.statpool.BasicStatPool;
 import com.ebicep.warlords.pve.items.types.AbstractItem;
 import com.ebicep.warlords.pve.items.types.AppliesToWarlordsPlayer;
 import com.ebicep.warlords.pve.items.types.specialitems.gauntlets.omega.GlassKnuckles;
+import com.ebicep.warlords.util.warlords.Utils;
 
 import java.util.Set;
 
@@ -27,27 +30,43 @@ public class PendragonGauntlets extends SpecialDeltaGauntlet implements AppliesT
     }
 
     @Override
+    public void applyToWarlordsPlayer(WarlordsPlayer warlordsPlayer, PveOption pveOption) {
+        AbstractAbility weapon = warlordsPlayer.getSpec().getWeapon();
+        weapon.getEnergyCost().addAdditiveModifier(getName(), 10);
+        warlordsPlayer.getCooldownManager().addCooldown(new PermanentCooldown<>(
+                getName(),
+                null,
+                SamsonsFists.class,
+                null,
+                warlordsPlayer,
+                CooldownTypes.WEAPON,
+                cooldownManager -> {
+
+                },
+                false
+        ) {
+            @Override
+            public void onDamageFromAttacker(WarlordsDamageHealingEvent event, float currentDamageValue, boolean isCrit) {
+                if (event.getAbility().equals(weapon.getName())) {
+                    Utils.addKnockback(getName(), warlordsPlayer.getLocation(), event.getWarlordsEntity(), -.7, 0.15f);
+                }
+            }
+        });
+    }
+
+    @Override
     public String getName() {
         return "Pendragon Gauntlets";
     }
 
     @Override
     public String getBonus() {
-        return "Increases Ground Slam's Crit Chance by 20%.";
+        return "Weapon right-clicks deals moderate knockback, at slightly increased energy cost.";
     }
 
     @Override
     public String getDescription() {
         return "For the worthy.";
-    }
-
-    @Override
-    public void applyToWarlordsPlayer(WarlordsPlayer warlordsPlayer, PveOption pveOption) {
-        for (AbstractAbility ability : warlordsPlayer.getSpec().getAbilities()) {
-            if (ability instanceof AbstractGroundSlam) {
-                ability.setCritChance(ability.getCritChance() + 20);
-            }
-        }
     }
 
     @Override
