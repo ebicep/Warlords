2d851e549861743e47059fad2dd2a4dd2991a16b
ebicep <wmah314@gmail.com>
2023-09-29 17:05:07 -0400

    refactor: AthenianAegis bonus

--- src/main/java/com/ebicep/warlords/pve/items/types/specialitems/buckler/omega/AthenianAegis.java
+++ src/main/java/com/ebicep/warlords/pve/items/types/specialitems/buckler/omega/AthenianAegis.java
@@ -3,12 +3,12 @@ package com.ebicep.warlords.pve.items.types.specialitems.buckler.omega;
 import com.ebicep.warlords.events.player.ingame.WarlordsDamageHealingEvent;
 import com.ebicep.warlords.game.option.pve.PveOption;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
+import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.PermanentCooldown;
 import com.ebicep.warlords.pve.items.statpool.BasicStatPool;
 import com.ebicep.warlords.pve.items.types.AppliesToWarlordsPlayer;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
+import com.ebicep.warlords.pve.items.types.specialitems.buckler.delta.BucklerPiece;
 
-import java.util.Objects;
 import java.util.Set;
 
 public class AthenianAegis extends SpecialOmegaBuckler implements AppliesToWarlordsPlayer {
@@ -21,40 +21,42 @@ public class AthenianAegis extends SpecialOmegaBuckler implements AppliesToWarlo
     }
 
     @Override
-    public String getName() {
-        return "Athenian Aegis";
-    }
-
-    @Override
-    public String getBonus() {
-        return "For every mob on the field, increase your ability to heal by 1%.";
-    }
-
-    @Override
     public String getDescription() {
         return "It's covered in olive oil. No, it doesn't come off.";
     }
 
     @Override
-    public void applyToWarlordsPlayer(WarlordsPlayer warlordsPlayer, PveOption pveOption) {
-        warlordsPlayer.getGame().registerEvents(new Listener() {
+    public String getBonus() {
+        return "For every mob on the field, increase your healing by 0.5%.";
+    }
 
-            @EventHandler
-            public void onDamageHeal(WarlordsDamageHealingEvent event) {
-                if (!Objects.equals(event.getAttacker(), warlordsPlayer)) {
-                    return;
-                }
-                if (event.isDamageInstance()) {
-                    return;
-                }
-                float healingBoost = getHealingBoost();
-                event.setMin(event.getMin() * healingBoost);
-                event.setMax(event.getMax() * healingBoost);
+    @Override
+    public String getName() {
+        return "Athenian Aegis";
+    }
+
+    @Override
+    public void applyToWarlordsPlayer(WarlordsPlayer warlordsPlayer, PveOption pveOption) {
+        warlordsPlayer.getCooldownManager().addCooldown(new PermanentCooldown<>(
+                getName(),
+                null,
+                BucklerPiece.class,
+                null,
+                warlordsPlayer,
+                CooldownTypes.ITEM,
+                cooldownManager -> {
+                },
+                false
+        ) {
+
+            @Override
+            public float doBeforeHealFromAttacker(WarlordsDamageHealingEvent event, float currentHealValue) {
+                return currentHealValue * getHealingBoost();
             }
 
             private float getHealingBoost() {
                 int mobCount = pveOption.mobCount();
-                return 1 + (mobCount * .01f);
+                return 1 + (mobCount * .005f);
             }
         });
     }
