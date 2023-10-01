ec2ab4c2162014cae51148812c1947e7262825ad
ebicep <wmah314@gmail.com>
2023-10-01 02:53:06 -0400

    feat: added bounties for mob tiers

---
+++ src/main/java/com/ebicep/warlords/pve/bountysystem/bounties/HuntEliteI.java
@@ -0,0 +1,62 @@
+package com.ebicep.warlords.pve.bountysystem.bounties;
+
+import com.ebicep.warlords.events.player.ingame.WarlordsDeathEvent;
+import com.ebicep.warlords.game.Game;
+import com.ebicep.warlords.player.ingame.WarlordsNPC;
+import com.ebicep.warlords.pve.bountysystem.AbstractBounty;
+import com.ebicep.warlords.pve.bountysystem.Bounty;
+import com.ebicep.warlords.pve.bountysystem.costs.DailyCost;
+import com.ebicep.warlords.pve.bountysystem.rewards.WeeklyRewardSpendable1;
+import com.ebicep.warlords.pve.bountysystem.trackers.TracksDuringGame;
+import com.ebicep.warlords.pve.mobs.tiers.EliteMob;
+import org.springframework.data.annotation.Transient;
+
+import java.util.UUID;
+
+public class HuntEliteI extends AbstractBounty implements TracksDuringGame, DailyCost, WeeklyRewardSpendable1 {
+
+    @Transient
+    private int newKills = 0;
+
+    @Override
+    public String getName() {
+        return "Hunt-Elite";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Kill 500 Elite enemies in any gamemode.";
+    }
+
+    @Override
+    public int getTarget() {
+        return 500;
+    }
+
+    @Override
+    public Bounty getBounty() {
+        return Bounty.HUNT_ELITE_I;
+    }
+
+    @Override
+    public void reset() {
+        newKills = 0;
+    }
+
+    @Override
+    public void onKill(UUID uuid, WarlordsDeathEvent event) {
+        if (event.getWarlordsEntity() instanceof WarlordsNPC warlordsNPC && warlordsNPC.getMob() instanceof EliteMob) {
+            newKills++;
+        }
+    }
+
+    @Override
+    public boolean trackGame(Game game) {
+        return true;
+    }
+
+    @Override
+    public long getNewValue() {
+        return newKills;
+    }
+}
