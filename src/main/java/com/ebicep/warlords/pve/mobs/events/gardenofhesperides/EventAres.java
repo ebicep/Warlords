9198c1accb0b9be44471fcbb58c3df429eb28258
ebicep <wmah314@gmail.com>
2023-10-03 02:16:02 -0400

    feat: event ares

--- src/main/java/com/ebicep/warlords/pve/mobs/events/gardenofhesperides/EventAres.java
+++ src/main/java/com/ebicep/warlords/pve/mobs/events/gardenofhesperides/EventAres.java
@@ -3,10 +3,23 @@ package com.ebicep.warlords.pve.mobs.events.gardenofhesperides;
 import com.ebicep.warlords.events.player.ingame.WarlordsDamageHealingEvent;
 import com.ebicep.warlords.game.option.pve.PveOption;
 import com.ebicep.warlords.player.ingame.WarlordsEntity;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownFilter;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
+import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.RegularCooldown;
+import com.ebicep.warlords.player.ingame.cooldowns.instances.InstanceFlags;
+import com.ebicep.warlords.pve.mobs.AbstractMob;
 import com.ebicep.warlords.pve.mobs.Mob;
+import com.ebicep.warlords.pve.mobs.abilities.SpawnMobAbility;
 import com.ebicep.warlords.pve.mobs.tiers.BossMinionMob;
 import com.ebicep.warlords.pve.mobs.zombie.AbstractZombie;
+import com.ebicep.warlords.util.bukkit.LocationUtils;
+import com.ebicep.warlords.util.warlords.GameRunnable;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Location;
+
+import javax.annotation.Nonnull;
+import java.util.List;
 
 public class EventAres extends AbstractZombie implements BossMinionMob {
 
@@ -30,7 +43,29 @@ public class EventAres extends AbstractZombie implements BossMinionMob {
                 walkSpeed,
                 damageResistance,
                 minMeleeDamage,
-                maxMeleeDamage
+                maxMeleeDamage,
+                new SpawnMobAbility(10, Mob.INTERMEDIATE_WARRIOR_BERSERKER) {
+                    @Override
+                    public AbstractMob<?> createMob(@Nonnull WarlordsEntity wp) {
+                        return mobToSpawn.createMob(pveOption.getRandomSpawnLocation(null));
+                    }
+
+                    @Override
+                    public int getSpawnAmount() {
+                        return (int) (pveOption.getGame().warlordsPlayers().count() + 1);
+                    }
+                },
+                new SpawnMobAbility(10, Mob.ADVANCED_WARRIOR_BERSERKER) {
+                    @Override
+                    public AbstractMob<?> createMob(@Nonnull WarlordsEntity wp) {
+                        return mobToSpawn.createMob(pveOption.getRandomSpawnLocation(null));
+                    }
+
+                    @Override
+                    public int getSpawnAmount() {
+                        return (int) (pveOption.getGame().warlordsPlayers().count());
+                    }
+                }
         );
     }
 
@@ -40,13 +75,55 @@ public class EventAres extends AbstractZombie implements BossMinionMob {
     }
 
     @Override
+    public void onSpawn(PveOption option) {
+        super.onSpawn(option);
+        new GameRunnable(option.getGame()) {
+
+            @Override
+            public void run() {
+                List<Location> mesmerSpawnLocations = LocationUtils.getCircle(warlordsNPC.getLocation(), 3, option.playerCount());
+                for (Location mesmerSpawnLocation : mesmerSpawnLocations) {
+                    option.spawnNewMob(Mob.ADVANCED_WARRIOR_BERSERKER.createMob(mesmerSpawnLocation));
+                }
+            }
+        }.runTaskLater(40);
+
+    }
+
+    @Override
     public void whileAlive(int ticksElapsed, PveOption option) {
 
     }
 
     @Override
     public void onAttack(WarlordsEntity attacker, WarlordsEntity receiver, WarlordsDamageHealingEvent event) {
-
+        event.getFlags().add(InstanceFlags.PIERCE);
+        receiver.getCooldownManager().removeCooldownByName("Ares Wounding");
+        receiver.getCooldownManager().addCooldown(new RegularCooldown<>(
+                "Ares Wounding",
+                "WND",
+                EventAres.class,
+                null,
+                attacker,
+                CooldownTypes.DEBUFF,
+                cooldownManager -> {
+                },
+                cooldownManager -> {
+                    if (new CooldownFilter<>(cooldownManager, RegularCooldown.class).filterNameActionBar("WND").stream().count() == 1) {
+                        receiver.sendMessage(
+                                Component.text("You are no longer ", NamedTextColor.GRAY)
+                                         .append(Component.text("wounded", NamedTextColor.RED))
+                                         .append(Component.text(".", NamedTextColor.GRAY))
+                        );
+                    }
+                },
+                60
+        ) {
+            @Override
+            public float modifyHealingFromSelf(WarlordsDamageHealingEvent event, float currentHealValue) {
+                return currentHealValue * .6f;
+            }
+        });
     }
 
     @Override
