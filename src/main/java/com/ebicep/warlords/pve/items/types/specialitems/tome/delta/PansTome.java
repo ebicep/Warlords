ff14cef7968f0afc7d58135ec65d24c290768a51
ebicep <wmah314@gmail.com>
2023-10-17 00:46:09 -0400

    fix: PansTome recursion

--- src/main/java/com/ebicep/warlords/pve/items/types/specialitems/tome/delta/PansTome.java
+++ src/main/java/com/ebicep/warlords/pve/items/types/specialitems/tome/delta/PansTome.java
@@ -5,11 +5,13 @@ import com.ebicep.warlords.game.option.pve.PveOption;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
 import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
 import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.PermanentCooldown;
+import com.ebicep.warlords.player.ingame.cooldowns.instances.InstanceFlags;
 import com.ebicep.warlords.pve.items.statpool.BasicStatPool;
 import com.ebicep.warlords.pve.items.types.AbstractItem;
 import com.ebicep.warlords.pve.items.types.specialitems.CraftsInto;
 import com.ebicep.warlords.pve.items.types.specialitems.tome.omega.GuideForTheRiverStyx;
 
+import java.util.EnumSet;
 import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -54,18 +56,21 @@ public class PansTome extends SpecialDeltaTome implements CraftsInto {
         ) {
             @Override
             public void onDamageFromAttacker(WarlordsDamageHealingEvent event, float currentDamageValue, boolean isCrit) {
-                if (event.getAbility().isEmpty() && ThreadLocalRandom.current().nextDouble() <= .2) {
-                    for (int i = 0; i < 2; i++) {
-                        event.getWarlordsEntity().addDamageInstance(
-                                warlordsPlayer,
-                                event.getAbility(),
-                                event.getMin(),
-                                event.getMax(),
-                                event.getCritChance(),
-                                event.getCritMultiplier(),
-                                event.getFlags()
-                        );
-                    }
+                if (!event.getAbility().isEmpty() || !(ThreadLocalRandom.current().nextDouble() <= .2) || event.getFlags().contains(InstanceFlags.RECURSIVE)) {
+                    return;
+                }
+                EnumSet<InstanceFlags> flags = EnumSet.copyOf(event.getFlags());
+                flags.add(InstanceFlags.RECURSIVE);
+                for (int i = 0; i < 2; i++) {
+                    event.getWarlordsEntity().addDamageInstance(
+                            warlordsPlayer,
+                            event.getAbility(),
+                            event.getMin(),
+                            event.getMax(),
+                            event.getCritChance(),
+                            event.getCritMultiplier(),
+                            flags
+                    );
                 }
             }
         });
