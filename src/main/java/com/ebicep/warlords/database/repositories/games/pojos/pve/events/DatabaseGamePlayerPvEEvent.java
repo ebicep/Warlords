6a45cc41ad537b3a830904d350a5538d0502ebd8
ebicep <wmah314@gmail.com>
2023-10-21 11:57:09 -0400

    fix: event game bounties not being tracked

--- src/main/java/com/ebicep/warlords/database/repositories/games/pojos/pve/events/DatabaseGamePlayerPvEEvent.java
+++ src/main/java/com/ebicep/warlords/database/repositories/games/pojos/pve/events/DatabaseGamePlayerPvEEvent.java
@@ -1,10 +1,15 @@
 package com.ebicep.warlords.database.repositories.games.pojos.pve.events;
 
+import com.ebicep.warlords.database.DatabaseManager;
+import com.ebicep.warlords.database.repositories.events.pojos.DatabaseGameEvent;
 import com.ebicep.warlords.database.repositories.games.pojos.pve.wavedefense.DatabaseGamePlayerPvEWaveDefense;
+import com.ebicep.warlords.database.repositories.player.pojos.pve.events.EventMode;
 import com.ebicep.warlords.events.game.WarlordsGameTriggerWinEvent;
 import com.ebicep.warlords.game.option.pve.wavedefense.WaveDefenseOption;
 import com.ebicep.warlords.game.option.pve.wavedefense.events.EventPointsOption;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.pve.bountysystem.trackers.TracksDuringGame;
+import com.ebicep.warlords.pve.bountysystem.trackers.TracksPostGame;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 public abstract class DatabaseGamePlayerPvEEvent extends DatabaseGamePlayerPvEWaveDefense {
@@ -23,6 +28,25 @@ public abstract class DatabaseGamePlayerPvEEvent extends DatabaseGamePlayerPvEWa
     ) {
         super(warlordsPlayer, gameWinEvent, waveDefenseOption);
         this.points = eventPointsOption.getPoints().getOrDefault(warlordsPlayer.getUuid(), 0);
+        if (DatabaseGameEvent.eventIsActive()) {
+            DatabaseManager.getPlayer(warlordsPlayer.getUuid(), databasePlayer -> {
+                DatabaseGameEvent currentGameEvent = DatabaseGameEvent.currentGameEvent;
+                EventMode eventMode = currentGameEvent.getEvent().eventsStatsFunction
+                        .apply(databasePlayer.getPveStats().getEventStats())
+                        .get(currentGameEvent.getStartDateSecond());
+                if (eventMode == null) {
+                    return;
+                }
+                eventMode.getTrackableBounties()
+                         .forEach(bounty -> {
+                             if (bounty instanceof TracksPostGame tracksPostGame) {
+                                 tracksPostGame.onGameEnd(waveDefenseOption.getGame(), warlordsPlayer, gameWinEvent);
+                             } else if (bounty instanceof TracksDuringGame tracksDuringGame) {
+                                 tracksDuringGame.apply(bounty);
+                             }
+                         });
+            });
+        }
     }
 
     public long getPoints() {
