66e8d359fcb3550fa01d506694955e9ed2bf2276
ebicep <wmah314@gmail.com>
2023-12-25 01:28:41 -0500

    fix: SpawnSettings maxSpawnTimes not being accounted for

--- src/main/java/com/ebicep/warlords/game/option/pve/wavedefense/waves/SimpleWave.java
+++ src/main/java/com/ebicep/warlords/game/option/pve/wavedefense/waves/SimpleWave.java
@@ -9,14 +9,15 @@ import org.bukkit.Location;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class SimpleWave implements Wave {
 
-    private int delay;
     private final RandomCollection<SpawnSettings> randomCollection = new RandomCollection<>();
     private final int count;
     private final Component message;
+    private int delay;
     private int spawnTickPeriod = 8;
 
     public SimpleWave(@Nullable Component message) {
@@ -35,12 +36,17 @@ public class SimpleWave implements Wave {
         return add(randomCollection.getSize() == 0 ? 1 : randomCollection.getTotal() / randomCollection.getSize(), factory);
     }
 
+    public SimpleWave add(double baseWeight, Mob factory) {
+        randomCollection.add(baseWeight, new SpawnSettings(baseWeight, factory, null));
+        return this;
+    }
+
     public SimpleWave add(Mob factory, Location customSpawnLocation) {
         return add(randomCollection.getSize() == 0 ? 1 : randomCollection.getTotal() / randomCollection.getSize(), factory, customSpawnLocation);
     }
 
-    public SimpleWave add(double baseWeight, Mob factory) {
-        randomCollection.add(baseWeight, new SpawnSettings(baseWeight, factory, null));
+    public SimpleWave add(double baseWeight, Mob factory, Location customSpawnLocation) {
+        randomCollection.add(baseWeight, new SpawnSettings(baseWeight, factory, customSpawnLocation));
         return this;
     }
 
@@ -49,14 +55,20 @@ public class SimpleWave implements Wave {
         return this;
     }
 
-    public SimpleWave add(double baseWeight, Mob factory, Location customSpawnLocation) {
-        randomCollection.add(baseWeight, new SpawnSettings(baseWeight, factory, customSpawnLocation));
-        return this;
-    }
-
     @Override
     public AbstractMob spawnMonster(Location loc) {
         SpawnSettings spawnSettings = randomCollection.next();
+        if (spawnSettings.incrementTimesSpawned()) {
+            // recreate the random collection without the spawn settings that have reached their max spawn times
+            RandomCollection<SpawnSettings> newRandomCollection = new RandomCollection<>();
+            for (Map.Entry<Double, SpawnSettings> entry : randomCollection.getMap().entrySet()) {
+                if (entry.getValue() != spawnSettings) {
+                    newRandomCollection.add(entry.getKey(), entry.getValue());
+                }
+            }
+            randomCollection.getMap().clear();
+            randomCollection.getMap().putAll(newRandomCollection.getMap());
+        }
         AbstractMob mob = spawnSettings.mob().createMob(spawnSettings.spawnLocations() == null ? loc : spawnSettings.getRandomSpawnLocation());
         if (mob instanceof BossMob) {
             loc.getWorld().strikeLightningEffect(loc);
@@ -84,13 +96,51 @@ public class SimpleWave implements Wave {
         return message;
     }
 
-    record SpawnSettings(double weight, int maxSpawnTimes, Mob mob, List<Location> spawnLocations) {
+    static final class SpawnSettings {
+        private final double weight;
+        private final int maxSpawnTimes;
+        private final Mob mob;
+        private final List<Location> spawnLocations;
+        private int timesSpawned = 0;
+
         public SpawnSettings(double weight, Mob mob, Location location) {
             this(weight, Integer.MAX_VALUE, mob, location == null ? null : List.of(location));
         }
 
+        SpawnSettings(double weight, int maxSpawnTimes, Mob mob, List<Location> spawnLocations) {
+            this.weight = weight;
+            this.maxSpawnTimes = maxSpawnTimes;
+            this.mob = mob;
+            this.spawnLocations = spawnLocations;
+        }
+
         public Location getRandomSpawnLocation() {
             return spawnLocations.get(ThreadLocalRandom.current().nextInt(spawnLocations.size()));
+        }
+
+        public double weight() {
+            return weight;
+        }
+
+        public int maxSpawnTimes() {
+            return maxSpawnTimes;
+        }
+
+        public Mob mob() {
+            return mob;
+        }
+
+        public List<Location> spawnLocations() {
+            return spawnLocations;
+        }
+
+        public int getTimesSpawned() {
+            return timesSpawned;
+        }
+
+        public boolean incrementTimesSpawned() {
+            timesSpawned++;
+            return timesSpawned >= maxSpawnTimes;
         }
     }
 }
