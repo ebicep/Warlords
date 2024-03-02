4a70fd947ed071b88e7f1505d626562017de9f82
ebicep <wmah314@gmail.com>
2024-03-02 14:50:50 -0500

    fix: JudgementStrike m2 having old code

--- src/main/java/com/ebicep/warlords/pve/upgrades/rogue/assassin/JudgementStrikeBranch.java
+++ src/main/java/com/ebicep/warlords/pve/upgrades/rogue/assassin/JudgementStrikeBranch.java
@@ -73,8 +73,6 @@ public class JudgementStrikeBranch extends AbstractUpgradeBranch<JudgementStrike
                 50000,
                 () -> {
                     ability.setCritMultiplier(ability.getCritMultiplier() + 45);
-                    ability.getEnergyCost().addAdditiveModifier("Master Upgrade Branch", -5);
-                    ability.multiplyMinMax(0.65f);
                 }
         );
     }
