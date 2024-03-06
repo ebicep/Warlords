4509a64135d18afd330c823f1c6375a9b224ed97
ebicep <wmah314@gmail.com>
2024-03-06 12:41:50 -0500

    feat: tower defense - tweaked AquamancerTower

--- src/main/java/com/ebicep/warlords/game/option/towerdefense/towers/AquamancerTower.java
+++ src/main/java/com/ebicep/warlords/game/option/towerdefense/towers/AquamancerTower.java
@@ -4,6 +4,7 @@ package com.ebicep.warlords.game.option.towerdefense.towers;
 import com.ebicep.warlords.effects.EffectUtils;
 import com.ebicep.warlords.game.Game;
 import com.ebicep.warlords.game.option.towerdefense.attributes.AttackSpeed;
+import com.ebicep.warlords.game.option.towerdefense.attributes.Damage;
 import com.ebicep.warlords.game.option.towerdefense.attributes.Heal;
 import com.ebicep.warlords.game.option.towerdefense.attributes.Range;
 import com.ebicep.warlords.game.option.towerdefense.attributes.upgradeable.TowerUpgrade;
@@ -18,22 +19,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class AquamancerTower extends AbstractTower implements Heal, Range, AttackSpeed, Upgradeable.Path2 {
+public class AquamancerTower extends AbstractTower implements Damage, Heal, Range, AttackSpeed, Upgradeable.Path2 {
 
+    private final List<FloatModifiable> damage = new ArrayList<>();
     private final List<FloatModifiable> heal = new ArrayList<>();
     private final List<FloatModifiable> range = new ArrayList<>();
     private final List<FloatModifiable> attackSpeed = new ArrayList<>();
     private final List<TowerUpgrade> upgrades = new ArrayList<>();
 
-    private final FloatModifiable healing = new FloatModifiable(500);
-    private final FloatModifiable healingRange = new FloatModifiable(30);
-    private final FloatModifiable healingAttackSpeed = new FloatModifiable(3 * 20); // 5 seconds
+    private final FloatModifiable boltDamage = new FloatModifiable(100);
+    private final FloatModifiable boltHealing = new FloatModifiable(100);
+    private final FloatModifiable boltRange = new FloatModifiable(30);
+    private final FloatModifiable boltAttackSpeed = new FloatModifiable(1 * 20); // 5 seconds
 
     public AquamancerTower(Game game, UUID owner, Location location) {
         super(game, owner, location);
-        heal.add(healing);
-        range.add(healingRange);
-        attackSpeed.add(healingAttackSpeed);
+        damage.add(boltDamage);
+        heal.add(boltHealing);
+        range.add(boltRange);
+        attackSpeed.add(boltAttackSpeed);
 //        TowerUpgradeInstance.DamageUpgradeInstance upgradeDamage1 = new TowerUpgradeInstance.DamageUpgradeInstance(25);
 //        TowerUpgradeInstance.DamageUpgradeInstance upgradeDamage2 = new TowerUpgradeInstance.DamageUpgradeInstance(25);
 //
@@ -66,11 +70,12 @@ public class AquamancerTower extends AbstractTower implements Heal, Range, Attac
         if (ticksElapsed % 5 == 0) {
 //            EffectUtils.displayParticle(Particle.CRIMSON_SPORE, centerLocation, 5, .5, .1, .5, 2);
         }
-        int attackSpeed = (int) healingAttackSpeed.getCalculatedValue();
+        int attackSpeed = (int) boltAttackSpeed.getCalculatedValue();
         if (ticksElapsed % attackSpeed == 0) {
-            float rangeValue = healingRange.getCalculatedValue();
-            float healValue = this.healing.getCalculatedValue();
-            getAllyMob(rangeValue, 1).forEach(warlordsNPC -> {
+            float rangeValue = boltRange.getCalculatedValue();
+            float damageValue = this.boltDamage.getCalculatedValue();
+            float healValue = this.boltHealing.getCalculatedValue();
+            getEnemyMobs(rangeValue, 1).forEach(warlordsNPC -> {
                 Entity npcEntity = warlordsNPC.getEntity();
                 Location effectLocation = warlordsNPC.getLocation().add(0, npcEntity.getHeight(), 0);
                 Utils.playGlobalSound(effectLocation, "mage.waterbolt.impact", 2, 1);
@@ -79,15 +84,33 @@ public class AquamancerTower extends AbstractTower implements Heal, Range, Attac
                         effectLocation,
                         npcEntity.getBoundingBox().getWidthX()
                 );
-                warlordsNPC.addHealingInstance(
+                warlordsNPC.addDamageInstance(
                         warlordsTower,
                         "Water",
-                        healValue,
-                        healValue,
+                        damageValue,
+                        damageValue,
                         0,
                         100
                 );
             });
+//            getAllyMob(rangeValue, 1).forEach(warlordsNPC -> {
+//                Entity npcEntity = warlordsNPC.getEntity();
+//                Location effectLocation = warlordsNPC.getLocation().add(0, npcEntity.getHeight(), 0);
+//                Utils.playGlobalSound(effectLocation, "mage.waterbolt.impact", 2, 1);
+//                EffectUtils.playCircularEffectAround(
+//                        Particle.DRIP_WATER,
+//                        effectLocation,
+//                        npcEntity.getBoundingBox().getWidthX()
+//                );
+//                warlordsNPC.addHealingInstance(
+//                        warlordsTower,
+//                        "Water",
+//                        healValue,
+//                        healValue,
+//                        0,
+//                        100
+//                );
+//            });
         }
     }
 
@@ -95,6 +118,11 @@ public class AquamancerTower extends AbstractTower implements Heal, Range, Attac
     @Override
     public List<TowerUpgrade> getUpgrades() {
         return upgrades;
+    }
+
+    @Override
+    public List<FloatModifiable> getDamages() {
+        return damage;
     }
 
     @Override
