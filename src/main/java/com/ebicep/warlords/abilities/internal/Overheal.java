57e338cefa3a8d5fbbdb818daa63a604e0567fa0
ebicep <wmah314@gmail.com>
2024-05-29 11:40:54 -0400

    feat: overheal 20s -> 15s

--- src/main/java/com/ebicep/warlords/abilities/internal/Overheal.java
+++ src/main/java/com/ebicep/warlords/abilities/internal/Overheal.java
@@ -18,7 +18,7 @@ public interface Overheal {
                 CooldownTypes.BUFF,
                 cooldownManager -> {
                 },
-                Overheal.OVERHEAL_DURATION * 20
+                Overheal.OVERHEAL_DURATION * 15
         );
     }
 }
