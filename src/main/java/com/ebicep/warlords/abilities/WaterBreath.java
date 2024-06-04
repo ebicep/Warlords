1c10bf1f2935d770f6529c8c04485ff3f4bf3e77
ebicep <wmah314@gmail.com>
2024-06-03 21:14:27 -0400

    feat: WaterBreath - healing 511-706 -> 409-551 cooldown 9.25 > 6.3

--- src/main/java/com/ebicep/warlords/abilities/WaterBreath.java
+++ src/main/java/com/ebicep/warlords/abilities/WaterBreath.java
@@ -48,7 +48,7 @@ public class WaterBreath extends AbstractAbility implements RedAbilityIcon, CanR
     private double velocity = 1.1;
 
     public WaterBreath() {
-        super("Water Breath", 511, 706, 9.25f, 60, 25, 175);
+        super("Water Breath", 409, 551, 6.3f, 60, 25, 175);
     }
 
     @Override
