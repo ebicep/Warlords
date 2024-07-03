7bec4782bce2359a2206c8001b0bb3e5a497da57
ebicep <wmah314@gmail.com>
2024-07-03 00:48:15 -0400

    feat: increase flag cap detection

--- src/main/java/com/ebicep/warlords/game/option/pvp/FlagSpawnPointOption.java
+++ src/main/java/com/ebicep/warlords/game/option/pvp/FlagSpawnPointOption.java
@@ -258,7 +258,7 @@ public class FlagSpawnPointOption implements Option {
                     ));
                 }
             }
-        }.runTaskTimer(0, 3);
+        }.runTaskTimer(0, 2);
         new GameRunnable(game) {
             @Override
             public void run() {
