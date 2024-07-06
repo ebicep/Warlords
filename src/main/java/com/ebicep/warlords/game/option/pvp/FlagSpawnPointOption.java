be87128657264e6dfcbeaa4ce9fc49f7f33c2695
ebicep <wmah314@gmail.com>
2024-07-05 23:49:06 -0400

    fix: flag % ramping

--- src/main/java/com/ebicep/warlords/game/option/pvp/FlagSpawnPointOption.java
+++ src/main/java/com/ebicep/warlords/game/option/pvp/FlagSpawnPointOption.java
@@ -53,7 +53,7 @@ public class FlagSpawnPointOption implements Option {
             .name(Component.text("Flag Finder", NamedTextColor.GREEN))
             .unbreakable()
             .get();
-    public static final int FLAG_MULTIPLIER_PERIOD = 30; // ticks
+    public static final int FLAG_MULTIPLIER_PERIOD = 50; // ticks
 
     @Nonnull
     private final FlagInfo info;
