8a100e059455e58d96c3fd41351e1d9574e586fb
ebicep <ebicep314@gmail.com>
2025-05-17 13:16:35 -0400

    fix: pyromancer m1

--- src/main/java/com/ebicep/warlords/abilities/TimeWarpPyromancer.java
+++ src/main/java/com/ebicep/warlords/abilities/TimeWarpPyromancer.java
@@ -134,7 +134,7 @@ public class TimeWarpPyromancer extends AbstractTimeWarp {
 
             @Override
             public float modifyDamageBeforeInterveneFromAttacker(WarlordsDamageHealingEvent event, float currentDamageValue) {
-                if (pveMasterUpgrade2) {
+                if (pveMasterUpgrade) {
                     return currentDamageValue * convertToMultiplicationDecimal(wp.getBlocksTravelled() - startingBlocksTravelled);
                 }
                 return currentDamageValue;
