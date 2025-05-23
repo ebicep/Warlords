eedf29a5c78efbff3b96976ace83d976198d3823
ebicep <ebicep314@gmail.com>
2025-05-23 00:26:01 -0400

    refactor: ArcaneShatter to be on expire as well

--- src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/ArcaneShatter.java
+++ src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/ArcaneShatter.java
@@ -1,18 +1,23 @@
 package com.ebicep.warlords.player.general.specboosts.boosts;
 
-import com.ebicep.warlords.abilities.ArcaneShield;
 import com.ebicep.warlords.abilities.internal.AbstractAbility;
+import com.ebicep.warlords.abilities.internal.Shield;
 import com.ebicep.warlords.classes.AbstractPlayerClass;
+import com.ebicep.warlords.events.player.ingame.WarlordsAddCooldownEvent;
 import com.ebicep.warlords.events.player.ingame.WarlordsDamageHealingEvent;
 import com.ebicep.warlords.player.general.specboosts.SpecBoostManager;
 import com.ebicep.warlords.player.ingame.WarlordsEntity;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.player.ingame.cooldowns.AbstractCooldown;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownManager;
 import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
 import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.RegularCooldown;
 import com.ebicep.warlords.util.warlords.PlayerFilter;
 import org.bukkit.event.EventHandler;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
 
 public class ArcaneShatter implements SpecBoostManager.SpecBoost<ArcaneShatter> {
 
@@ -71,29 +76,38 @@ public class ArcaneShatter implements SpecBoostManager.SpecBoost<ArcaneShatter> 
         }
 
         @EventHandler
-        public void onWarlordsArcaneShieldBrokenEvent(ArcaneShield.WarlordsArcaneShieldBrokenEvent event) {
+        public void onCooldownAddEvent(WarlordsAddCooldownEvent event) {
             if (!warlordsEntity.equals(event.getWarlordsEntity())) {
                 return;
             }
-            PlayerFilter.entitiesAround(warlordsEntity, range, range, range)
-                        .aliveEnemiesOf(warlordsEntity)
-                        .forEach(we -> we.setStunTicks(stunTicks));
-            warlordsEntity.getCooldownManager().addCooldown(new RegularCooldown<>(
-                    getStringName(),
-                    null,
-                    Boost.class,
-                    null,
-                    warlordsEntity,
-                    CooldownTypes.SPEC_BOOST,
-                    cooldownManager -> {},
-                    stunTicks
-            ) {
-                @Override
-                public float modifyDamageBeforeInterveneFromAttacker(WarlordsDamageHealingEvent event, float currentDamageValue) {
-                    return currentDamageValue * AbstractAbility.convertToMultiplicationDecimal(damageIncrease);
-                }
+            AbstractCooldown<?> cooldown = event.getAbstractCooldown();
+            if (!Objects.equals(cooldown.getName(), "Arcane Shield") || !(cooldown.getCooldownObject() instanceof Shield shield) || !cooldown.getFrom().equals(warlordsEntity)) {
+                return;
+            }
+            Consumer<CooldownManager> oldOnRemove = cooldown.getOnRemove();
+            cooldown.setOnRemove(cooldownManager -> {
+                oldOnRemove.accept(cooldownManager);
+                PlayerFilter.entitiesAround(warlordsEntity, range, range, range)
+                            .aliveEnemiesOf(warlordsEntity)
+                            .forEach(we -> we.setStunTicks(stunTicks));
+                warlordsEntity.getCooldownManager().addCooldown(new RegularCooldown<>(
+                        getStringName(),
+                        null,
+                        Boost.class,
+                        null,
+                        warlordsEntity,
+                        CooldownTypes.SPEC_BOOST,
+                        cM -> {},
+                        stunTicks
+                ) {
+                    @Override
+                    public float modifyDamageBeforeInterveneFromAttacker(WarlordsDamageHealingEvent event, float currentDamageValue) {
+                        return currentDamageValue * AbstractAbility.convertToMultiplicationDecimal(damageIncrease);
+                    }
+                });
             });
         }
+
 
     }
 
