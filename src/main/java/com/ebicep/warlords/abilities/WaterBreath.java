647e6777020bf3a9fe354c619b000239bb588e6f
ebicep <ebicep314@gmail.com>
2025-06-03 01:34:06 -0400

    feat: removed WaterBreath removing slowness

--- src/main/java/com/ebicep/warlords/abilities/WaterBreath.java
+++ src/main/java/com/ebicep/warlords/abilities/WaterBreath.java
@@ -95,7 +95,6 @@ public class WaterBreath extends AbstractAbility implements RedAbilityIcon, CanR
         );
         int previousDebuffsRemoved = stats.debuffsRemoved;
         stats.debuffsRemoved += wp.getCooldownManager().removeDebuffCooldowns();
-        wp.getSpeed().removeNegativeModifiers();
         wp.addInstance(InstanceBuilder.healing().ability(this).source(wp).value(healingValues.breathHealing));
         Location playerEyeLoc = new LocationBuilder(wp.getLocation()).pitch(0).backward(1);
         Vector viewDirection = playerLoc.getDirection();
@@ -108,7 +107,6 @@ public class WaterBreath extends AbstractAbility implements RedAbilityIcon, CanR
             if (wp.isTeammate(breathTarget)) {
                 stats.targetsHealed++;
                 stats.debuffsRemoved += breathTargetCooldownManager.removeDebuffCooldowns();
-                breathTarget.getSpeed().removeNegativeModifiers();
                 breathTarget.addInstance(InstanceBuilder.healing().ability(this).source(wp).value(healingValues.breathHealing).flags(InstanceFlags.CAN_OVERHEAL_OTHERS));
                 Overheal.giveOverHeal(wp, breathTarget);
                 if (pveMasterUpgrade || pveMasterUpgrade2) {
