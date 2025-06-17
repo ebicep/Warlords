603121f6545ef9dfb36cc1189a1220c0b5557a14
ebicep <ebicep314@gmail.com>
2025-06-17 12:05:41 -0400

    feat: ChillyAura stacking slowness

--- src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/ChillyAura.java
+++ src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/ChillyAura.java
@@ -1,6 +1,7 @@
 package com.ebicep.warlords.player.general.specboosts.boosts;
 
 import com.ebicep.warlords.abilities.IceBarrier;
+import com.ebicep.warlords.abilities.internal.AbstractAbility;
 import com.ebicep.warlords.events.player.ingame.WarlordsAddCooldownEvent;
 import com.ebicep.warlords.player.general.specboosts.SpecBoostManager;
 import com.ebicep.warlords.player.ingame.WarlordsEntity;
@@ -8,6 +9,8 @@ import com.ebicep.warlords.player.ingame.WarlordsPlayer;
 import com.ebicep.warlords.player.ingame.cooldowns.AbstractCooldown;
 import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.RegularCooldown;
 import com.ebicep.warlords.player.ingame.instances.InstanceBuilder;
+import com.ebicep.warlords.player.ingame.motionsystem.MotionModifierBuilder;
+import com.ebicep.warlords.player.ingame.motionsystem.speed.ConditionalStackValueModifier;
 import com.ebicep.warlords.util.warlords.PlayerFilter;
 import org.bukkit.event.EventHandler;
 
@@ -76,6 +79,14 @@ public class ChillyAura implements SpecBoostManager.SpecBoost<ChillyAura> {
                                 .aliveEnemiesOf(warlordsEntity)
                                 .forEach(we -> {
                                     we.addSpeedModifier(warlordsEntity, getStringName(), -slowAmountPercent, 6);
+                                    we.addSpeedModifier(new MotionModifierBuilder()
+                                            .setFrom(warlordsEntity)
+                                            .setName(getStringName())
+                                            .setModifier(-slowAmountPercent)
+                                            .setDuration(6)
+                                            .addAddons(new ConditionalStackValueModifier(AbstractAbility.convertToDivisionDecimal(slowAmountPercent)))
+                                            .build()
+                                    );
                                     float damage = we.getMaxHealth() * healthLossPercent / 100;
                                     we.addInstance(InstanceBuilder
                                             .damage()
