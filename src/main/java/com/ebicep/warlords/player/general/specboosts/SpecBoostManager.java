704f6b762216db8099130a78eade47bede827abd
ebicep <ebicep314@gmail.com>
2025-06-20 22:16:41 -0400

    feat: spec boost HouseOfLife

--- src/main/java/com/ebicep/warlords/player/general/specboosts/SpecBoostManager.java
+++ src/main/java/com/ebicep/warlords/player/general/specboosts/SpecBoostManager.java
@@ -57,6 +57,7 @@ public class SpecBoostManager {
     public static final SpecBoost<Haze> HAZE = new Haze();
     public static final SpecBoost<HealingLink> HEALING_LINK = new HealingLink();
     public static final SpecBoost<HeroicIntervention> HEROIC_INTERVENTION = new HeroicIntervention();
+    public static final SpecBoost<HouseOfLife> HOUSE_OF_LIFE = new HouseOfLife();
     public static final SpecBoost<IceBlock> ICE_BLOCK = new IceBlock();
     public static final SpecBoost<LustrousCrown> LUSTROUS_CROWN = new LustrousCrown();
     public static final SpecBoost<MarkedForDeath> MARKED_FOR_DEATH = new MarkedForDeath();
@@ -114,7 +115,7 @@ public class SpecBoostManager {
         SPEC_BOOSTS.put(Specializations.ASSASSIN, List.of(TRICKSTER, BLINK, RIFT_AMBUSH, TORRENTIAL_SOUL, HAZE));
         SPEC_BOOSTS.put(Specializations.VINDICATOR, List.of(ABYSSAL_GRASP, VITAL_PULSE, SANCTION_BURST, RIGHTEOUS_RAMPAGE, SWIFT_JUSTICE));
         SPEC_BOOSTS.put(Specializations.APOTHECARY, List.of(AURA_OF_RESTORATION, ALCHEMISTS_FURY, SUSTAINED_ONSLAUGHT, DETONATION_CATALYST, SUPER_BREW));
-        SPEC_BOOSTS.put(Specializations.CONJURER, List.of(SHARP_FANGS));
+        SPEC_BOOSTS.put(Specializations.CONJURER, List.of(SHARP_FANGS, HOUSE_OF_LIFE));
     }
 
     public static List<SpecBoost<?>> getSpecBoosts(Specializations specializations) {
---
+++ src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/HouseOfLife.java
@@ -0,0 +1,59 @@
+package com.ebicep.warlords.player.general.specboosts.boosts;
+
+import com.ebicep.warlords.abilities.EnergySeerConjurer;
+import com.ebicep.warlords.player.general.specboosts.SpecBoostManager;
+import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+
+import java.util.List;
+
+public class HouseOfLife implements SpecBoostManager.SpecBoost<HouseOfLife> {
+
+    private int healthIncrease;
+    private int energyIncrease;
+    private float energySeerHealingMultiplier;
+
+    @Override
+    public void init() {
+        this.healthIncrease = getValue("healthIncrease", int.class);
+        this.energyIncrease = getValue("energyIncrease", int.class);
+        this.energySeerHealingMultiplier = getValue("energySeerHealingMultiplier", float.class);
+    }
+
+    @Override
+    public String getConfigFieldName() {
+        return "houseOfLife";
+    }
+
+    @Override
+    public List<Object> getVariables() {
+        return List.of(healthIncrease, energyIncrease, energySeerHealingMultiplier);
+    }
+
+    @Override
+    public SpecBoostManager.Boost create() {
+        return new Boost();
+    }
+
+    @Override
+    public HouseOfLife get() {
+        return this;
+    }
+
+    public class Boost implements SpecBoostManager.Boost {
+
+
+        @Override
+        public void apply(WarlordsPlayer warlordsPlayer) {
+            warlordsPlayer.getHealth().addAdditiveModifier("Spec Boost (Base)", healthIncrease);
+            warlordsPlayer.getEnergy().addMultiplicativeModifierMult("Spec Boost", energyIncrease);
+
+            warlordsPlayer.getAbilitiesMatching(EnergySeerConjurer.class).forEach(energySeer -> {
+                energySeer.getHealValues().getSeerHealingMultiplier().forEachValue(floatModifiable ->
+                        floatModifiable.addOverridingModifier("Spec Boost", energySeerHealingMultiplier / 100)
+                );
+            });
+        }
+
+    }
+
+}
