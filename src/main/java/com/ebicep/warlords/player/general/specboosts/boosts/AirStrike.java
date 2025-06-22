3d118a2761f85508da3b9bf54ba8e02455ca6fdd
ebicep <ebicep314@gmail.com>
2025-06-22 14:27:16 -0400

    fix: AirStrike when dying

--- src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/AirStrike.java
+++ src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/AirStrike.java
@@ -12,7 +12,6 @@ import com.ebicep.warlords.player.ingame.WarlordsPlayer;
 import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
 import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.RegularCooldown;
 import com.ebicep.warlords.util.java.MathUtils;
-import com.ebicep.warlords.util.warlords.GameRunnable;
 import com.ebicep.warlords.util.warlords.modifiablevalues.FloatModifiable;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -21,6 +20,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class AirStrike implements SpecBoostManager.SpecBoost<AirStrike> {
@@ -75,8 +75,6 @@ public class AirStrike implements SpecBoostManager.SpecBoost<AirStrike> {
     public class Boost implements SpecBoostManager.Boost {
 
         private WarlordsEntity warlordsEntity;
-        private boolean inAir = false;
-        private boolean rising = false;
 
         @Override
         public void apply(WarlordsPlayer warlordsPlayer) {
@@ -84,51 +82,130 @@ public class AirStrike implements SpecBoostManager.SpecBoost<AirStrike> {
         }
 
         @EventHandler
-        public void onHorseActivate(WarlordsPlayerHorseEvent event) {
-            if (event.getWarlordsEntity().equals(warlordsEntity) && inAir) {
-                event.setCancelled(true);
-            }
-        }
-
-        @EventHandler
-        public void onWarlordsAbilityActivatePreEvent(WarlordsAbilityActivateEvent.Pre event) {
-            if (warlordsEntity.equals(event.getWarlordsEntity()) && rising) {
-                event.setCancelled(true);
-            }
-        }
-
-        @EventHandler
         public void onWarlordsAbilityActivatePostEvent(WarlordsAbilityActivateEvent.Post event) {
             if (!warlordsEntity.equals(event.getWarlordsEntity())) {
                 return;
             }
-            if (event.getAbility() instanceof AstralPlague) {
-                inAir = true;
-                rising = true;
-                Location location = warlordsEntity.getLocation();
-                Location targetLoc = location.clone();
-                for (int i = 0; i < ascendHeight; i++) {
-                    if (targetLoc.clone().add(0, 2, 0).getBlock().getType() == Material.AIR) {
-                        targetLoc.add(0, 1, 0);
-                    }
+            if (!(event.getAbility() instanceof AstralPlague)) {
+                return;
+            }
+            Location location = warlordsEntity.getLocation();
+            Location targetLoc = location.clone();
+            for (int i = 0; i < ascendHeight; i++) {
+                if (targetLoc.clone().add(0, 2, 0).getBlock().getType() == Material.AIR) {
+                    targetLoc.add(0, 1, 0);
                 }
-                int maxCasts = warlordsEntity.getAbilitiesMatching(SoulfireBeam.class)
-                                             .stream()
-                                             .anyMatch(soulfireBeam -> !soulfireBeam.anyCharges()) ?
-                               maxBeamCastsOnCooldown :
-                               maxBeamCastsNormal;
-
-                new GameRunnable(warlordsEntity.getGame()) {
-
-                    int ticksElapsed = 0;
-
-                    @Override
-                    public void run() {
-                        ticksElapsed++;
+            }
+            int maxCasts = warlordsEntity.getAbilitiesMatching(SoulfireBeam.class)
+                                         .stream()
+                                         .anyMatch(soulfireBeam -> !soulfireBeam.anyCharges()) ?
+                           maxBeamCastsOnCooldown :
+                           maxBeamCastsNormal;
+            warlordsEntity.getCooldownManager().addCooldown(new RegularCooldown<>(
+                    getStringName(),
+                    "AIR",
+                    Boost.class,
+                    null,
+                    warlordsEntity,
+                    CooldownTypes.SPEC_BOOST,
+                    cooldownManager -> {
                         if (warlordsEntity.isDead()) {
-                            this.cancel();
+                            if (warlordsEntity.getEntity() instanceof Player player) {
+                                player.setAllowFlight(false);
+                                player.setFlying(false);
+                                player.setFlySpeed(0.15f);
+                            }
                             return;
                         }
+                        List<FloatModifiable.FloatModifier> modifiers = new ArrayList<>();
+                        warlordsEntity.getAbilitiesMatching(SoulfireBeam.class).forEach(soulfireBeam -> {
+                            modifiers.add(soulfireBeam.getCooldown().addOverridingModifier(getStringName(), 0, airStrikeDurationTicks));
+                            modifiers.add(soulfireBeam.getMaxDistance().addOverridingModifier(getStringName(), soulfireBeamMaxRange, airStrikeDurationTicks));
+                            soulfireBeam.setCurrentCooldown(0);
+                        });
+                        warlordsEntity.getCooldownManager().addCooldown(new RegularCooldown<>(
+                                getStringName(),
+                                "STRIKE",
+                                Boost.class,
+                                null,
+                                warlordsEntity,
+                                CooldownTypes.SPEC_BOOST,
+                                cooldownManager1 -> {
+                                    warlordsEntity.teleportLocationOnly(location);
+                                },
+                                cooldownManager1 -> {
+                                    modifiers.forEach(FloatModifiable.FloatModifier::forceEnd);
+                                    if (warlordsEntity.getEntity() instanceof Player player) {
+                                        player.setAllowFlight(false);
+                                        player.setFlying(false);
+                                        player.setFlySpeed(0.15f);
+                                    }
+                                    warlordsEntity.getAbilitiesMatching(SoulfireBeam.class).forEach(soulfireBeam -> {
+                                        soulfireBeam.getCooldown().tick();
+                                        soulfireBeam.useAbility();
+                                    });
+                                },
+                                airStrikeDurationTicks
+                        ) {
+
+                            @Override
+                            protected Listener getListener() {
+                                RegularCooldown<Boost> regularCooldown = this;
+                                return new Listener() {
+
+                                    int casts = 0;
+
+                                    @EventHandler
+                                    public void onWarlordsAbilityActivatePreEvent(WarlordsAbilityActivateEvent.Pre event) {
+                                        if (isMarkedForRemoval()) {
+                                            return;
+                                        }
+                                        if (!warlordsEntity.equals(event.getWarlordsEntity())) {
+                                            return;
+                                        }
+                                        if (!(event.getAbility() instanceof SoulfireBeam)) {
+                                            event.setCancelled(true);
+                                        }
+                                    }
+
+                                    @EventHandler
+                                    public void onWarlordsAbilityActivatePreEvent(WarlordsAbilityActivateEvent.Post event) {
+                                        if (isMarkedForRemoval()) {
+                                            return;
+                                        }
+                                        if (!warlordsEntity.equals(event.getWarlordsEntity())) {
+                                            return;
+                                        }
+                                        if (event.getAbility() instanceof SoulfireBeam) {
+                                            casts++;
+                                            if (casts >= maxCasts) {
+                                                warlordsEntity.getCooldownManager().removeCooldown(regularCooldown);
+                                                modifiers.forEach(FloatModifiable.FloatModifier::forceEnd);
+                                                return;
+                                            }
+                                        }
+                                    }
+
+                                    @EventHandler
+                                    public void onHorseActivate(WarlordsPlayerHorseEvent event) {
+                                        if (event.getWarlordsEntity().equals(warlordsEntity)) {
+                                            event.setCancelled(true);
+                                        }
+                                    }
+                                };
+                            }
+
+                            @Override
+                            public float modifyDamageBeforeInterveneFromAttacker(WarlordsDamageHealingEvent event, float currentDamageValue) {
+                                if (event.getAbility() instanceof SoulfireBeam) {
+                                    return currentDamageValue * AbstractAbility.convertToDivisionDecimal(soulfireBeamDamageReductionPercent);
+                                }
+                                return currentDamageValue;
+                            }
+                        });
+                    },
+                    ascendDurationTicks,
+                    Collections.singletonList((cooldown, ticksLeft, ticksElapsed) -> {
                         float ratio = (float) ticksElapsed / ascendDurationTicks;
                         if (warlordsEntity.getEntity() instanceof Player player) {
                             player.setAllowFlight(true);
@@ -143,95 +220,27 @@ public class AirStrike implements SpecBoostManager.SpecBoost<AirStrike> {
                                 targetLoc.getPitch()
                         );
                         warlordsEntity.teleportLocationOnly(newLocation);
-                        if (ratio >= 1) {
-                            this.cancel();
-                            rising = false;
-
-                            List<FloatModifiable.FloatModifier> modifiers = new ArrayList<>();
-                            warlordsEntity.getAbilitiesMatching(SoulfireBeam.class).forEach(soulfireBeam -> {
-                                modifiers.add(soulfireBeam.getCooldown().addOverridingModifier(getStringName(), 0, airStrikeDurationTicks));
-                                modifiers.add(soulfireBeam.getMaxDistance().addOverridingModifier(getStringName(), soulfireBeamMaxRange, airStrikeDurationTicks));
-                                soulfireBeam.setCurrentCooldown(0);
-                            });
-                            RegularCooldown<Boost> cooldown = new RegularCooldown<>(
-                                    getStringName(),
-                                    "AIR",
-                                    Boost.class,
-                                    null,
-                                    warlordsEntity,
-                                    CooldownTypes.SPEC_BOOST,
-                                    cooldownManager -> {
-                                    },
-                                    cooldownManager -> {
-                                        inAir = false;
-                                        modifiers.forEach(FloatModifiable.FloatModifier::forceEnd);
-                                        if (warlordsEntity.getEntity() instanceof Player player) {
-                                            player.setAllowFlight(false);
-                                            player.setFlying(false);
-                                            player.setFlySpeed(0.15f);
-                                        }
-                                        warlordsEntity.teleportLocationOnly(location);
-                                        warlordsEntity.getAbilitiesMatching(SoulfireBeam.class).forEach(soulfireBeam -> {
-                                            soulfireBeam.getCooldown().tick();
-                                            soulfireBeam.useAbility();
-                                        });
-                                    },
-                                    airStrikeDurationTicks
-                            ) {
-
-                                @Override
-                                protected Listener getListener() {
-                                    RegularCooldown<Boost> regularCooldown = this;
-                                    return new Listener() {
-
-                                        int casts = 0;
-
-                                        @EventHandler
-                                        public void onWarlordsAbilityActivatePreEvent(WarlordsAbilityActivateEvent.Pre event) {
-                                            if (isMarkedForRemoval()) {
-                                                return;
-                                            }
-                                            if (!warlordsEntity.equals(event.getWarlordsEntity())) {
-                                                return;
-                                            }
-                                            if (!(event.getAbility() instanceof SoulfireBeam)) {
-                                                event.setCancelled(true);
-                                            }
-                                        }
-
-                                        @EventHandler
-                                        public void onWarlordsAbilityActivatePreEvent(WarlordsAbilityActivateEvent.Post event) {
-                                            if (isMarkedForRemoval()) {
-                                                return;
-                                            }
-                                            if (!warlordsEntity.equals(event.getWarlordsEntity())) {
-                                                return;
-                                            }
-                                            if (event.getAbility() instanceof SoulfireBeam) {
-                                                casts++;
-                                                if (casts >= maxCasts) {
-                                                    warlordsEntity.getCooldownManager().removeCooldown(regularCooldown);
-                                                    modifiers.forEach(FloatModifiable.FloatModifier::forceEnd);
-                                                    return;
-                                                }
-                                            }
-                                        }
-                                    };
-                                }
-
-                                @Override
-                                public float modifyDamageBeforeInterveneFromAttacker(WarlordsDamageHealingEvent event, float currentDamageValue) {
-                                    if (event.getAbility() instanceof SoulfireBeam) {
-                                        return currentDamageValue * AbstractAbility.convertToDivisionDecimal(soulfireBeamDamageReductionPercent);
-                                    }
-                                    return currentDamageValue;
-                                }
-                            };
-                            warlordsEntity.getCooldownManager().addCooldown(cooldown);
+                    })
+            ) {
+                @Override
+                protected Listener getListener() {
+                    return new Listener() {
+                        @EventHandler
+                        public void onHorseActivate(WarlordsPlayerHorseEvent event) {
+                            if (event.getWarlordsEntity().equals(warlordsEntity)) {
+                                event.setCancelled(true);
+                            }
                         }
-                    }
-                }.runTaskTimer(0, 1);
-            }
+
+                        @EventHandler
+                        public void onWarlordsAbilityActivatePreEvent(WarlordsAbilityActivateEvent.Pre event) {
+                            if (warlordsEntity.equals(event.getWarlordsEntity())) {
+                                event.setCancelled(true);
+                            }
+                        }
+                    };
+                }
+            });
         }
 
     }
