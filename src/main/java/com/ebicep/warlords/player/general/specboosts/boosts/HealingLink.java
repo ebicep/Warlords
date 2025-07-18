8f6a3ac4423035bfaf4fb53441558df2e8561e04
Chessking345 <chessking345@gmail.com>
2025-07-18 10:01:14 -0700

    feat: Healing Link: Ground Slam cooldown reduction 0s > 1.5s

--- src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/HealingLink.java
+++ src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/HealingLink.java
@@ -1,5 +1,6 @@
 package com.ebicep.warlords.player.general.specboosts.boosts;
 
+import com.ebicep.warlords.abilities.GroundSlamRevenant;
 import com.ebicep.warlords.abilities.RecklessCharge;
 import com.ebicep.warlords.abilities.internal.AbstractAbility;
 import com.ebicep.warlords.player.general.specboosts.SpecBoostManager;
@@ -10,8 +11,11 @@ import java.util.List;
 
 public class HealingLink implements SpecBoostManager.SpecBoost<HealingLink> {
 
+    private int groundSlamCooldownReductionTicks;
+
     @Override
     public void init() {
+        this.groundSlamCooldownReductionTicks = getValue("groundSlamCooldownReductionTicks", int.class);
     }
 
     @Override
@@ -26,7 +30,7 @@ public class HealingLink implements SpecBoostManager.SpecBoost<HealingLink> {
 
     @Override
     public List<Object> getVariables() {
-        return List.of();
+        return List.of(groundSlamCooldownReductionTicks);
     }
 
     @Override
@@ -50,6 +54,8 @@ public class HealingLink implements SpecBoostManager.SpecBoost<HealingLink> {
                     com.ebicep.warlords.abilities.HealingLink healingLink = new com.ebicep.warlords.abilities.HealingLink();
                     healingLink.init(healingLink.getBuilder());
                     abilities.set(i, healingLink);
+                } else if (ability instanceof GroundSlamRevenant groundSlam) {
+                    groundSlam.getCooldown().addAdditiveModifier("Spec Boost", -groundSlamCooldownReductionTicks);
                 }
             }
             warlordsPlayer.resetAbilityTree();
