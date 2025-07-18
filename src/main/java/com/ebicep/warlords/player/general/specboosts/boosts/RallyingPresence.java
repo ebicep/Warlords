2c0291df850524ab072c0a2ac2851d6f2e77fd2d
Chessking345 <chessking345@gmail.com>
2025-07-18 09:11:21 -0700

    feat: Rallying Presence: Duration decrease 1s > 2s, EPS increase 9 > 6. Now gives 30% speed, 15% kb resistance, and +5 energy from Crusader's Strike while holding flag

--- src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/RallyingPresence.java
+++ src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/RallyingPresence.java
@@ -1,8 +1,15 @@
 package com.ebicep.warlords.player.general.specboosts.boosts;
 
+import com.ebicep.warlords.abilities.CrusadersStrike;
 import com.ebicep.warlords.abilities.InspiringPresence;
+import com.ebicep.warlords.events.game.WarlordsFlagUpdatedEvent;
+import com.ebicep.warlords.events.player.ingame.WarlordsAbilityActivateEvent;
+import com.ebicep.warlords.game.flags.PlayerFlagLocation;
 import com.ebicep.warlords.player.general.specboosts.SpecBoostManager;
+import com.ebicep.warlords.player.ingame.WarlordsEntity;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.player.ingame.motionsystem.MotionModifierBuilder;
+import org.bukkit.event.EventHandler;
 
 import java.util.List;
 
@@ -11,12 +18,18 @@ public class RallyingPresence implements SpecBoostManager.SpecBoost<RallyingPres
     private int durationDecreaseTicks;
     private int energyPerSecondIncrease;
     private int speedIncreasePercent;
+    private float flagSpeedIncreasePercent;
+    private float flagKnockbackResistancePercent;
+    private int crusaderStrikeEnergyIncrease;
 
     @Override
     public void init() {
         this.durationDecreaseTicks = getValue("durationDecreaseTicks", int.class);
         this.energyPerSecondIncrease = getValue("energyPerSecondIncrease", int.class);
         this.speedIncreasePercent = getValue("speedIncreasePercent", int.class);
+        this.flagSpeedIncreasePercent = getValue("flagSpeedIncreasePercent", float.class);
+        this.flagKnockbackResistancePercent = getValue("flagKnockbackResistancePercent", float.class);
+        this.crusaderStrikeEnergyIncrease = getValue("crusaderStrikeEnergyIncrease", int.class);
     }
 
     @Override
@@ -26,7 +39,7 @@ public class RallyingPresence implements SpecBoostManager.SpecBoost<RallyingPres
 
     @Override
     public List<Object> getVariables() {
-        return List.of(durationDecreaseTicks, energyPerSecondIncrease, speedIncreasePercent);
+        return List.of(durationDecreaseTicks, energyPerSecondIncrease, speedIncreasePercent, flagSpeedIncreasePercent, flagKnockbackResistancePercent, crusaderStrikeEnergyIncrease);
     }
 
     @Override
@@ -41,13 +54,43 @@ public class RallyingPresence implements SpecBoostManager.SpecBoost<RallyingPres
 
     public class Boost implements SpecBoostManager.Boost {
 
+        private WarlordsEntity warlordsEntity;
+
         @Override
         public void apply(WarlordsPlayer warlordsPlayer) {
+            this.warlordsEntity = warlordsPlayer;
             warlordsPlayer.getAbilitiesMatching(InspiringPresence.class).forEach(inspiringPresence -> {
                 inspiringPresence.setTickDuration(inspiringPresence.getTickDuration() - durationDecreaseTicks);
                 inspiringPresence.setEnergyPerSecond(inspiringPresence.getEnergyPerSecond() + energyPerSecondIncrease);
                 inspiringPresence.setSpeedBuff(inspiringPresence.getSpeedBuff() + speedIncreasePercent);
             });
+        }
+
+        @EventHandler
+        public void onWarlordsAbilityActivate(WarlordsAbilityActivateEvent.Pre event) {
+            if (!warlordsEntity.equals(event.getWarlordsEntity())) {
+                return;
+            }
+            if (warlordsEntity.hasFlag() && event.getAbility() instanceof CrusadersStrike crusadersStrike) {
+                crusadersStrike.setEnergyGiven(crusadersStrike.getEnergyGiven() + crusaderStrikeEnergyIncrease);
+            }
+        }
+
+        @EventHandler
+        public void onWarlordsFlagUpdated(WarlordsFlagUpdatedEvent event) {
+            if (event.getNew() instanceof PlayerFlagLocation playerFlagLocation && playerFlagLocation.getPlayer().equals(warlordsEntity)) {
+                warlordsEntity.getKnockback().addModifier(new MotionModifierBuilder()
+                        .setFrom(warlordsEntity)
+                        .setName(getStringName())
+                        .setModifier(-flagKnockbackResistancePercent)
+                        .setDuration(-1)
+                        .build()
+                );
+                warlordsEntity.addSpeedModifier(warlordsEntity, getStringName(), flagSpeedIncreasePercent, -1);
+            } else if (event.getOld() instanceof PlayerFlagLocation playerFlagLocation && playerFlagLocation.getPlayer().equals(warlordsEntity)) {
+                warlordsEntity.getKnockback().removeModifier(getStringName());
+                warlordsEntity.getSpeed().removeModifier(getStringName());
+            }
         }
 
     }
