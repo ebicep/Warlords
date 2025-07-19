c98978b7f0f659c13fab56e7847eff97fddb7a08
Ice16 <79466329+Ice16-3602@users.noreply.github.com>
2025-07-19 15:00:42 -0400

    fix: rename to STORM_WIND_2

--- src/main/java/com/ebicep/warlords/game/GameMap.java
+++ src/main/java/com/ebicep/warlords/game/GameMap.java
@@ -55,7 +55,7 @@ public abstract class GameMap {
     public static final GameMap SCORCHED = new Scorched();
     public static final GameMap SIEGE = new Siege();
     public static final GameMap STORM_WIND = new StormWind();
-    public static final GameMap STORM_WIND2 = new StormWind2();
+    public static final GameMap STORM_WIND_2 = new StormWind2();
     public static final GameMap SUN_AND_MOON = new SunAndMoon();
     public static final GameMap TARTARUS = new Tartarus();
     public static final GameMap THE_OBSIDIAN_TRAIL_RAID = new TheObsidianTrailRaid();
@@ -106,7 +106,7 @@ public abstract class GameMap {
             SCORCHED,
             SIEGE,
             STORM_WIND,
-            STORM_WIND2,
+            STORM_WIND_2,
             SUN_AND_MOON,
             TARTARUS,
             THE_OBSIDIAN_TRAIL_RAID,
