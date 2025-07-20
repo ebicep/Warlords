1b7837ef08f45e2523239c6fd32d119c99b9a48d
Chessking345 <chessking345@gmail.com>
2025-07-20 12:02:20 -0700

    revert: Rallying Presence: extra ally

--- src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/RallyingPresence.java
+++ src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/RallyingPresence.java
@@ -20,7 +20,6 @@ public class RallyingPresence implements SpecBoostManager.SpecBoost<RallyingPres
     private float flagSpeedIncreasePercent;
     private float flagKnockbackResistancePercent;
     private int crusaderStrikeEnergyIncrease;
-    private int crusaderStrikeAllyIncrease;
 
     @Override
     public void init() {
@@ -30,7 +29,6 @@ public class RallyingPresence implements SpecBoostManager.SpecBoost<RallyingPres
         this.flagSpeedIncreasePercent = getValue("flagSpeedIncreasePercent", float.class);
         this.flagKnockbackResistancePercent = getValue("flagKnockbackResistancePercent", float.class);
         this.crusaderStrikeEnergyIncrease = getValue("crusaderStrikeEnergyIncrease", int.class);
-        this.crusaderStrikeAllyIncrease = getValue("crusaderStrikeAllyIncrease", int.class);
     }
 
     @Override
@@ -46,8 +44,7 @@ public class RallyingPresence implements SpecBoostManager.SpecBoost<RallyingPres
                 speedIncreasePercent,
                 flagSpeedIncreasePercent,
                 flagKnockbackResistancePercent,
-                crusaderStrikeEnergyIncrease,
-                crusaderStrikeAllyIncrease);
+                crusaderStrikeEnergyIncrease);
     }
 
     @Override
@@ -87,14 +84,12 @@ public class RallyingPresence implements SpecBoostManager.SpecBoost<RallyingPres
                 warlordsEntity.addSpeedModifier(warlordsEntity, getStringName(), flagSpeedIncreasePercent, -1);
                 warlordsEntity.getAbilitiesMatching(CrusadersStrike.class).forEach(crusadersStrike -> {
                     crusadersStrike.setEnergyGiven(crusadersStrike.getEnergyGiven() + crusaderStrikeEnergyIncrease);
-                    crusadersStrike.setEnergyMaxAllies(crusadersStrike.getEnergyMaxAllies() + crusaderStrikeAllyIncrease);
                 });
             } else if (event.getOld() instanceof PlayerFlagLocation playerFlagLocation && playerFlagLocation.getPlayer().equals(warlordsEntity)) {
                 warlordsEntity.getKnockback().removeModifier(getStringName());
                 warlordsEntity.getSpeed().removeModifier(getStringName());
                 warlordsEntity.getAbilitiesMatching(CrusadersStrike.class).forEach(crusadersStrike -> {
                     crusadersStrike.setEnergyGiven(crusadersStrike.getEnergyGiven() - crusaderStrikeEnergyIncrease);
-                    crusadersStrike.setEnergyMaxAllies(crusadersStrike.getEnergyMaxAllies() - crusaderStrikeAllyIncrease);
                 });
             }
         }
