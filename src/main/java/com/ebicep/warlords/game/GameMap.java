4a56ad8d505a5f7c98abafe733ed5a0a500eae32
Ice16 <79466329+Ice16-3602@users.noreply.github.com>
2025-07-20 06:54:42 +0000

    feat: add stormwind2 gamemap

--- src/main/java/com/ebicep/warlords/game/GameMap.java
+++ src/main/java/com/ebicep/warlords/game/GameMap.java
@@ -55,6 +55,7 @@ public abstract class GameMap {
     public static final GameMap SCORCHED = new Scorched();
     public static final GameMap SIEGE = new Siege();
     public static final GameMap STORM_WIND = new StormWind();
+    public static final GameMap STORM_WIND_2 = new StormWind2();
     public static final GameMap SUN_AND_MOON = new SunAndMoon();
     public static final GameMap SUN_AND_MOON_2 = new SunAndMoon2();
     public static final GameMap TARTARUS = new Tartarus();
@@ -106,6 +107,7 @@ public abstract class GameMap {
             SCORCHED,
             SIEGE,
             STORM_WIND,
+            STORM_WIND_2,
             SUN_AND_MOON,
             SUN_AND_MOON_2,
             TARTARUS,
