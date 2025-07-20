e99e28e4b999bdfbe079704244980f095bb82834
Chessking345 <chessking345@gmail.com>
2025-07-19 17:03:36 -0700

    fix: Sun and Moon name

--- src/main/java/com/ebicep/warlords/game/GameMap.java
+++ src/main/java/com/ebicep/warlords/game/GameMap.java
@@ -56,7 +56,7 @@ public abstract class GameMap {
     public static final GameMap SIEGE = new Siege();
     public static final GameMap STORM_WIND = new StormWind();
     public static final GameMap SUN_AND_MOON = new SunAndMoon();
-    public static final GameMap SUN_AND_MOON2 = new SunAndMoon2();
+    public static final GameMap SUN_AND_MOON_2 = new SunAndMoon2();
     public static final GameMap TARTARUS = new Tartarus();
     public static final GameMap THE_OBSIDIAN_TRAIL_RAID = new TheObsidianTrailRaid();
     public static final GameMap TREASURE_HUNT = new TreasureHunt();
@@ -107,7 +107,7 @@ public abstract class GameMap {
             SIEGE,
             STORM_WIND,
             SUN_AND_MOON,
-            SUN_AND_MOON2,
+            SUN_AND_MOON_2,
             TARTARUS,
             THE_OBSIDIAN_TRAIL_RAID,
             TREASURE_HUNT,
