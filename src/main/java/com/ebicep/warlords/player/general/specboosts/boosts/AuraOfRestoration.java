fe5ecee26af392a12c03d3b93130c350023afba6
Chessking345 <chessking345@gmail.com>
2025-07-24 23:19:27 -0700

    fix: Aura of Restoration: Add percents

--- src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/AuraOfRestoration.java
+++ src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/AuraOfRestoration.java
@@ -19,13 +19,13 @@ import java.util.List;
 public class AuraOfRestoration implements SpecBoostManager.SpecBoost<AuraOfRestoration> {
 
     private float soothingElixirCooldownIncreaseSeconds;
-    private float puddleHealingIncrease;
+    private float puddleHealingIncreasePercent;
     private float remedicChainsBreakRadiusIncrease;
 
     @Override
     public void init() {
         this.soothingElixirCooldownIncreaseSeconds = getValue("soothingElixirCooldownIncreaseSeconds", float.class);
-        this.puddleHealingIncrease = getValue("puddleHealingIncrease", float.class);
+        this.puddleHealingIncreasePercent = getValue("puddleHealingIncreasePercent", float.class);
         this.remedicChainsBreakRadiusIncrease = getValue("remedicChainsBreakRadiusIncrease", float.class);
     }
 
@@ -38,7 +38,7 @@ public class AuraOfRestoration implements SpecBoostManager.SpecBoost<AuraOfResto
     public List<Object> getVariables() {
         return List.of(
                 soothingElixirCooldownIncreaseSeconds,
-                puddleHealingIncrease,
+                puddleHealingIncreasePercent,
                 remedicChainsBreakRadiusIncrease
         );
     }
@@ -63,7 +63,7 @@ public class AuraOfRestoration implements SpecBoostManager.SpecBoost<AuraOfResto
             warlordsPlayer.getAbilitiesMatching(SoothingElixir.class).forEach(soothingElixir -> {
                 soothingElixir.getCooldown().addAdditiveModifier("Spec Boost", soothingElixirCooldownIncreaseSeconds);
                 soothingElixir.getHealValues().getElixirDOTHealing()
-                              .forEachValue(floatModifiable -> floatModifiable.addMultiplicativeModifierAdd("Spec Boost", puddleHealingIncrease / 100));
+                              .forEachValue(floatModifiable -> floatModifiable.addMultiplicativeModifierAdd("Spec Boost", puddleHealingIncreasePercent / 100));
             });
             warlordsPlayer.getAbilitiesMatching(RemedicChains.class).forEach(remedicChains -> {
                 remedicChains.setLinkBreakRadius((int) (remedicChains.getLinkBreakRadius() + remedicChainsBreakRadiusIncrease));
