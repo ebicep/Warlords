6c6dc9136077d59c99c0eb794d43df121d5b44a1
Chessking345 <chessking345@gmail.com>
2025-08-16 11:44:01 -0700

    Revert "feat: Contagion: Remove energy per hit increase"
    This reverts commit a5ad14cf29378f2befd20690a8a964d6db3dea5c.

--- src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/Contagion.java
+++ src/main/java/com/ebicep/warlords/player/general/specboosts/boosts/Contagion.java
@@ -8,11 +8,13 @@ import java.util.List;
 
 public class Contagion implements SpecBoostManager.SpecBoost<Contagion> {
 
+    private int ephIncrease;
     private float facadeResistanceIncreasePercent;
     private int facadePhexStacksIncrease;
 
     @Override
     public void init() {
+        this.ephIncrease = getValue("ephIncrease", int.class);
         this.facadeResistanceIncreasePercent = getValue("facadeResistanceIncreasePercent", float.class);
         this.facadePhexStacksIncrease = getValue("facadePhexStacksIncrease", int.class);
     }
@@ -24,7 +26,7 @@ public class Contagion implements SpecBoostManager.SpecBoost<Contagion> {
 
     @Override
     public List<Object> getVariables() {
-        return List.of(facadeResistanceIncreasePercent, facadePhexStacksIncrease);
+        return List.of(ephIncrease, facadeResistanceIncreasePercent, facadePhexStacksIncrease);
     }
 
     @Override
@@ -41,6 +43,7 @@ public class Contagion implements SpecBoostManager.SpecBoost<Contagion> {
 
         @Override
         public void apply(WarlordsPlayer warlordsPlayer) {
+            warlordsPlayer.getEnergyPerHit().addAdditiveModifier("Spec Boost", ephIncrease);
             warlordsPlayer.getAbilitiesMatching(ContagiousFacade.class).forEach(contagiousFacade -> {
                 contagiousFacade.getDamageAbsorption().addAdditiveModifier("Spec Boost", facadeResistanceIncreasePercent);
                 contagiousFacade.setStacksGranted(contagiousFacade.getStacksGranted() + facadePhexStacksIncrease);
