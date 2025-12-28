9fb0bd43eb41318003a4dd40ca627556a3257880
Plikie <plikie341@gmail.com>
2025-12-28 23:03:03 +0100

    Implement Madrakan set

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Madrakan.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Madrakan.java
@@ -1,8 +1,14 @@
 package com.ebicep.warlords.pve.newitems.setbonus.sets;
 
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
+import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.PermanentCooldown;
+import com.ebicep.warlords.player.ingame.instances.InstanceBuilder;
+import com.ebicep.warlords.player.ingame.instances.InstanceFlags;
+import com.ebicep.warlords.player.ingame.instances.type.Modifier;
 import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
 import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
+import com.ebicep.warlords.util.warlords.modifiablevalues.FloatModifiable;
 
 import java.util.List;
 
@@ -37,6 +43,28 @@ public class Madrakan extends BaseSet {
 
         @Override
         public void apply(WarlordsPlayer warlordsPlayer) {
+            warlordsPlayer.getCooldownManager().addCooldown(new PermanentCooldown<>(
+                    "Madrakan",
+                    null,
+                    Madrakan.class,
+                    null,
+                    warlordsPlayer,
+                    CooldownTypes.ITEM,
+                    cooldownManager -> {},
+                    false
+            ).addModifier(
+                    Modifier.MODIFY_OUTGOING_CRIT_MULTIPLIER,
+                    (event, currentCritMultiplier) -> {
+                        if (disableCriticalHits) {
+                            currentCritMultiplier.addModifier(FloatModifiable.ModifierType.OVERRIDING, getName(), 0);
+                        }
+                    }
+            ).addModifier(
+                    Modifier.MODIFY_OUTGOING_DAMAGE_BEFORE_INTERVENE,
+                    (event, currentDamageValue) -> {
+                        currentDamageValue.addModifier(FloatModifiable.ModifierType.MULTIPLICATIVE_MULTIPLIER, getName(), 1 + (damageIncreasePercent / 100f));
+                    }
+            ));
             // Implementation for:
             // 1. Increasing the player's base damage by damageIncreasePercent.
             // 2. Setting the player's Crit Chance or Crit Multiplier to 0
