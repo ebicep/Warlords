3a748ee06db79e289ea0bfa93d70a6d153345280
Plikie <plikie341@gmail.com>
2025-12-28 21:59:36 +0100

    Implement Olympic set

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Olympic.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Olympic.java
@@ -1,8 +1,16 @@
 package com.ebicep.warlords.pve.newitems.setbonus.sets;
 
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
+import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.PermanentCooldown;
+import com.ebicep.warlords.player.ingame.instances.type.Modifier;
+import com.ebicep.warlords.pve.items.types.specialitems.buckler.delta.AerialAegis;
 import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
 import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
+import com.ebicep.warlords.util.warlords.GameRunnable;
+import com.ebicep.warlords.util.warlords.modifiablevalues.FloatModifiable;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.List;
 
@@ -37,8 +45,31 @@ public class Olympic extends BaseSet {
 
         @Override
         public void apply(WarlordsPlayer warlordsPlayer) {
-            // Implementation for jump height modification and 
-            // damage reduction while the player's in-air state is true
+            new GameRunnable(warlordsPlayer.getGame()) {
+                @Override
+                public void run() {
+                    warlordsPlayer.addPotionEffect(new PotionEffect(PotionEffectType.JUMP_BOOST, 120, jumpHeight, true, false));
+                }
+            }.runTaskTimer(0, 100);
+            warlordsPlayer.getCooldownManager().addCooldown(new PermanentCooldown<>(
+                    getName(),
+                    null,
+                    AerialAegis.class,
+                    null,
+                    warlordsPlayer,
+                    CooldownTypes.ITEM,
+                    cooldownManager -> {
+
+                    },
+                    false
+            ).addModifier(
+                    Modifier.MODIFY_INCOMING_DAMAGE_AFTER_INTERVENE,
+                    (event, currentDamageValue) -> {
+                        if (!warlordsPlayer.getEntity().isOnGround()) {
+                            currentDamageValue.addModifier(FloatModifiable.ModifierType.MULTIPLICATIVE_MULTIPLIER, getName(), airResist / 100f);
+                        }
+                    }
+            ));
         }
 
     }
