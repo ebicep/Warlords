a36ea519afc37e56ccb300864b1b872815644c6a
Plikie <plikie341@gmail.com>
2025-12-29 23:46:07 +0100

    Implement Ocean Light set

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/OceanLight.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/OceanLight.java
@@ -1,10 +1,17 @@
 package com.ebicep.warlords.pve.newitems.setbonus.sets;
 
+import com.ebicep.warlords.game.option.pve.PveOption;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
+import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.PermanentCooldown;
+import com.ebicep.warlords.player.ingame.instances.type.Modifier;
 import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
 import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
+import com.ebicep.warlords.util.warlords.modifiablevalues.FloatModifiable;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class OceanLight extends BaseSet {
 
@@ -35,8 +42,29 @@ public class OceanLight extends BaseSet {
 
         @Override
         public void apply(WarlordsPlayer warlordsPlayer) {
-            // Implementation to count active mobs on the field 
-            // and apply a healing multiplier based on healingPerMobPercent.
+            Optional<PveOption> pveOption = warlordsPlayer.getGame().getOption(PveOption.class)
+                    .stream()
+                    .findFirst();
+            warlordsPlayer.getCooldownManager().addCooldown(new PermanentCooldown<>(
+                    getName(),
+                    null,
+                    OceanLight.class,
+                    null,
+                    warlordsPlayer,
+                    CooldownTypes.ITEM,
+                    cooldownManager -> {
+                    },
+                    false
+            ).addModifier(
+                    Modifier.MODIFY_OUTGOING_HEALING,
+                    (event, currentHealValue) -> {
+                        AtomicInteger mobCount = new AtomicInteger();
+                        pveOption.ifPresent(option -> {
+                            mobCount.set(option.mobCount());
+                        });
+                        currentHealValue.addModifier(FloatModifiable.ModifierType.MULTIPLICATIVE_MULTIPLIER, getName(), 1 + (mobCount.get() * healingPerMobPercent / 100f));
+                    }
+            ));
         }
 
     }
