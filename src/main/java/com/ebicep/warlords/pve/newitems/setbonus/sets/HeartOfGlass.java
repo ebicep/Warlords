bf9a73dbd8ff979fe5ff9bc15970873f61a3d16b
Plikie <plikie341@gmail.com>
2025-12-30 00:01:31 +0100

    Implement Heart of Glass set

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/HeartOfGlass.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/HeartOfGlass.java
@@ -1,22 +1,24 @@
 package com.ebicep.warlords.pve.newitems.setbonus.sets;
 
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
+import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.PermanentCooldown;
+import com.ebicep.warlords.player.ingame.instances.type.Modifier;
 import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
 import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
+import com.ebicep.warlords.util.warlords.modifiablevalues.FloatModifiable;
 
 import java.util.List;
 
 public class HeartOfGlass extends BaseSet {
 
-    private int maxHealthReductionPercent;
-    private int damageReductionPenaltyPercent;
+    private int defenseStatsPenalty;
     private int damageIncreasePercent;
 
     @Override
     public void init() {
         super.init();
-        this.maxHealthReductionPercent = getValue("maxHealthReductionPercent", int.class);
-        this.damageReductionPenaltyPercent = getValue("damageReductionPenaltyPercent", int.class);
+        this.defenseStatsPenalty = getValue("defenseStatsPenalty", int.class);
         this.damageIncreasePercent = getValue("damageIncreasePercent", int.class);
     }
 
@@ -32,17 +34,34 @@ public class HeartOfGlass extends BaseSet {
 
     @Override
     public List<Object> getVariables() {
-        return List.of(maxHealthReductionPercent, damageReductionPenaltyPercent, damageIncreasePercent);
+        return List.of(defenseStatsPenalty, damageIncreasePercent);
     }
 
     public class Bonus implements SetBonus.Bonus {
 
         @Override
         public void apply(WarlordsPlayer warlordsPlayer) {
-            // Implementation for "Glass Cannon" mechanics:
-            // 1. Subtract maxHealthReductionPercent from the player's max health.
-            // 2. Reduce total damage reduction by damageReductionPenaltyPercent.
-            // 3. Apply a multiplicative damage increase of damageIncreasePercent.
+            warlordsPlayer.getHealth().addModifier(FloatModifiable.ModifierType.MULTIPLICATIVE_MULTIPLIER, getName(), 1 - defenseStatsPenalty / 100f);
+            warlordsPlayer.getCooldownManager().addCooldown(new PermanentCooldown<>(
+                    "Heart of Glass",
+                    null,
+                    HeartOfGlass.class,
+                    null,
+                    warlordsPlayer,
+                    CooldownTypes.ITEM,
+                    cooldownManager -> {},
+                    false
+            ).addModifier(
+                    Modifier.MODIFY_OUTGOING_DAMAGE_BEFORE_INTERVENE,
+                    (event, currentDamageValue) -> {
+                        currentDamageValue.addModifier(FloatModifiable.ModifierType.MULTIPLICATIVE_MULTIPLIER, getName(), 1 + (damageIncreasePercent / 100f));
+                    }
+            ).addModifier(
+                    Modifier.MODIFY_INCOMING_DAMAGE_AFTER_INTERVENE,
+                    (event, currentDamageValue) -> {
+                        currentDamageValue.addModifier(FloatModifiable.ModifierType.MULTIPLICATIVE_MULTIPLIER, getName(), 1 + (defenseStatsPenalty / 100f));
+                    }
+            ));
         }
 
     }
