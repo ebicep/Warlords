562e1f7c6a6147665844f50e0b71b56971f69910
Plikie <plikie341@gmail.com>
2026-01-02 00:22:34 +0100

    Implement Encumber item set

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Encumber.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Encumber.java
@@ -1,8 +1,12 @@
 package com.ebicep.warlords.pve.newitems.setbonus.sets;
 
+import com.ebicep.warlords.abilities.internal.AbstractAbility;
+import com.ebicep.warlords.abilities.internal.icon.BlueAbilityIcon;
+import com.ebicep.warlords.abilities.internal.icon.RedAbilityIcon;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
 import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
 import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
+import com.ebicep.warlords.util.warlords.modifiablevalues.FloatModifiable;
 
 import java.util.List;
 
@@ -35,7 +39,12 @@ public class Encumber extends BaseSet {
 
         @Override
         public void apply(WarlordsPlayer warlordsPlayer) {
-
+            for (AbstractAbility ability : warlordsPlayer.getAbilities()) {
+                if (ability instanceof BlueAbilityIcon) {
+                    ability.setMaxCharges(ability.getMaxCharges() + extraCharges);
+                    ability.setCurrentCharges(ability.getMaxCharges());
+                }
+            }
         }
 
     }
