8f87028fc3992672b4543222bf056b0b8f310b0c
Plikie <plikie341@gmail.com>
2026-01-01 19:51:01 +0100

    Implement Charm item set

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Charm.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Charm.java
@@ -1,8 +1,15 @@
 package com.ebicep.warlords.pve.newitems.setbonus.sets;
 
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
+import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.PermanentCooldown;
+import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.RegularCooldown;
+import com.ebicep.warlords.player.ingame.instances.type.Modifier;
+import com.ebicep.warlords.pve.items.types.specialitems.buckler.delta.CrossNecklaceCharm;
 import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
 import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
+import com.ebicep.warlords.util.warlords.PlayerFilter;
+import com.ebicep.warlords.util.warlords.modifiablevalues.FloatModifiable;
 
 import java.util.List;
 
@@ -39,7 +46,43 @@ public class Charm extends BaseSet {
 
         @Override
         public void apply(WarlordsPlayer warlordsPlayer) {
-            // Logic for aura effect: radius, damage increase, and slowness
+            warlordsPlayer.getCooldownManager().addCooldown(new PermanentCooldown<>(
+                    getName(),
+                    null,
+                    Charm.class,
+                    null,
+                    warlordsPlayer,
+                    CooldownTypes.ITEM,
+                    cooldownManager -> {
+                    },
+                    false,
+                    (cooldown, ticksElapsed) -> {
+                        if (ticksElapsed % 3 == 0) {
+                            PlayerFilter.entitiesAround(warlordsPlayer, radius, radius, radius)
+                                    .aliveEnemiesOf(warlordsPlayer)
+                                    .forEach(enemy -> {
+                                        enemy.addSpeedModifier(enemy, getName(), -slow, 3);
+                                        enemy.getCooldownManager().removeCooldownByName(getName() + " Damage");
+                                        enemy.getCooldownManager().addCooldown(new RegularCooldown<>(
+                                                getName() + " Damage",
+                                                null,
+                                                Charm.class,
+                                                null,
+                                                enemy,
+                                                CooldownTypes.ITEM,
+                                                cooldownManager -> {
+                                                },
+                                                3
+                                        ).addModifier(Modifier.INCOMING_DAMAGE_BEFORE_INTERVENE, (event, currentDamageValue) -> {
+                                                    currentDamageValue.addModifier(FloatModifiable.ModifierType.MULTIPLICATIVE_MULTIPLIER,
+                                                            getName() + " Damage", 1 + damage / 100f
+                                                    );
+                                                }
+                                        ));
+                                    });
+                        }
+                    }
+            ));
         }
 
     }
