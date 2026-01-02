e1af64653dd4061251bee0612826fe04d4e01a23
Plikie <plikie341@gmail.com>
2026-01-02 22:31:51 +0100

    Implement Obelisk item set

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Obelisk.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Obelisk.java
@@ -1,10 +1,17 @@
 package com.ebicep.warlords.pve.newitems.setbonus.sets;
 
+import com.ebicep.warlords.player.ingame.WarlordsNPC;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
+import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.PermanentCooldown;
+import com.ebicep.warlords.player.ingame.instances.type.Modifier;
+import com.ebicep.warlords.pve.mobs.flags.BossLike;
 import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
 import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
+import com.ebicep.warlords.util.warlords.modifiablevalues.FloatModifiable;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Obelisk extends BaseSet {
 
@@ -37,8 +44,31 @@ public class Obelisk extends BaseSet {
 
         @Override
         public void apply(WarlordsPlayer warlordsPlayer) {
-            // Implementation for increasing outgoing damage to bosses 
-            // and decreasing incoming damage from boss-type entities.
+            warlordsPlayer.getCooldownManager().addCooldown(new PermanentCooldown<>(
+                    getName(),
+                    null,
+                    Obelisk.class,
+                    null,
+                    warlordsPlayer,
+                    CooldownTypes.ITEM,
+                    cooldownManager -> {
+                    },
+                    false
+            ).addModifier(
+                    Modifier.MODIFY_OUTGOING_DAMAGE_BEFORE_INTERVENE,
+                    (event, currentHealValue) -> {
+                        if (event.getWarlordsEntity() instanceof WarlordsNPC warlordsNPC && warlordsNPC.getMob() instanceof BossLike) {
+                            currentHealValue.addModifier(FloatModifiable.ModifierType.MULTIPLICATIVE_MULTIPLIER, getName(), 1 + (bossDamageIncreasePercent / 100f));
+                        }
+                    }
+            ).addModifier(
+                    Modifier.MODIFY_INCOMING_DAMAGE_AFTER_INTERVENE,
+                    (event, currentHealValue) -> {
+                        if (event.getWarlordsEntity() instanceof WarlordsNPC warlordsNPC && warlordsNPC.getMob() instanceof BossLike) {
+                            currentHealValue.addModifier(FloatModifiable.ModifierType.MULTIPLICATIVE_MULTIPLIER, getName(), 1 - (bossDamageTakenReductionPercent / 100f));
+                        }
+                    }
+            ));
         }
 
     }
