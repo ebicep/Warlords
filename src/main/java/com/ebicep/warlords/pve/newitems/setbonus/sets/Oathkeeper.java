90a2476b68144feddbb0110e0d3e0e67ea1ff7ac
Plikie <plikie341@gmail.com>
2026-01-04 23:50:31 +0100

    Implement Oathkeeper item set

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Oathkeeper.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Oathkeeper.java
@@ -1,8 +1,16 @@
 package com.ebicep.warlords.pve.newitems.setbonus.sets;
 
+import com.ebicep.warlords.player.ingame.WarlordsNPC;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
+import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.PermanentCooldown;
+import com.ebicep.warlords.player.ingame.instances.InstanceBuilder;
+import com.ebicep.warlords.player.ingame.instances.type.Modifier;
+import com.ebicep.warlords.pve.mobs.flags.BossLike;
 import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
 import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
+import com.ebicep.warlords.util.warlords.GameRunnable;
+import com.ebicep.warlords.util.warlords.modifiablevalues.FloatModifiable;
 
 import java.util.List;
 
@@ -37,7 +45,43 @@ public class Oathkeeper extends BaseSet {
 
         @Override
         public void apply(WarlordsPlayer warlordsPlayer) {
-
+            warlordsPlayer.getCooldownManager().addCooldown(new PermanentCooldown<>(
+                    getName(),
+                    null,
+                    Oathkeeper.class,
+                    null,
+                    warlordsPlayer,
+                    CooldownTypes.ITEM,
+                    cooldownManager -> {
+                    },
+                    false
+            ).addModifier(
+                    Modifier.MODIFY_OUTGOING_DAMAGE_BEFORE_INTERVENE,
+                    (event, currentDamageValue) -> {
+                        if (!event.getCause().isEmpty()) {
+                            return;
+                        }
+                        currentDamageValue.addModifier(
+                                FloatModifiable.ModifierType.MULTIPLICATIVE_MULTIPLIER,
+                                getName(),
+                                1 + (meleeDamageIncreasePercent / 100f)
+                        );
+                        new GameRunnable(warlordsPlayer.getGame()) {
+                            @Override
+                            public void run() {
+                                event.getWarlordsEntity().addInstance(InstanceBuilder
+                                        .damage()
+                                        .cause(event.getCause())
+                                        .source(event.getSource())
+                                        .min(event.getMin().getCalculatedValue())
+                                        .max(event.getMax().getCalculatedValue())
+                                        .critChance(event.getCritChance().getCalculatedValue())
+                                        .critMultiplier(event.getCritMultiplier().getCalculatedValue())
+                                );
+                            }
+                        }.runTaskLater(3);
+                    }
+            ));
         }
 
     }
