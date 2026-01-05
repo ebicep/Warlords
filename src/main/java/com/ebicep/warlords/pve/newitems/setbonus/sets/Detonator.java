54f93fa648078c63dd4acb7afd7a3939ecd0d8ca
Plikie <plikie341@gmail.com>
2026-01-05 23:20:24 +0100

    Implement Detonator item set

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Detonator.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Detonator.java
@@ -1,8 +1,13 @@
 package com.ebicep.warlords.pve.newitems.setbonus.sets;
 
+import com.ebicep.warlords.abilities.internal.AbstractAbility;
+import com.ebicep.warlords.abilities.internal.Damages;
+import com.ebicep.warlords.abilities.internal.icon.OrangeAbilityIcon;
+import com.ebicep.warlords.abilities.internal.icon.RedAbilityIcon;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
 import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
 import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
+import com.ebicep.warlords.util.warlords.modifiablevalues.FloatModifiable;
 
 import java.util.List;
 
@@ -35,12 +40,17 @@ public class Detonator extends BaseSet {
 
         @Override
         public void apply(WarlordsPlayer warlordsPlayer) {
-            // Implementation for:
-            // 1. Locating the ability assigned to the Red Rune slot.
-            // 2. Applying a flat 5% damage increase to that specific ability's 
-            //    min/max damage values.
+            for (AbstractAbility ability : warlordsPlayer.getAbilities()) {
+                if (ability instanceof RedAbilityIcon && ability instanceof Damages<?>) {
+                    ((Damages<?>) ability).getDamageValues().getValues().forEach(damageValue -> {
+                        damageValue.forEachAllValues(
+                                value -> value.addModifier(
+                                        FloatModifiable.ModifierType.MULTIPLICATIVE_MULTIPLIER,
+                                        getName(),
+                                        1 + redRuneAbilityDamageIncreasePercent / 100f));
+                    });
+                }
+            }
         }
-
     }
-
 }
