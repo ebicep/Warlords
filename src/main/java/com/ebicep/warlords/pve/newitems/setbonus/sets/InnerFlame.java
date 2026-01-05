1a2d22b400d0defd925033e830ca6e9531de6791
Plikie <plikie341@gmail.com>
2026-01-05 23:20:15 +0100

    Implement Inner Flame item set

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/InnerFlame.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/InnerFlame.java
@@ -1,8 +1,15 @@
 package com.ebicep.warlords.pve.newitems.setbonus.sets;
 
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
+import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.PermanentCooldown;
+import com.ebicep.warlords.player.ingame.instances.InstanceBuilder;
+import com.ebicep.warlords.player.ingame.instances.InstanceFlags;
+import com.ebicep.warlords.player.ingame.instances.type.Modifier;
 import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
 import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
+import com.ebicep.warlords.util.warlords.PlayerFilter;
+import com.ebicep.warlords.util.warlords.modifiablevalues.FloatModifiable;
 
 import java.util.List;
 
@@ -35,6 +42,33 @@ public class InnerFlame extends BaseSet {
 
         @Override
         public void apply(WarlordsPlayer warlordsPlayer) {
+            warlordsPlayer.getCooldownManager().addCooldown(new PermanentCooldown<>(
+                    getName(),
+                    null,
+                    InnerFlame.class,
+                    null,
+                    warlordsPlayer,
+                    CooldownTypes.ITEM,
+                    cooldownManager -> {
+                    },
+                    false
+            ).addModifier(
+                    Modifier.ON_OUTGOING_HEALING,
+                    (event, currentHealValue, isCrit) -> {
+                        PlayerFilter.entitiesAround(event.getWarlordsEntity(), 4, 4, 4)
+                                .aliveEnemiesOf(warlordsPlayer)
+                                .forEach(entity -> {
+                                    entity.addInstance(InstanceBuilder
+                                            .damage()
+                                            .cause(getName())
+                                            .source(warlordsPlayer)
+                                            .value(currentHealValue * (healingDamageConversion / 100f))
+                                            .flags(InstanceFlags.IGNORE_SOURCE_DAMAGE_BOOST)
+                                    );
+                                }
+                        );
+                    }
+            ));
             // Implementation for converting healing done into AoE damage 
             // for nearby enemies based on healingDamageConversion percentage.
         }
