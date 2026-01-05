6f79882cb05d6d1251890f3fa2248b5de8edd423
Plikie <plikie341@gmail.com>
2026-01-05 23:20:31 +0100

    Implement Brittle Crown item set

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/BrittleCrown.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/BrittleCrown.java
@@ -1,8 +1,19 @@
 package com.ebicep.warlords.pve.newitems.setbonus.sets;
 
+import com.ebicep.warlords.events.player.ingame.pve.WarlordsAddCurrencyEvent;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
+import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.PermanentCooldown;
+import com.ebicep.warlords.player.ingame.instances.InstanceBuilder;
+import com.ebicep.warlords.player.ingame.instances.InstanceFlags;
+import com.ebicep.warlords.player.ingame.instances.type.Modifier;
 import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
 import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
+import com.ebicep.warlords.util.warlords.PlayerFilter;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 
 import java.util.List;
 
@@ -37,6 +48,32 @@ public class BrittleCrown extends BaseSet {
 
         @Override
         public void apply(WarlordsPlayer warlordsPlayer) {
+            warlordsPlayer.getGame().registerEvents(new Listener() {
+                @EventHandler
+                public void onInsigniaGain(WarlordsAddCurrencyEvent event) {
+                    float currency = event.getCurrencyToAdd();
+                    event.setCurrencyToAdd(currency * (1 + insigniaGainBonusPercent / 100f));
+                }
+            });
+            warlordsPlayer.getCooldownManager().addCooldown(new PermanentCooldown<>(
+                    getName(),
+                    null,
+                    BrittleCrown.class,
+                    null,
+                    warlordsPlayer,
+                    CooldownTypes.ITEM,
+                    cooldownManager -> {
+                    },
+                    false
+            ).addModifier(
+                    Modifier.ON_INCOMING_DAMAGE,
+                    (event, currentDamageValue, isCrit) -> {
+                        int loss = (int) Math.floor(warlordsPlayer.getCurrency() * insigniaLossOnHitPercent / 100f);
+                        int newValue = (int) Math.max(0L, warlordsPlayer.getCurrency() - loss);
+                        warlordsPlayer.setCurrency(newValue);
+                        warlordsPlayer.sendMessage(Component.text("You lost " + loss + " ❂ insignia!", NamedTextColor.RED));
+                    }
+            ));
             // Implementation for:
             // 1. Modifying the insignia multiplier for every kill made by the player.
             // 2. Registering a listener for when the player takes damage.
