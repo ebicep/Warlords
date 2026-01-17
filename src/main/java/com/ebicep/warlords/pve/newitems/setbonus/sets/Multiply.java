27d32077df4a98766ef4a81d676240b90e946396
Plikie <plikie341@gmail.com>
2026-01-17 22:59:44 +0100

    Implement Multiply item set

---
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Multiply.java
@@ -0,0 +1,74 @@
+package com.ebicep.warlords.pve.newitems.setbonus.sets;
+
+import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
+import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.PermanentCooldown;
+import com.ebicep.warlords.player.ingame.instances.type.Modifier;
+import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
+import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
+import com.ebicep.warlords.util.warlords.modifiablevalues.FloatModifiable;
+
+import java.util.List;
+
+public class Multiply extends BaseSet {
+
+    private int maxEnergyRequirement;
+    private int critMultiplierIncreasePercent;
+
+    @Override
+    public void init() {
+        super.init();
+        this.maxEnergyRequirement = getValue("maxEnergyRequirement", int.class);
+        this.critMultiplierIncreasePercent = getValue("critMultiplierIncreasePercent", int.class);
+    }
+
+    @Override
+    public String getConfigFieldName() {
+        return "multiply";
+    }
+
+    @Override
+    public Bonus create() {
+        return new Bonus();
+    }
+
+    @Override
+    public List<Object> getVariables() {
+        return List.of(maxEnergyRequirement, critMultiplierIncreasePercent);
+    }
+
+    public class Bonus implements SetBonus.Bonus {
+        @Override
+        public void apply(WarlordsPlayer warlordsPlayer) {
+            warlordsPlayer.getCooldownManager().addCooldown(new PermanentCooldown<>(
+                    getName(),
+                    null,
+                    Multiply.class,
+                    null,
+                    warlordsPlayer,
+                    CooldownTypes.ITEM,
+                    cooldownManager -> {
+                    },
+                    false
+            ).addModifier(
+                    Modifier.MODIFY_OUTGOING_CRIT_MULTIPLIER,
+                    (event, currentCritMultiplier) -> {
+                        int stacks = (int) (warlordsPlayer.getMaxEnergy() / maxEnergyRequirement);
+                        if (stacks <= 0) {
+                            return;
+                        }
+
+                        float perStackMultiplier = critMultiplierIncreasePercent / 100f;
+                        currentCritMultiplier.addModifier(
+                                FloatModifiable.ModifierType.MULTIPLICATIVE_MULTIPLIER,
+                                getName(),
+                                1 + (stacks * perStackMultiplier)
+                        );
+                    }
+            ));
+
+        }
+
+    }
+
+}
