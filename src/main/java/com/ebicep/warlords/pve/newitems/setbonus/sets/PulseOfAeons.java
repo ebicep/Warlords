b2e330b56e1754c42f59710163c5279d5da980b2
Plikie <plikie341@gmail.com>
2026-01-18 18:49:16 +0100

    Implement Pulse of Aeons item set

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/PulseOfAeons.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/PulseOfAeons.java
@@ -1,8 +1,19 @@
 package com.ebicep.warlords.pve.newitems.setbonus.sets;
 
+import com.ebicep.warlords.effects.EffectUtils;
+import com.ebicep.warlords.game.state.EndState;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
+import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.PermanentCooldown;
+import com.ebicep.warlords.player.ingame.instances.InstanceBuilder;
+import com.ebicep.warlords.player.ingame.instances.InstanceFlags;
+import com.ebicep.warlords.player.ingame.instances.type.Modifier;
 import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
 import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
+import com.ebicep.warlords.util.warlords.PlayerFilter;
+import com.ebicep.warlords.util.warlords.Utils;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 
 import java.util.List;
 
@@ -35,12 +46,42 @@ public class PulseOfAeons extends BaseSet {
 
     public class Bonus implements SetBonus.Bonus {
 
+        int stacks = 0;
+
         @Override
         public void apply(WarlordsPlayer warlordsPlayer) {
-            // Implementation for:
-            // 1. Listening for healing instances triggered by the player.
-            // 2. Tracking a counter until it reaches healsPerPulse.
-            // 3. Triggering a global heal of pulseHealAmount to all allies.
+            warlordsPlayer.getCooldownManager().addCooldown(new PermanentCooldown<>(
+                    getName(),
+                    null,
+                    PulseOfAeons.class,
+                    null,
+                    warlordsPlayer,
+                    CooldownTypes.ITEM,
+                    cooldownManager -> {},
+                    false,
+                    (cooldown, ticksElapsed) -> {
+
+                    }
+            ).addModifier(Modifier.ON_OUTGOING_HEALING, (event, currentHealValue, isCrit) -> {
+                stacks++;
+
+                if (stacks == healsPerPulse) {
+                    stacks = 0;
+                    Utils.playGlobalSound(warlordsPlayer.getLocation(), Sound.BLOCK_BEACON_POWER_SELECT, 2, 2f);
+                    PlayerFilter.playingGame(warlordsPlayer.getGame())
+                            .aliveTeammatesOfExcludingSelf(warlordsPlayer)
+                            .forEach(entity -> {
+                                entity.addInstance(InstanceBuilder
+                                        .healing()
+                                        .cause(getName())
+                                        .source(warlordsPlayer)
+                                        .value(pulseHealAmount)
+                                        .flags(InstanceFlags.TRUE_HEALING)
+                                );
+                            }
+                    );
+                }
+            }));
         }
 
     }
