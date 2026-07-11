cb419557a19b102b21e96baec8646878e2c5eefb
ebicep <ebicep314@gmail.com>
2026-07-11 13:57:11 -0400

    chore(build): clean up build.gradle.kts
    Remove dead commented code, deduplicate repository and JavaCompile
    configuration, and extract shared archive version logic without changing
    build behavior.

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,11 @@ group = "com.ebicep"
 version = "Prelude to Chaos"
 description = "Warlords"
 
+val archiveVersionSuffix: String
+    get() = version.toString().replace(" ", "-")
+
 java {
-    // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
+    // Configure the java toolchain. This allows gradle to auto-provision JDK 21 on systems that only have JDK 8 installed for example.
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
@@ -25,60 +28,20 @@ repositories {
     mavenCentral()
     google()
 
-    maven {
-        url = uri("https://repo.papermc.io/repository/maven-public/")
-    }
-
-    maven {
-        url = uri("https://repo.infernalsuite.com/repository/maven-snapshots/")
-    }
-
-//    maven {
-//        url = uri("https://repo.rapture.pw/repository/maven-releases/")
-//    }
-
-    maven {
-        url = uri("https://repo.codemc.io/repository/maven-public/")
-    }
-
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.infernalsuite.com/repository/maven-snapshots/")
+    maven("https://repo.codemc.io/repository/maven-public/")
     maven {
         name = "citizens-repo"
         url = uri("https://maven.citizensnpcs.co/repo")
     }
-
-    maven {
-        url = uri("https://hub.spigotmc.org/nexus/content/groups/public/")
-    }
-
-
-    maven {
-        url = uri("https://repo.aikar.co/content/groups/aikar/")
-    }
-
-    maven {
-        url = uri("https://repo.dmulloy2.net/repository/public/")
-    }
-
-    maven {
-        url = uri("https://m2.dv8tion.net/releases")
-    }
-
-    maven {
-        url = uri("https://repo.maven.apache.org/maven2/")
-    }
-
-    maven {
-        url = uri("https://jitpack.io")
-    }
-
-    maven {
-        url = uri("https://maven.enginehub.org/repo")
-    }
-
-    maven {
-        url = uri("https://repo.onarandombox.com/content/groups/public/")
-    }
-
+    maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+    maven("https://repo.aikar.co/content/groups/aikar/")
+    maven("https://repo.dmulloy2.net/repository/public/")
+    maven("https://m2.dv8tion.net/releases")
+    maven("https://jitpack.io")
+    maven("https://maven.enginehub.org/repo")
+    maven("https://repo.onarandombox.com/content/groups/public/")
 }
 
 dependencies {
@@ -93,7 +56,6 @@ dependencies {
 
     implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
 
-//    implementation("com.github.Rapha149.SignGUI:signgui:5232fbd3f6")
     implementation("de.rapha149.signgui:signgui:2.5.0")
 
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
@@ -126,21 +88,11 @@ publishing {
     }
 }
 
-
 tasks {
     // Configure reobfJar to run when invoking the build task
     assemble {
         dependsOn(reobfJar)
     }
-
-    compileJava {
-        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
-
-        // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
-        // See https://openjdk.java.net/jeps/247 for more information.
-        options.release.set(21)
-    }
-
 
     javadoc {
         options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
@@ -151,55 +103,35 @@ tasks {
     }
 
     jar {
-        enabled = false
-        archiveVersion.set(project.version.toString().replace(" ", "-"))
+        archiveVersion.set(archiveVersionSuffix)
     }
 
     shadowJar {
-        archiveVersion.set(project.version.toString().replace(" ", "-"))
+        archiveVersion.set(archiveVersionSuffix)
         relocate("co.aikar.commands", "com.ebicep.warlords.acf.acf")
         relocate("co.aikar.locales", "com.ebicep.warlords.acf.locales")
     }
 
     reobfJar {
-        val safeVersion = project.version.toString().replace(" ", "-")
         val output = System.getProperty("outputDirectory")
         if (output != null) {
-            outputJar.set(layout.buildDirectory.file("${output}${project.name}-${safeVersion}.jar"))
+            outputJar.set(layout.buildDirectory.file("${output}${project.name}-${archiveVersionSuffix}.jar"))
         } else {
-            outputJar.set(layout.buildDirectory.file("libs/${project.name}-${safeVersion}.jar"))
+            outputJar.set(layout.buildDirectory.file("libs/${project.name}-${archiveVersionSuffix}.jar"))
         }
     }
 
     runServer {
         version.set("1.21.4")
     }
-
-    register("printReobfJarPath") {
-        dependsOn(reobfJar)
-        doLast {
-            println(reobfJar.get().outputJar.get().asFile.absolutePath)
-        }
-    }
-
 }
 
 tasks.withType<JavaCompile>().configureEach {
-//    doFirst {
-//        configure(options, closureOf<CompileOptions> {
-//            configure(forkOptions, closureOf<ForkOptions> {
-//                executable = null
-//                javaHome = null
-//            })
-//        })
-//    }
-    options.encoding = "UTF-8"
+    options.encoding = Charsets.UTF_8.name()
     options.compilerArgs.add("-parameters")
     options.isFork = true
-    options.release = 21
-//    options.forkOptions.executable = "javac"
+    options.release.set(21)
 }
-
 
 paperPluginYaml {
     main = "com.ebicep.warlords.Warlords"
@@ -232,4 +164,3 @@ paperPluginYaml {
         }
     }
 }
-
