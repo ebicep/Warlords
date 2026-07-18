a727793fc2b93e0b27001f482351e040a5430d7d
Paki <53056381+Plikie@users.noreply.github.com>
2026-07-18 12:00:11 +0200

    Wire Plains of Dunestar escort anomaly

--- src/main/java/com/ebicep/warlords/game/maps/PlainsOfDunestar.java
+++ src/main/java/com/ebicep/warlords/game/maps/PlainsOfDunestar.java
@@ -1,22 +1,30 @@
 package com.ebicep.warlords.game.maps;
 
+import com.ebicep.warlords.game.option.Option;
+import com.ebicep.warlords.game.option.pve.anomaly.DunestarEscortOption;
+import com.ebicep.warlords.game.option.pve.anomaly.DunestarRouteMarker;
+import com.ebicep.warlords.util.bukkit.LocationFactory;
+
+import java.util.List;
+
 public class PlainsOfDunestar extends AbstractAnomalyMap {
 
+    private static final double[][] ROUTE_LOCATIONS = {
+            {0.5, 80, 0.5},
+            {30.5, 80, 5.5},
+            {60.5, 80, -8.5},
+            {90.5, 80, 0.5}
+    };
+
     public PlainsOfDunestar() {
-        super(
-                "Plains of Dunestar",
-                "PlainsOfDunestar",
-                new double[]{0.5, 80, 0.5},
-                new double[][]{
-                        {22.5, 80, 8.5},
-                        {-8.5, 80, 22.5},
-                        {-20.5, 80, -10.5}
-                },
-                new double[][][]{
-                        {{38.5, 80, 8.5}, {32.5, 80, 22.5}, {35.5, 80, -7.5}},
-                        {{-8.5, 80, 38.5}, {7.5, 80, 34.5}, {-24.5, 80, 34.5}},
-                        {{-36.5, 80, -10.5}, {-31.5, 80, 5.5}, {-30.5, 80, -25.5}}
-                }
-        );
+        super("Plains of Dunestar", "PlainsOfDunestar", ROUTE_LOCATIONS[0]);
+    }
+
+    @Override
+    protected void addAnomalyOptions(List<Option> options, LocationFactory loc) {
+        for (int routeIndex = 0; routeIndex < ROUTE_LOCATIONS.length; routeIndex++) {
+            options.add(DunestarRouteMarker.create(routeIndex, location(loc, ROUTE_LOCATIONS[routeIndex])).asOption());
+        }
+        options.add(new DunestarEscortOption());
     }
 }
