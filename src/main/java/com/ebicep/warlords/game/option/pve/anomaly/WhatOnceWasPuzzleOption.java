c2b50f012b405ea00935806e13755dfa134a9515
Paki <53056381+Plikie@users.noreply.github.com>
2026-07-18 11:59:34 +0200

    Add What Once Was rune puzzle anomaly

---
+++ src/main/java/com/ebicep/warlords/game/option/pve/anomaly/WhatOnceWasPuzzleOption.java
@@ -0,0 +1,9 @@
+package com.ebicep.warlords.game.option.pve.anomaly;
+
+import com.ebicep.warlords.Warlords;
+import com.ebicep.warlords.game.Game;
+import com.ebicep.warlords.game.option.marker.scoreboard.ScoreboardHandler;
+import com.ebicep.warlords.game.option.marker.scoreboard.SimpleScoreboardHandler;
+import com.ebicep.warlords.player.ingame.WarlordsEntity;
+import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.util.w
