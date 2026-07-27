82785ed0b4db888314051149b67c57b665615f9a
Paki <53056381+Plikie@users.noreply.github.com>
2026-07-27 14:47:31 +0200

    Implement Voidcarver cooldown aura

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Voidcarver.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Voidcarver.java
@@ -1,46 +1,21 @@
 package com.ebicep.warlords.pve.newitems.setbonus.sets;
 
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
+import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
+import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.PermanentCooldown;
 import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
 import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
-
+import com.ebicep.warlords.util.warlords.PlayerFilter;
 import java.util.List;
 
 public class Voidcarver extends BaseSet {
-
-    private int radius;
-    private int cdrBoost;
-
-    @Override
-    public void init() {
-        super.init();
-        this.radius = getValue("radius", int.class);
-        this.cdrBoost = getValue("cdrBoost", int.class);
+    private int radius,cdrBoost;
+    @Override public void init(){super.init();radius=getValue("radius",int.class);cdrBoost=getValue("cdrBoost",int.class);}
+    @Override public String getConfigFieldName(){return "voidcarver";}
+    @Override public Bonus create(){return new Bonus();}
+    @Override public List<Object> getVariables(){return List.of(radius,cdrBoost);}
+    public class Bonus implements SetBonus.Bonus{
+        private float extraCooldownTicks;
+        @Override public void apply(WarlordsPlayer player){player.getCooldownManager().addCooldown(new PermanentCooldown<>(getName(),null,Voidcarver.class,null,player,CooldownTypes.ITEM,m->{},false,(cooldown,ticks)->{extraCooldownTicks+=cdrBoost/100f;int subtract=(int)extraCooldownTicks;if(subtract<=0)return;extraCooldownTicks-=subtract;PlayerFilter.entitiesAround(player,radius,radius,radius).aliveTeammatesOfExcludingSelf(player).forEach(ally->ally.getCooldownManager().subtractTicksOnRegularCooldowns(subtract,CooldownTypes.ABILITY));}));}
     }
-
-    @Override
-    public String getConfigFieldName() {
-        return "voidcarver";
-    }
-
-    @Override
-    public Bonus create() {
-        return new Bonus();
-    }
-
-    @Override
-    public List<Object> getVariables() {
-        return List.of(radius, cdrBoost);
-    }
-
-    public class Bonus implements SetBonus.Bonus {
-
-        @Override
-        public void apply(WarlordsPlayer warlordsPlayer) {
-            // Implementation for the aura effect: 
-            // Reducing cooldowns of allies within the specified radius.
-        }
-
-    }
-
 }
