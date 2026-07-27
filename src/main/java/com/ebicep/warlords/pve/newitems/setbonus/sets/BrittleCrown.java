0ea25d12b6560fe4757ed0daa3bfe2b9279bda41
Paki <53056381+Plikie@users.noreply.github.com>
2026-07-27 14:44:23 +0200

    Restrict Brittle Crown bonus to kill rewards

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/BrittleCrown.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/BrittleCrown.java
@@ -1,81 +1,29 @@
 package com.ebicep.warlords.pve.newitems.setbonus.sets;
 
+import com.ebicep.warlords.events.player.ingame.WarlordsDeathEvent;
 import com.ebicep.warlords.events.player.ingame.pve.WarlordsAddCurrencyEvent;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
 import com.ebicep.warlords.player.ingame.cooldowns.CooldownTypes;
 import com.ebicep.warlords.player.ingame.cooldowns.cooldowns.PermanentCooldown;
-import com.ebicep.warlords.player.ingame.instances.InstanceBuilder;
-import com.ebicep.warlords.player.ingame.instances.InstanceFlags;
 import com.ebicep.warlords.player.ingame.instances.type.Modifier;
 import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
 import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
-import com.ebicep.warlords.util.warlords.PlayerFilter;
+import com.ebicep.warlords.util.warlords.GameRunnable;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class BrittleCrown extends BaseSet {
-
-    private int insigniaGainBonusPercent;
-    private int insigniaLossOnHitPercent;
-
-    @Override
-    public void init() {
-        super.init();
-        this.insigniaGainBonusPercent = getValue("insigniaGainBonusPercent", int.class);
-        this.insigniaLossOnHitPercent = getValue("insigniaLossOnHitPercent", int.class);
+    private int insigniaGainBonusPercent,insigniaLossOnHitPercent;
+    @Override public void init(){super.init();insigniaGainBonusPercent=getValue("insigniaGainBonusPercent",int.class);insigniaLossOnHitPercent=getValue("insigniaLossOnHitPercent",int.class);}
+    @Override public String getConfigFieldName(){return "brittleCrown";}
+    @Override public Bonus create(){return new Bonus();}
+    @Override public List<Object> getVariables(){return List.of(insigniaGainBonusPercent,insigniaLossOnHitPercent);}
+    public class Bonus implements SetBonus.Bonus{
+        @Override public void apply(WarlordsPlayer player){AtomicBoolean killWindow=new AtomicBoolean(false);player.getGame().registerEvents(new Listener(){@EventHandler public void onDeath(WarlordsDeathEvent event){if(!Objects.equals(event.getKiller(),player)||event.getWarlordsEntity().getTeam().equals(player.getTeam()))return;killWindow.set(true);new GameRunnable(player.getGame()){@Override public void run(){killWindow.set(false);}}.runTaskLater(1);}@EventHandler public void onCurrency(WarlordsAddCurrencyEvent event){if(event.getWarlordsEntity().equals(player)&&killWindow.get())event.setCurrencyToAdd(event.getCurrencyToAdd()*(1+insigniaGainBonusPercent/100f));}});player.getCooldownManager().addCooldown(new PermanentCooldown<>(getName(),null,BrittleCrown.class,null,player,CooldownTypes.ITEM,m->{},false).addModifier(Modifier.ON_INCOMING_DAMAGE,(event,value,crit)->{int loss=(int)Math.floor(player.getCurrency()*insigniaLossOnHitPercent/100f);player.setCurrency(Math.max(0,player.getCurrency()-loss));player.sendMessage(Component.text("You lost "+loss+" ❂ insignia!",NamedTextColor.RED));}));}
     }
-
-    @Override
-    public String getConfigFieldName() {
-        return "brittleCrown";
-    }
-
-    @Override
-    public Bonus create() {
-        return new Bonus();
-    }
-
-    @Override
-    public List<Object> getVariables() {
-        return List.of(insigniaGainBonusPercent, insigniaLossOnHitPercent);
-    }
-
-    public class Bonus implements SetBonus.Bonus {
-
-        @Override
-        public void apply(WarlordsPlayer warlordsPlayer) {
-            warlordsPlayer.getGame().registerEvents(new Listener() {
-                @EventHandler
-                public void onInsigniaGain(WarlordsAddCurrencyEvent event) {
-                    float currency = event.getCurrencyToAdd();
-                    event.setCurrencyToAdd(currency * (1 + insigniaGainBonusPercent / 100f));
-                }
-            });
-            warlordsPlayer.getCooldownManager().addCooldown(new PermanentCooldown<>(
-                    getName(),
-                    null,
-                    BrittleCrown.class,
-                    null,
-                    warlordsPlayer,
-                    CooldownTypes.ITEM,
-                    cooldownManager -> {
-                    },
-                    false
-            ).addModifier(
-                    Modifier.ON_INCOMING_DAMAGE,
-                    (event, currentDamageValue, isCrit) -> {
-                        int loss = (int) Math.floor(warlordsPlayer.getCurrency() * insigniaLossOnHitPercent / 100f);
-                        int newValue = (int) Math.max(0L, warlordsPlayer.getCurrency() - loss);
-                        warlordsPlayer.setCurrency(newValue);
-                        warlordsPlayer.sendMessage(Component.text("You lost " + loss + " ❂ insignia!", NamedTextColor.RED));
-                    }
-            ));
-        }
-
-    }
-
 }
