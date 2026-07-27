cb296673a41cfa0cfb7fe1f78bf51eca16ee79af
Paki <53056381+Plikie@users.noreply.github.com>
2026-07-27 14:49:48 +0200

    Restrict Gravemind summons to wearer kills

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Gravemind.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Gravemind.java
@@ -18,107 +18,21 @@ import org.bukkit.Sound;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Gravemind extends BaseSet {
-
+    public static final ItemStack CHESTPLATE=Utils.applyColorTo(Material.LEATHER_CHESTPLATE,255,200,0),LEGGINGS=Utils.applyColorTo(Material.LEATHER_LEGGINGS,255,200,0),BOOTS=Utils.applyColorTo(Material.LEATHER_BOOTS,255,200,0);
     private int summonChance;
     private float duration;
-
-    public static final ItemStack CHESTPLATE = Utils.applyColorTo(Material.LEATHER_CHESTPLATE, 255, 200, 0);
-    public static final ItemStack LEGGINGS = Utils.applyColorTo(Material.LEATHER_LEGGINGS, 255, 200, 0);
-    public static final ItemStack BOOTS = Utils.applyColorTo(Material.LEATHER_BOOTS, 255, 200, 0);
-
-    @Override
-    public void init() {
-        super.init();
-        this.summonChance = getValue("summonChance", int.class);
-        this.duration = getValue("duration", float.class);
+    @Override public void init(){super.init();summonChance=getValue("summonChance",int.class);duration=getValue("duration",float.class);}
+    @Override public String getConfigFieldName(){return "gravemind";}
+    @Override public Bonus create(){return new Bonus();}
+    @Override public List<Object> getVariables(){return List.of(summonChance,duration);}
+    public class Bonus implements SetBonus.Bonus{
+        @Override public void apply(WarlordsPlayer player){player.getGame().registerEvents(new Listener(){@EventHandler public void onDeath(WarlordsDeathEvent event){if(!Objects.equals(event.getKiller(),player)||event.getWarlordsEntity().getTeam().equals(player.getTeam())||ThreadLocalRandom.current().nextDouble()>summonChance/100.0)return;spawnMob(player,event.getWarlordsEntity().getLocation());}});}
     }
-
-    @Override
-    public String getConfigFieldName() {
-        return "gravemind";
-    }
-
-    @Override
-    public Bonus create() {
-        return new Bonus();
-    }
-
-    @Override
-    public List<Object> getVariables() {
-        return List.of(summonChance, duration);
-    }
-
-    public class Bonus implements SetBonus.Bonus {
-
-        @Override
-        public void apply(WarlordsPlayer warlordsPlayer) {
-            Listener listener = new Listener() {
-
-                @EventHandler
-                private void onEnemyDeath(WarlordsDeathEvent event) {
-                    if (event.getWarlordsEntity().equals(warlordsPlayer)) {
-                        return;
-                    }
-                    if (event.getWarlordsEntity().getTeam().equals(warlordsPlayer.getTeam())) {
-                        return;
-                    }
-                    if (ThreadLocalRandom.current().nextDouble() > summonChance / 100.0) {
-                        return;
-                    }
-                    spawnMob(warlordsPlayer, event.getWarlordsEntity().getLocation());
-                }
-            };
-            warlordsPlayer.getGame().registerEvents(listener);
-        }
-
-    }
-
-    private void spawnMob(WarlordsPlayer warlordsPlayer, Location summonLocation) {
-        Optional<PveOption> pveOption = warlordsPlayer.getGame().getOption(PveOption.class)
-                .stream()
-                .findFirst();
-        warlordsPlayer.playSound(warlordsPlayer.getLocation(), Sound.ENTITY_ZOMBIE_VILLAGER_CURE, 0.5f, 0.5f);
-        warlordsPlayer.sendMessage(Component.text("Your gravemind has summoned a mob for 30 seconds!", NamedTextColor.GREEN));
-
-        HashSet<AbstractMob> spawnedMobs = new HashSet<>();
-        AbstractMob mob = Mob.ZOMBIE_SWORDSMAN.createMob(summonLocation);
-        updateMobEquipment(mob, warlordsPlayer);
-        spawnedMobs.add(mob);
-        if (warlordsPlayer.getTeam() == null) {
-            return;
-        }
-        pveOption.get().spawnNewMob(mob, warlordsPlayer.getTeam());
-
-        new GameRunnable(warlordsPlayer.getGame()) {
-            @Override
-            public void run() {
-                spawnedMobs.forEach(mob -> {
-                    if (pveOption.get().getMobs().contains(mob)) {
-                        mob.getWarlordsNPC().die(mob.getWarlordsNPC(), WarlordsDeathEvent.DeathInfoBuilder.create().setForced(true));
-                        pveOption.get().despawnMob(mob);
-                    }
-                });
-                spawnedMobs.clear();
-            }
-        }.runTaskLater((long) (20 * duration));
-    }
-
-    private static void updateMobEquipment(AbstractMob mob, WarlordsPlayer player) {
-        mob.setEquipment(new Utils.SimpleEntityEquipment(
-                HeadUtils.getHead(player.getUuid()),
-                CHESTPLATE,
-                LEGGINGS,
-                BOOTS,
-                mob.getEquipment().getItemInMainHand(),
-                mob.getEquipment().getItemInOffHand()
-        ));
-        mob.updateEquipment();
-    }
+    private void spawnMob(WarlordsPlayer player,Location location){Optional<PveOption> optional=player.getGame().getOption(PveOption.class).stream().findFirst();if(optional.isEmpty()||player.getTeam()==null)return;PveOption pve=optional.get();player.playSound(player.getLocation(),Sound.ENTITY_ZOMBIE_VILLAGER_CURE,.5f,.5f);player.sendMessage(Component.text("Your Gravemind summoned a mob for "+(int)duration+" seconds!",NamedTextColor.GREEN));AbstractMob mob=Mob.ZOMBIE_SWORDSMAN.createMob(location);mob.setEquipment(new Utils.SimpleEntityEquipment(HeadUtils.getHead(player.getUuid()),CHESTPLATE,LEGGINGS,BOOTS,mob.getEquipment().getItemInMainHand(),mob.getEquipment().getItemInOffHand()));mob.updateEquipment();Summoner.registerSummon(mob,player);pve.spawnNewMob(mob,player.getTeam());new GameRunnable(player.getGame()){@Override public void run(){if(pve.getMobs().contains(mob)){mob.getWarlordsNPC().die(mob.getWarlordsNPC(),WarlordsDeathEvent.DeathInfoBuilder.create().setForced(true));pve.despawnMob(mob);}}}.runTaskLater((long)(20*duration));}
 }
