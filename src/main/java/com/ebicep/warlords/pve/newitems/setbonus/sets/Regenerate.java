7000b70b40f6ee4771957e3d821937431c64cf1b
Paki <53056381+Plikie@users.noreply.github.com>
2026-07-27 14:40:36 +0200

    Implement Regenerate interval behavior

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Regenerate.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Regenerate.java
@@ -1,52 +1,31 @@
 package com.ebicep.warlords.pve.newitems.setbonus.sets;
 
-import com.ebicep.warlords.events.player.ingame.pve.WarlordsAddCurrencyEvent;
+import com.ebicep.warlords.database.repositories.config.ConfigManager;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
 import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
 import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
 import com.ebicep.warlords.util.warlords.GameRunnable;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-
 import java.util.List;
 
 public class Regenerate extends BaseSet {
-
-    @Override
-    public void init() {
-        super.init();
-    }
-
-    @Override
-    public String getConfigFieldName() {
-        return "regenerate";
-    }
-
-    @Override
-    public Bonus create() {
-        return new Bonus();
-    }
-
-    @Override
-    public List<Object> getVariables() {
-        return List.of();
-    }
-
-    public class Bonus implements SetBonus.Bonus {
-
-        @Override
-        public void apply(WarlordsPlayer warlordsPlayer) {
-            new GameRunnable(warlordsPlayer.getGame()) {
-                @Override
-                public void run() {
-                    if (warlordsPlayer.isDead()) {
-                        return;
-                    }
-                    if (warlordsPlayer.getRegenTickTimer() > 1) {
-                        warlordsPlayer.setRegenTickTimer(1);
-                    }
+    @Override public String getConfigFieldName(){return "regenerate";}
+    @Override public Bonus create(){return new Bonus();}
+    @Override public List<Object> getVariables(){return List.of();}
+    public class Bonus implements SetBonus.Bonus{
+        @Override public void apply(WarlordsPlayer player){
+            new GameRunnable(player.getGame()){
+                private int ticks;
+                @Override public void run(){
+                    if(player.isDead())return;
+                    player.setRegenTickTimer(1);
+                    ticks++;
+                    if(ticks%40!=0||player.getCurrentHealth()>=player.getMaxHealth())return;
+                    int regen=ConfigManager.getGameConfigValue(ConfigManager.DEFAULT_NAMESPACES,"regenHealth",int.class);
+                    player.getRegenPerSecond().setBaseValue(regen);
+                    player.getRegenPerSecond().refresh();
+                    player.setCurrentHealth(Math.min(player.getMaxHealth(),player.getCurrentHealth()+player.getRegenPerSecond().getCalculatedValue()));
                 }
-            }.runTaskTimer(0, 0);
-         }
+            }.runTaskTimer(0,1);
+        }
     }
 }
