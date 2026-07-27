ac1722f3e433f41f7c011989c877581c9fe31877
Paki <53056381+Plikie@users.noreply.github.com>
2026-07-27 14:40:16 +0200

    Fix Lean configuration and orange range effect

--- src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Lean.java
+++ src/main/java/com/ebicep/warlords/pve/newitems/setbonus/sets/Lean.java
@@ -4,62 +4,19 @@ import com.ebicep.warlords.abilities.internal.AbstractAbility;
 import com.ebicep.warlords.abilities.internal.HitBox;
 import com.ebicep.warlords.abilities.internal.Splash;
 import com.ebicep.warlords.abilities.internal.icon.OrangeAbilityIcon;
-import com.ebicep.warlords.abilities.internal.icon.RedAbilityIcon;
-import com.ebicep.warlords.abilities.internal.icon.WeaponAbilityIcon;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
 import com.ebicep.warlords.pve.newitems.setbonus.BaseSet;
 import com.ebicep.warlords.pve.newitems.setbonus.SetBonus;
 import com.ebicep.warlords.util.warlords.modifiablevalues.FloatModifiable;
-
 import java.util.List;
 
 public class Lean extends BaseSet {
-
-    private int blueRuneAbilityRangeIncreasePercent;
-
-    @Override
-    public void init() {
-        super.init();
-        this.blueRuneAbilityRangeIncreasePercent = getValue("blueRuneAbilityRangeIncreasePercent", int.class);
-    }
-
-    @Override
-    public String getConfigFieldName() {
-        return "cell";
-    }
-
-    @Override
-    public Bonus create() {
-        return new Bonus();
-    }
-
-    @Override
-    public List<Object> getVariables() {
-        return List.of(blueRuneAbilityRangeIncreasePercent);
-    }
-
-    public class Bonus implements SetBonus.Bonus {
-
-        @Override
-        public void apply(WarlordsPlayer warlordsPlayer) {
-            for (AbstractAbility ability : warlordsPlayer.getAbilities()) {
-                if (ability instanceof OrangeAbilityIcon) {
-                    if (ability instanceof HitBox hitBox) {
-                        hitBox.getHitBoxRadius().addModifier(
-                                FloatModifiable.ModifierType.MULTIPLICATIVE_MULTIPLIER,
-                                getName(),
-                                1 + blueRuneAbilityRangeIncreasePercent / 100f
-                        );
-                    }
-                    if (ability instanceof Splash splash) {
-                        splash.getSplashRadius().addModifier(
-                                FloatModifiable.ModifierType.MULTIPLICATIVE_MULTIPLIER,
-                                getName(),
-                                1 + blueRuneAbilityRangeIncreasePercent / 100f
-                        );
-                    }
-                }
-            }
-        }
+    private int orangeRuneAbilityRangeIncreasePercent;
+    @Override public void init(){super.init();orangeRuneAbilityRangeIncreasePercent=getValue("orangeRuneAbilityRangeIncreasePercent",int.class);}
+    @Override public String getConfigFieldName(){return "lean";}
+    @Override public Bonus create(){return new Bonus();}
+    @Override public List<Object> getVariables(){return List.of(orangeRuneAbilityRangeIncreasePercent);}
+    public class Bonus implements SetBonus.Bonus{
+        @Override public void apply(WarlordsPlayer player){for(AbstractAbility ability:player.getAbilities()){if(!(ability instanceof OrangeAbilityIcon))continue;if(ability instanceof HitBox hitBox)hitBox.getHitBoxRadius().addModifier(FloatModifiable.ModifierType.MULTIPLICATIVE_MULTIPLIER,getName(),1+orangeRuneAbilityRangeIncreasePercent/100f);if(ability instanceof Splash splash)splash.getSplashRadius().addModifier(FloatModifiable.ModifierType.MULTIPLICATIVE_MULTIPLIER,getName(),1+orangeRuneAbilityRangeIncreasePercent/100f);}}
     }
 }
