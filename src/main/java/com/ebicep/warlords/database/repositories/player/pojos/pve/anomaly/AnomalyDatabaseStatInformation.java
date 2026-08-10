4638677005a6d0edbf14562c1b98f7ce7fb7c139
Paki <53056381+Plikie@users.noreply.github.com>
2026-08-10 20:26:20 +0200

    Add Anomaly PvE stat information

---
+++ src/main/java/com/ebicep/warlords/database/repositories/player/pojos/pve/anomaly/AnomalyDatabaseStatInformation.java
@@ -0,0 +1,8 @@
+package com.ebicep.warlords.database.repositories.player.pojos.pve.anomaly;
+
+import com.ebicep.warlords.database.repositories.games.pojos.pve.anomaly.DatabaseGamePlayerPvEAnomaly;
+import com.ebicep.warlords.database.repositories.games.pojos.pve.anomaly.DatabaseGamePvEAnomaly;
+import com.ebicep.warlords.database.repositories.player.pojos.pve.PvEDatabaseStatInformation;
+
+public class AnomalyDatabaseStatInformation extends PvEDatabaseStatInformation<DatabaseGamePvEAnomaly, DatabaseGamePlayerPvEAnomaly> implements AnomalyStats {
+}
