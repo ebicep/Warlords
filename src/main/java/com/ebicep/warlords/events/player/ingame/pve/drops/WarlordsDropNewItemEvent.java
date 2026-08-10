fcd31ecc51c6c95368a257ce2b2a738f20b82cad
Paki <53056381+Plikie@users.noreply.github.com>
2026-08-10 09:38:49 +0200

    Add NewItem drop rate event

---
+++ src/main/java/com/ebicep/warlords/events/player/ingame/pve/drops/WarlordsDropNewItemEvent.java
@@ -0,0 +1,17 @@
+package com.ebicep.warlords.events.player.ingame.pve.drops;
+
+import com.ebicep.warlords.player.ingame.WarlordsEntity;
+import com.ebicep.warlords.pve.mobs.AbstractMob;
+import com.google.common.util.concurrent.AtomicDouble;
+import org.bukkit.event.HandlerList;
+
+public class WarlordsDropNewItemEvent extends AbstractWarlordsDropRewardEvent {
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    public WarlordsDropNewItemEvent(WarlordsEntity player, AbstractMob deadMob, AtomicDouble dropRate) {
+        super(player, deadMob, RewardType.ITEM, dropRate);
+    }
+}
