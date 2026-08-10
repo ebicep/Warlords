7a62d87f3a068da13e040baacc99cd2fe8064b39
Paki <53056381+Plikie@users.noreply.github.com>
2026-08-10 09:39:06 +0200

    Apply vial drop modifiers

--- src/main/java/com/ebicep/warlords/pve/consumables/ConsumableListener.java
+++ src/main/java/com/ebicep/warlords/pve/consumables/ConsumableListener.java
@@ -1,6 +1,9 @@
 package com.ebicep.warlords.pve.consumables;
 
 import com.ebicep.warlords.events.player.ingame.pve.WarlordsAddCurrencyEvent;
+import com.ebicep.warlords.events.player.ingame.pve.drops.AbstractWarlordsDropRewardEvent;
+import com.ebicep.warlords.events.player.ingame.pve.drops.WarlordsDropNewItemEvent;
+import com.ebicep.warlords.events.player.ingame.pve.drops.WarlordsDropWeaponEvent;
 import com.ebicep.warlords.player.ingame.WarlordsPlayer;
 import com.ebicep.warlords.pve.consumables.vials.VialEffect;
 import com.ebicep.warlords.pve.consumables.vials.VialManager;
@@ -17,5 +20,23 @@ public class ConsumableListener implements Listener {
         }
         double multiplier = VialManager.getMultiplier(warlordsPlayer.getDatabasePlayer(), VialEffect.INSIGNIA_GAIN);
         event.setCurrencyToAdd((float) (event.getCurrencyToAdd() * multiplier));
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onWeaponDrop(WarlordsDropWeaponEvent event) {
+        applyDropMultiplier(event, VialEffect.WEAPON_DROP_RATE);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onNewItemDrop(WarlordsDropNewItemEvent event) {
+        applyDropMultiplier(event, VialEffect.ITEM_DROP_RATE);
+    }
+
+    private void applyDropMultiplier(AbstractWarlordsDropRewardEvent event, VialEffect effect) {
+        if (!(event.getWarlordsEntity() instanceof WarlordsPlayer warlordsPlayer) || !warlordsPlayer.isInPve()) {
+            return;
+        }
+        double multiplier = VialManager.getMultiplier(warlordsPlayer.getDatabasePlayer(), effect);
+        event.setModifier(event.getModifier() * multiplier);
     }
 }
