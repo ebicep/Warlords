20644b10bba82d1a220d80cff21a25d6f3220c9b
ebicep <ebicep314@gmail.com>
2026-08-22 13:17:35 -0400

    fix(worlds): disable natural mob spawning via game rule
    Set SPAWN_MOBS false on world load and at plugin enable so phantoms and other vanilla mobs cannot spawn while plugin/CUSTOM spawns still work.

--- src/main/java/com/ebicep/warlords/Warlords.java
+++ src/main/java/com/ebicep/warlords/Warlords.java
@@ -365,6 +365,7 @@ public class Warlords extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new WarlordsEvents(), this);
         getServer().getPluginManager().registerEvents(new GeneralEvents(), this);
+        GeneralEvents.applyWorldRulesToLoadedWorlds();
         getServer().getPluginManager().registerEvents(new MenuEventListener(this), this);
         getServer().getPluginManager().registerEvents(new PartyListener(), this);
         getServer().getPluginManager().registerEvents(new BotListener(), this);
