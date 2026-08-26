53e95f422836c1813bc7c603546b47d44b87ee11
Plikie <plikie341@gmail.com>
2026-08-26 23:16:51 +0200

    Revert "fix(anomaly): retain What Once Was caches on defeat"
    This reverts commit 2e3ff7671636481e81d1588b3c4bbf29210b304d.

--- src/main/java/com/ebicep/warlords/game/option/pve/anomaly/WhatOnceWasPuzzleOption.java
+++ src/main/java/com/ebicep/warlords/game/option/pve/anomaly/WhatOnceWasPuzzleOption.java
@@ -26,7 +26,6 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.Interaction;
 import org.bukkit.entity.ItemDisplay;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.EquipmentSlot;
@@ -82,7 +81,6 @@ public class WhatOnceWasPuzzleOption extends AbstractAnomalyOption {
     private boolean vaultRunning;
     private boolean guardianWaveRunning;
     private boolean failed;
-    private boolean unlockedCachesGranted;
     private List<AncientRune> activeCode = List.of();
 
     @Override
@@ -126,13 +124,6 @@ public class WhatOnceWasPuzzleOption extends AbstractAnomalyOption {
                 }
                 event.setCancelled(true);
                 enterRune(event.getPlayer(), rune);
-            }
-
-            @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-            public void onWin(WarlordsGameTriggerWinEvent event) {
-                if (event.getDeclaredWinner() == Team.RED) {
-                    grantUnlockedCaches();
-                }
             }
         });
 
@@ -478,10 +469,6 @@ public class WhatOnceWasPuzzleOption extends AbstractAnomalyOption {
     }
 
     private void grantUnlockedCaches() {
-        if (unlockedCachesGranted) {
-            return;
-        }
-        unlockedCachesGranted = true;
         int eligibleObjectiveCount = Math.min(cacheEligibility.length, currentAnomaly.getRewardPools().size());
         game.warlordsPlayers().forEach(warlordsPlayer -> {
             DatabasePlayer databasePlayer = DatabaseManager.getPlayer(warlordsPlayer.getUuid());
